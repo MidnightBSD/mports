152a543911530873212376a5406dc3b922531122
Lucas Holt <luke@foolishgames.com>
2007-04-04 20:07:02 +0000

    Lets do a little cleanup work.  Add some new versions of tcl and tk since we're working on porting those.

--- Mk/bsd.tcl.mk
+++ Mk/bsd.tcl.mk
@@ -1,7 +1,7 @@
 # -*- mode: Makefile; tab-width: 4; -*-
 # ex: ts=4
 #
-# $MidnightBSD: mports/Mk/bsd.tcl.mk,v 1.2 2006/09/17 18:36:23 laffer1 Exp $
+# $MidnightBSD: mports/Mk/bsd.tcl.mk,v 1.3 2007/02/18 03:10:07 laffer1 Exp $
 # $FreeBSD: ports/Mk/bsd.tcl.mk,v 1.3 2006/05/01 19:49:57 sem Exp $
 #
 
@@ -10,17 +10,20 @@
 Tcl_Pre_Include=		bsd.tcl.mk
 Tcl_Include_MAINTAINER=	ports@MidnightBSD.org
 
-# USE_TCL:		Depend on tcl to run. In case of incompatible APIs of different 
-#				TCL versions the version can be specified directly. If version
-#				is not specified (USE_TCL=yes) then the latest version is
-#				used (8.4 currently).
-#				Available values are: 84, 83, 82, 81 and 80.
+# USE_TCL:		- Depend on tcl to run. In case of incompatible APIs of;
+#			 	different TCL versions the version can be 
+#				specified directly. If version is not specified
+#				(USE_TCL=yes) then the latest version is used
+#				(8.4 currently).
+#				Available values are: 85, 85-thread, 84, 
+#				84-thread, 83, 82, 81 and 80.
 #
-# USE_TK:		Depend on tk to run. In case of incompatible APIs of different
-#				TK versions the version can be specified directly. If version
-#				is not specified (USE_TK=yes) then the latest version is
-#				used (8.4 currently).
-#				Available values are: 84, 83, 82, 81 and 80.
+# USE_TK:		- Depend on tk to run. In case of incompatible APIs of 
+#				different TK versions the version can be 
+#				specified directly. If version is not specified
+#				(USE_TK=yes) then the latest version is used
+#				(8.4 currently).
+#				Available values are: 85, 84, 83, 82, 81 and 80.
 ##
 # TCL_LIBDIR:		Path where tcl libraries can be found
 #
@@ -44,7 +47,11 @@ Tcl_Include_MAINTAINER=	ports@MidnightBSD.org
 
 .if defined(USE_TCL) || defined(USE_TCL_BUILD)
 
-_TCL_VERSIONS=	84 83 82 81 80
+_TCL_VERSIONS=	85 85-thread 84 84-thread 83 82 81 80
+
+.if defined(USE_TCL)
+_RUN=		yes
+.endif
 
 .if defined(USE_TCL_BUILD)
 USE_TCL=	${USE_TCL_BUILD}
@@ -55,7 +62,7 @@ _BUILD=		yes
 USE_TCL=	84
 .endif
 
-TCL_VER:=	${USE_TCL:S/8/8./}
+TCL_VER:=	${USE_TCL:S/8/8./:S/-thread//}
 
 # Special case
 .if ${USE_TCL} == "81"
@@ -64,11 +71,12 @@ USE_TCL=	tcl81-thread
 
 _FOUND=		no
 .for ver in ${_TCL_VERSIONS}
-. if ${USE_TCL} == ${ver}
+. if ${USE_TCL} == "${ver}"
 _FOUND=		yes
 .  if defined(_BUILD)
 BUILD_DEPENDS+=	tclsh${TCL_VER}:${PORTSDIR}/lang/tcl${USE_TCL}
-.  else
+.  endif
+.  if defined(_RUN)
 RUN_DEPENDS+=	tclsh${TCL_VER}:${PORTSDIR}/lang/tcl${USE_TCL}
 .  endif
 TCL_INCLUDEDIR=	${LOCALBASE}/include/tcl${TCL_VER}
@@ -80,11 +88,20 @@ TCLSH=			${LOCALBASE}/bin/tclsh${TCL_VER}
 .if ${_FOUND} == "no"
 IGNORE=		Unknown TCL version specified: ${USE_TCL}
 .endif
-.endif # defined(USE_TCL)
+.endif # defined(USE_TCL) || defined(USE_TCL_BUILD)
+
+.if defined(USE_TK) || defined(USE_TK_BUILD)
+
+_TK_VERSIONS=	85 84 83 82 81 80
 
 .if defined(USE_TK)
+_TK_RUN=	yes
+.endif
 
-_TK_VERSIONS=	84 83 82 81 80
+.if defined(USE_TK_BUILD)
+USE_TK=		${USE_TK_BUILD}
+_TK_BUILD=	yes
+.endif
 
 .if ${USE_TK} == "yes"
 USE_TK=		84
@@ -101,7 +118,12 @@ _FOUND=		no
 .for ver in ${_TK_VERSIONS}
 . if ${USE_TK} == ${ver}
 _FOUND=		yes
+.  if defined(_TK_BUILD)
+BUILD_DEPENDS+=	wish${TK_VER}:${PORTSDIR}/x11-toolkits/tk${USE_TK}
+.endif
+.  if defined(_TK_RUN)
 RUN_DEPENDS+=	wish${TK_VER}:${PORTSDIR}/x11-toolkits/tk${USE_TK}
+.endif
 TCL_INCLUDEDIR=	${LOCALBASE}/include/tcl${TK_VER}
 TCL_LIBDIR=		${LOCALBASE}/lib/tcl${TK_VER}
 TK_INCLUDEDIR=	${LOCALBASE}/include/tk${TK_VER}
@@ -114,7 +136,7 @@ WISH=			${LOCALBASE}/bin/wish${TK_VER}
 .if ${_FOUND} == "no"
 IGNORE=		Unknown TK version specified: ${USE_TK}
 .endif
-.endif # defined(USE_TK)
+.endif # defined(USE_TK) || defined(USE_TK_BUILD)
 
 .endif # !defined(_POSTMKINCLUDED) && !defined(Tcl_Pre_Include)
 
