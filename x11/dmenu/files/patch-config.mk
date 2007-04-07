83cc1b9535afd5de35e02cdf7ae1a8a11387240b
wintellect <phil@bsdnexus.com>
2007-04-07 10:51:50 +0000

    updated dmenu

--- x11/dmenu/files/patch-config.mk
+++ x11/dmenu/files/patch-config.mk
@@ -1,30 +1,18 @@
---- config.mk.orig	Tue Aug 29 20:32:42 2006
-+++ config.mk	Tue Aug 29 20:33:56 2006
-@@ -4,21 +4,21 @@
+--- config.mk.orig	Sat Apr  7 11:45:07 2007
++++ config.mk	Sat Apr  7 11:46:13 2007
+@@ -4,11 +4,11 @@
  # Customize below to fit your system
  
  # paths
 -PREFIX = /usr/local
 -MANPREFIX = ${PREFIX}/share/man
-+PREFIX ?= /usr/local
++#PREFIX = /usr/local
 +MANPREFIX = ${PREFIX}/man
  
 -X11INC = /usr/X11R6/include
 -X11LIB = /usr/X11R6/lib
-+X11INC = $(X11BASE)/include
-+X11LIB = $(X11BASE)/lib
++X11INC = ${X11BASE}/include
++X11LIB = ${X11BASE}/lib
  
  # includes and libs
  INCS = -I. -I/usr/include -I${X11INC}
- LIBS = -L/usr/lib -lc -L${X11LIB} -lX11
- 
- # flags
--CFLAGS = -Os ${INCS} -DVERSION=\"${VERSION}\"
-+CFLAGS+= ${INCS} -DVERSION=\"${VERSION}\"
- LDFLAGS = ${LIBS}
- #CFLAGS = -g -Wall -O2 ${INCS} -DVERSION=\"${VERSION}\"
- #LDFLAGS = -g ${LIBS}
- 
- # compiler and linker
--CC = cc
-+CC?= cc
