12af494e3ad2e53ee1f2837a39d7fad2321258d5
Chris Reinhardt <github@triv.org>
2007-09-05 19:25:36 +0000

    Add recurse_ports.  Only export symbols on request.

--- Tools/lib/Mport/Utils.pm
+++ Tools/lib/Mport/Utils.pm
@@ -1,16 +1,18 @@
 package Mport::Utils;
 #
-# $MidnightBSD$
+# $MidnightBSD: mports/Tools/lib/Mport/Utils.pm,v 1.1 2007/08/15 20:55:39 ctriv Exp $
 #
 use strict;
 use warnings;
 use Exporter ();
+use Cwd ();
 use Text::ParseWords qw(shellwords);
-use Mport::Globals qw($MAKE);
+
+use Mport::Globals qw($MAKE $ROOT);
 
 *import = \&Exporter::import;
 
-our @EXPORT = qw(make_var);
+our @EXPORT_OK = qw(make_var recurse_ports);
 
 sub make_var {
   my $vars  = join(' ', map { "-V $_" } @_);
@@ -22,8 +24,31 @@ sub make_var {
     return $ret;
   }
 }
-  
 
+sub recurse_ports (&) {
+  my ($code) = @_;
+  my $orig = Cwd::getcwd();
+  
+  _do_recurse($code, $ROOT);
+  
+  chdir($orig);
+}
+
+sub _do_recurse {
+  my ($code, $cwd) = @_;
+  
+  chdir($cwd);
+  
+  my @dirs = make_var('SUBDIR');
+  
+  if (@dirs) {
+    foreach my $dir (@dirs) {
+      _do_recurse($code, "$cwd/$dir");
+    }
+  } else {
+    $code->($cwd);
+  }
+}      
 
 1;
 __END__
