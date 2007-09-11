5f5735f09ef2bff36ba0d171bd464b8aa1a05101
ctriv <ctriv@c872e0db-cc11-e311-ab8b-002590791c19>
2007-09-11 02:29:26 +0000

    A pretty significant refactoring.  This code is still in active developement, and future such changes are likely.

--- trunk/Tools/lib/Mport/Index.pm
+++ trunk/Tools/lib/Mport/Index.pm
@@ -1,13 +1,12 @@
 package Mport::Index;
 #
-# $MidnightBSD$
+# $MidnightBSD: mports/Tools/lib/Mport/Index.pm,v 1.1 2007/08/15 20:55:39 ctriv Exp $
 #
 use strict;
 use warnings;
 
 use Mport::Globals qw($ROOT $INDEX);
-use Mport::Port ();
-use Mport::Utils qw(make_var);
+use Mport::Utils qw(make_var recurse_ports);
 use YAML;
 
 use Cwd;
@@ -17,78 +16,78 @@ use DBI;
 sub build {
   my ($class) = @_;
   
-  my $cwd = $ROOT;
   my $dbh = create_db();
-      
-  chdir($cwd);
-
-  recurse($cwd, $dbh);
-}
-
-sub recurse {  
-  my ($cwd, $dbh) = @_;
   
-  my @subdirs = make_var('SUBDIR');
+  my $categories_inserted;
   
-  if (!@subdirs) {
-    return process_port($cwd, $dbh);
-  }
+  recurse_ports {  
+    insert_categories($dbh) unless $categories_inserted++;
   
-  foreach my $subdir (@subdirs) {
-    $subdir = "$cwd/$subdir";
-    eval { chdir($subdir); };
-    next if $@;
-    recurse($subdir, $dbh)
-  }   
-}
-
-sub process_port {
-  my ($cwd, $dbh) = @_;
+    my $yaml = `make describe-yaml`;
+    my %port;
+    eval {
+      %port = %{Load($yaml)};  
+    };
   
-  my $yaml = `make describe-yaml`;
-  my %port;
-  eval {
-    %port = %{Load($yaml)};  
-  };
-  
-  if ($@) {
-    warn "Unable to parse yaml for $cwd\n";
-    return;
-  }
-
-  local $dbh->{AutoCommit} = 0;
-  
-  eval {
-    my $sth = $dbh->prepare("INSERT INTO mports (name, version, description, origin) VALUES (?,?,?,?)");
-    $sth->execute(@port{qw(name version description origin)});
-    $sth->finish;
-  
-    $sth = $dbh->prepare("INSERT INTO depends (port, type, dependency) VALUES (?,?,?)");
-    foreach my $type (qw(lib run build patch extract fetch)) {
-      foreach my $dep (@{$port{"${type}_depends"}}) {
-        $sth->execute($port{'name'}, $type, $dep);
-      }
+    if ($@) {
+      warn "Unable to parse yaml for $_[0]: $@\n";
+      return;
     }
-    $sth->finish;
-    
-    $dbh->commit;
-  };
+
+    local $dbh->{AutoCommit} = 0;
   
-  if ($@) {
-    warn "Unable to insert $port{'name'}: $@\n";
-    eval { $dbh->rollback };
-  }    
+    eval {
+      my $sth = $dbh->prepare("INSERT INTO ports (name, version, description, pkgname, license) VALUES (?,?,?,?,?)");
+      $sth->execute(@port{qw(name version description pkgname license)});
+      $sth->finish;
+  
+      $sth = $dbh->prepare("INSERT INTO depends (port, type, dependency) VALUES (?,?,?)");
+      while (my ($type, $deps) = each %{$port{'depends'}}) {
+        foreach my $dep (@$deps) {
+          $sth->execute($port{'name'}, $type, $dep);
+        }
+      }
+      $sth->finish;
+      
+      $sth = $dbh->prepare("INSERT INTO port_categories (port, category_id) VALUES (?, (SELECT id FROM categories WHERE category=?))");      
+      foreach my $cat (@{$port{'categories'}}) {      
+        $sth->execute($port{'name'}, $cat);
+      }
+      $sth->finish;
+      
+      $dbh->commit;
+    };
+  
+    if ($@) {
+      warn "Unable to insert $port{'name'} ($_[0]): $@\n";
+      eval { $dbh->rollback };
+    }    
+  };
 }
+
+sub insert_categories {
+  my ($dbh) = @_;
+  
+  my @categories = make_var("VALID_CATEGORIES");
+  
+  my $sth = $dbh->prepare("INSERT INTO categories (category) VALUES (?)");
+  foreach my $cat (@categories) {
+    $sth->execute($cat);
+  }
+  $sth->finish;
+}
+      
 
 sub create_db {
   unlink($INDEX);
   my $dbh = DBI->connect("dbi:SQLite:dbname=$INDEX", "", "", { RaiseError => 1, PrintError => 0 });
   $dbh->do(<<END_O_SQL);
-CREATE TABLE mports (
+CREATE TABLE ports (
   name text primary key,
   version text,
   description text,
-  origin text
+  license text,
+  pkgname text
 )
 END_O_SQL
   $dbh->do(<<END_O_SQL);
@@ -98,10 +97,26 @@ CREATE TABLE depends (
   dependency text
 );
 END_O_SQL
-  $dbh->do('CREATE INDEX mport_version_index ON mports (name, version)');
+  $dbh->do(<<END_O_SQL);
+CREATE TABLE categories (
+  id integer primary key autoincrement, 
+  category text
+);
+END_O_SQL
+  $dbh->do(<<END_O_SQL);
+CREATE TABLE port_categories (
+  port text, 
+  category_id int
+);
+END_O_SQL
+
+  $dbh->do('CREATE INDEX mport_version_index ON ports (name, version)');
   $dbh->do('CREATE INDEX depends_port ON depends (port)');
   $dbh->do('CREATE INDEX depends_revese ON depends (dependency)');
   $dbh->do('CREATE INDEX depends_type ON depends (port, type)');
+
+  # XXX more indexes needed.
+  
   
   return $dbh;
 }
