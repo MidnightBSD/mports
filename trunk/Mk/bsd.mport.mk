c4b32b83c790136c636bd090b1bf5acaaf6fd657
ctriv <ctriv@c872e0db-cc11-e311-ab8b-002590791c19>
2007-12-05 16:23:55 +0000

    Add other license type. This should be used when the license is known, but not one of our categories. Unknown is for when the license cannot be found.

--- trunk/Mk/bsd.mport.mk
+++ trunk/Mk/bsd.mport.mk
@@ -1,7 +1,7 @@
 #-*- mode: makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $MidnightBSD: mports/Mk/bsd.mport.mk,v 1.76 2007/11/20 11:28:07 laffer1 Exp $
+# $MidnightBSD: mports/Mk/bsd.mport.mk,v 1.77 2007/12/03 23:39:14 ctriv Exp $
 # $FreeBSD: ports/Mk/bsd.port.mk,v 1.540 2006/08/14 13:24:18 erwin Exp $
 #
 #   bsd.mport.mk - 2007/04/01 Chris Reinhardt
@@ -2852,7 +2852,7 @@ USE_LDCONFIG!=	${ECHO_CMD} ${LDCONFIG_DIRS} | ${SED} ${PLIST_SUB:S/$/!g/:S/^/ -e
 #
 _LICENSES= 	agpl gpl gpl2 gpl3 lgpl bsd4 bsd3 bsd2 python ruby x11 guile artistic artistic2 \
 		bdb mpl npl publicdom zlib apache2 apache1.1 apache1 apsl2 apsl1 php \
-		restricted perl modula3 unknown sgi agg
+		restricted perl modula3 unknown other sgi agg
 
 
 
@@ -5468,7 +5468,7 @@ makeplist:
 	@${ECHO_MSG} "===>   Generating packing list"
 	@if [ ! -f ${DESCR} ]; then ${ECHO_MSG} "** Missing pkg-descr for ${PKGNAME}."; exit 1; fi
 	@${MKDIR} `${DIRNAME} ${GENPLIST}`
-	@${ECHO_CMD} '@comment $$MidnightBSD: mports/Mk/bsd.mport.mk,v 1.76 2007/11/20 11:28:07 laffer1 Exp $$' > ${GENPLIST}
+	@${ECHO_CMD} '@comment $$MidnightBSD: mports/Mk/bsd.mport.mk,v 1.77 2007/12/03 23:39:14 ctriv Exp $$' > ${GENPLIST}
 
 .	if !defined(NO_MTREE)
 		@cd ${FAKE_DESTDIR}${PREFIX}; directories=""; files=""; \
