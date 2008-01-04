12e946527d9f4d56dd4739c588db8fdffea9179e
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2008-01-04 19:17:06 +0000

    Update gamin.

--- trunk/devel/gamin/files/patch-server_gam_kqueue.c
+++ trunk/devel/gamin/files/patch-server_gam_kqueue.c
@@ -1,5 +1,5 @@
---- server/gam_kqueue.c.orig	Wed Aug 10 23:50:32 2005
-+++ server/gam_kqueue.c	Tue Feb 14 10:00:17 2006
+--- server/gam_kqueue.c.orig	2007-07-04 09:50:41.000000000 -0400
++++ server/gam_kqueue.c	2007-08-10 15:03:59.000000000 -0400
 @@ -10,9 +10,10 @@
   *       FAM should do: we do not call g_dir_open() if the file is a
   *       symbolic link).
@@ -37,16 +37,16 @@
  #include <sys/types.h>
  #include <sys/sysctl.h>
  #include <sys/stat.h>
-@@ -63,7 +66,7 @@
- #include "gam_kqueue.h"
- #include "gam_event.h"
- #include "gam_server.h"
--#include "gam_poll.h"
-+#include "gam_poll_basic.h"
+@@ -326,7 +329,7 @@ gam_kqueue_isdir (const char *pathname, 
+ static gboolean
+ gam_kqueue_get_uint_sysctl (const char *name, unsigned int *value)
+ {
+-  unsigned int value_len = sizeof(*value);
++  size_t value_len = sizeof(*value);
  
- /*** tunable constants, modify to tweak the backend aggressivity *************/
- 
-@@ -509,6 +512,9 @@
+   if (sysctlbyname(name, value, &value_len, (void *)NULL, 0) < 0)
+     {
+@@ -509,6 +512,9 @@ static gboolean
  gam_kqueue_monitor_enable_kqueue (Monitor *mon)
  {
    struct kevent ev[1];
@@ -56,7 +56,7 @@
  
    if (open_files == max_open_files)
      {
-@@ -516,26 +522,36 @@
+@@ -516,26 +522,36 @@ gam_kqueue_monitor_enable_kqueue (Monito
        return FALSE;
      }
    
@@ -99,7 +99,7 @@
  }
  
  static void
-@@ -840,6 +856,8 @@
+@@ -840,6 +856,8 @@ gam_kqueue_sub_monitor_emit_event (SubMo
      case GAMIN_EVENT_MOVED:
        gam_kqueue_sub_monitor_set_missing(smon);
        break;
@@ -108,7 +108,7 @@
      }
  
    gam_server_emit_event(mon->pathname, isdir, event, smon->subs, 1);
-@@ -981,6 +999,8 @@
+@@ -981,6 +999,8 @@ gam_kqueue_file_monitor_emit_event (File
  	
        gam_kqueue_hash_table_remove(fmon->smon->fmons, fmon);
        break;
@@ -117,24 +117,13 @@
      }
  }
  
-@@ -1167,11 +1187,15 @@
+@@ -1167,6 +1187,9 @@ gam_kqueue_init (void)
    channel = g_io_channel_unix_new(kq);
    g_io_add_watch(channel, G_IO_IN, gam_kqueue_kevent_cb, NULL);
  
--  
--  gam_poll_set_kernel_handler(NULL, NULL, GAMIN_K_KQUEUE);
--  gam_backend_add_subscription = gam_kqueue_add_subscription;
--  gam_backend_remove_subscription = gam_kqueue_remove_subscription;
--  gam_backend_remove_all_for = gam_kqueue_remove_all_for;
 +#ifdef USE_GAMIN_POLLER
-+  gam_poll_basic_init (); 
++  gam_poll_basic_init ();
 +#endif
-+  gam_server_install_kernel_hooks (GAMIN_K_KQUEUE,
-+                                   gam_kqueue_add_subscription,
-+                                   gam_kqueue_remove_subscription,
-+                                   gam_kqueue_remove_all_for,
-+                                   NULL,
-+                                   NULL);
- 
-   return TRUE;
- }
+   
+   gam_server_install_kernel_hooks(GAMIN_K_KQUEUE,
+   				  gam_kqueue_add_subscription,
