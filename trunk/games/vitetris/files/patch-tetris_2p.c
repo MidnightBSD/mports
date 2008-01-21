34c9cf11586d3083af4ead4477e25b30a1c0397d
smultron <smultron@c872e0db-cc11-e311-ab8b-002590791c19>
2008-01-21 03:29:49 +0000

    *** empty log message ***

---
+++ trunk/games/vitetris/files/patch-tetris_2p.c
@@ -0,0 +1,8 @@
+--- tetris_2p.c.orig	Sun Jan 20 20:50:31 2008
++++ tetris_2p.c	Sun Jan 20 20:50:39 2008
+@@ -1,5 +1,4 @@
+ #include <stdlib.h>
+-#include <alloca.h>
+ #include <string.h>
+ #include "tetris.h"
+ #include "input.h"
