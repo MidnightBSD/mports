9e30a9d8b6269e31fbd9c92b21879d1c8bcae189
Lucas Holt <luke@foolishgames.com>
2008-02-29 00:46:48 +0000

    Remove out of date patches.

--- games/vitetris/files/patch-vt.c
+++
@@ -1,9 +0,0 @@
---- vt.c.orig	Sun Jan 20 20:56:39 2008
-+++ vt.c	Sun Jan 20 20:56:44 2008
-@@ -1,6 +1,5 @@
- #include <stdio.h>
- #include <stdlib.h>
--#include <alloca.h>
- #include <string.h>
- #include <ctype.h>
- #include <unistd.h>
