00e46d1e54540439fd907ede7663352ad97a31a6
Chris Reinhardt <github@triv.org>
2008-03-25 05:13:39 +0000

    add distinct_ports method.

--- Tools/lib/Magus/Category.pm
+++ Tools/lib/Magus/Category.pm
@@ -24,7 +24,7 @@ package Magus::Category;
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# $MidnightBSD: mports/Tools/lib/Magus/Category.pm,v 1.1 2007/10/29 06:56:29 ctriv Exp $
+# $MidnightBSD: mports/Tools/lib/Magus/Category.pm,v 1.2 2008/02/24 23:58:47 ctriv Exp $
 # 
 # MAINTAINER=   ctriv@MidnightBSD.org
 #
@@ -37,6 +37,26 @@ use base qw(Magus::DBI);
 __PACKAGE__->table('categories');
 __PACKAGE__->columns(Essential => qw/id category/);
 __PACKAGE__->has_many(ports => [ 'Magus::PortCategory' => 'port']);
+
+
+=head2 $cat->distinct_ports
+
+Returns an arrayref of port names.  These are the distinct ports
+belonging to this cateogory, without duplicates.
+
+=cut
+sub distinct_ports {
+  my ($self) = @_;
+  
+  my $dbh = $self->db_Main;
+  
+  return $dbh->selectcol_arrayref(
+    "SELECT DISTINCT(ports.name) FROM ports,port_categories WHERE ports.id=port AND category=?", 
+    undef, 
+    $self->id
+  );
+}  
+  
 
 
 1;
