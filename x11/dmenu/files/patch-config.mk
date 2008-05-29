59b01e233b3e8860d9881df9e0ae708d252b261e
Lucas Holt <luke@foolishgames.com>
2008-05-29 21:22:23 +0000

    Update to 3.6.

--- x11/dmenu/files/patch-config.mk
+++ x11/dmenu/files/patch-config.mk
@@ -1,18 +1,34 @@
---- config.mk.orig	Sat Apr  7 11:45:07 2007
-+++ config.mk	Sat Apr  7 11:46:13 2007
-@@ -4,11 +4,11 @@
+--- config.mk.orig	2007-10-25 20:26:14.000000000 +0200
++++ config.mk	2007-10-27 00:36:35.000000000 +0200
+@@ -4,19 +4,19 @@
  # Customize below to fit your system
  
  # paths
 -PREFIX = /usr/local
 -MANPREFIX = ${PREFIX}/share/man
-+#PREFIX = /usr/local
++PREFIX?= /usr/local
 +MANPREFIX = ${PREFIX}/man
  
 -X11INC = /usr/X11R6/include
 -X11LIB = /usr/X11R6/lib
-+X11INC = ${X11BASE}/include
-+X11LIB = ${X11BASE}/lib
++X11INC = $(LOCALBASE)/include
++X11LIB = $(LOCALBASE)/lib
  
  # includes and libs
  INCS = -I. -I/usr/include -I${X11INC}
+ LIBS = -L/usr/lib -lc -L${X11LIB} -lX11
+ 
+ # flags
+-CFLAGS = -Os ${INCS} -DVERSION=\"${VERSION}\"
+-LDFLAGS = -s ${LIBS}
++CFLAGS+= ${INCS} -DVERSION=\"${VERSION}\"
++LDFLAGS+= ${LIBS}
+ #CFLAGS = -g -std=c99 -pedantic -Wall -O2 ${INCS} -DVERSION=\"${VERSION}\"
+ #LDFLAGS = -g ${LIBS}
+ 
+@@ -26,4 +26,4 @@
+ #CFLAGS += -xtarget=ultra
+ 
+ # compiler and linker
+-CC = cc
++CC?= cc
