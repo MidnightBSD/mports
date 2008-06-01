ad12cc8d0508ff5a58bac455d68a8b5b87e73bab
Adam Karim <adam@akarsoft.com>
2008-06-01 23:27:30 +0000

    Update of hal to latest version.

--- sysutils/hal/files/extra-patch-tools_hal-storage-mount.c
+++ sysutils/hal/files/extra-patch-tools_hal-storage-mount.c
@@ -1,30 +1,25 @@
---- tools/hal-storage-mount.c.orig	Sun May 14 20:57:59 2006
-+++ tools/hal-storage-mount.c	Sat Apr  7 12:50:53 2007
-@@ -513,23 +513,11 @@
- 	explicit_mount_point_given = FALSE;
- 	if (strlen (mount_point) == 0) {
- 		char *p;
--		const char *label;
-+		char *basename;
- 
--		if (volume != NULL)
--			label = libhal_volume_get_label (volume);
--		else
--			label = NULL;
--
--		if (label != NULL) {
--			/* best - use label */
--			g_strlcpy (mount_point, label, sizeof (mount_point));
--
--			/* TODO: use drive type */
--
--		} else {
--			/* fallback - use "disk" */
--			g_snprintf (mount_point, sizeof (mount_point), "disk");
--		}
-+		basename = g_path_get_basename (device);
-+		g_strlcpy (mount_point, basename, sizeof (mount_point));
-+		g_free (basename);
- 
- 		/* sanitize computed mount point name, e.g. replace invalid chars with '-' */
- 		p = mount_point;
+--- tools/hal-storage-mount.c.orig	2008-05-20 13:24:30.000000000 -0400
++++ tools/hal-storage-mount.c	2008-05-20 13:30:14.000000000 -0400
+@@ -585,17 +585,11 @@ handle_mount (LibHalContext *hal_ctx, 
+ 		explicit_mount_point_given = FALSE;
+ 		if (strlen (mount_point) == 0) {
+ 			char *p;
+-			
+-			if (label != NULL) {
+-				/* best - use label */
+-				g_strlcpy (mount_point, label, sizeof (mount_point));
+-				
+-				/* TODO: use drive type */
+-				
+-			} else {
+-				/* fallback - use "disk" */
+-				g_snprintf (mount_point, sizeof (mount_point), "%s", "disk");
+-			}
++			char *basename;
++
++			basename = g_path_get_basename (device);
++			g_strlcpy (mount_point, basename, sizeof (mount_point));
++			g_free (basename);
+ 			
+ 			/* sanitize computed mount point name, e.g. replace invalid chars with '-' */
+ 			p = mount_point;
