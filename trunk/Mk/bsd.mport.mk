571837349c565863d2deab19a69109f0927c7346
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2008-06-20 01:29:24 +0000

    add lgpl3 and opera license types.
    All ports using lgpl v3 should use lgpl3.  lgpl v2 and v2.1 should continue to use lgpl

--- trunk/Mk/bsd.mport.mk
+++ trunk/Mk/bsd.mport.mk
@@ -1,7 +1,7 @@
 #-*- mode: makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $MidnightBSD: mports/Mk/bsd.mport.mk,v 1.107 2008/06/06 00:09:50 laffer1 Exp $
+# $MidnightBSD: mports/Mk/bsd.mport.mk,v 1.108 2008/06/13 03:54:29 laffer1 Exp $
 # $FreeBSD: ports/Mk/bsd.port.mk,v 1.540 2006/08/14 13:24:18 erwin Exp $
 #
 #   bsd.mport.mk - 2007/04/01 Chris Reinhardt
@@ -2884,9 +2884,9 @@ USE_LDCONFIG!=	${ECHO_CMD} ${LDCONFIG_DIRS} | ${SED} ${PLIST_SUB:S/$/!g/:S/^/ -e
 #
 # LICENSE Setup
 #
-_LICENSES= 	agpl gpl gpl2 gpl3 lgpl bsd4 bsd3 bsd2 python ruby x11 guile artistic artistic2 \
+_LICENSES= 	agpl gpl gpl2 gpl3 lgpl lgpl3 bsd4 bsd3 bsd2 python ruby x11 guile artistic artistic2 \
 		bdb mpl npl publicdom zlib apache2 apache1.1 apache1 apsl2 apsl1 php openldap2.8 \
-		restricted perl mit modula3 cddl unknown other sgi agg
+		restricted perl mit modula3 cddl opera unknown other sgi agg
 
 
 
@@ -5231,7 +5231,7 @@ makeplist:
 	@${ECHO_MSG} "===>   Generating packing list"
 	@if [ ! -f ${DESCR} ]; then ${ECHO_MSG} "** Missing pkg-descr for ${PKGNAME}."; exit 1; fi
 	@${MKDIR} `${DIRNAME} ${GENPLIST}`
-	@${ECHO_CMD} '@comment $$MidnightBSD: mports/Mk/bsd.mport.mk,v 1.107 2008/06/06 00:09:50 laffer1 Exp $$' > ${GENPLIST}
+	@${ECHO_CMD} '@comment $$MidnightBSD: mports/Mk/bsd.mport.mk,v 1.108 2008/06/13 03:54:29 laffer1 Exp $$' > ${GENPLIST}
 
 .	if !defined(NO_MTREE)
 		@cd ${FAKE_DESTDIR}${PREFIX}; directories=""; files=""; \
@@ -5669,4 +5669,5 @@ install-desktop-entries:
 .endif
 
 .endif
+
 # End of post-makefile section.
