c209ef7ecf356af3781637d5fd6c60246d316a70
Lucas Holt <luke@foolishgames.com>
2008-10-02 00:59:59 +0000

    Update zip to 3.0.
     * Add infozip license type and place the license in Licenses.
     * Set the license in zip port.
     * Use new sourceforge mirror

--- Mk/bsd.mport.mk
+++ Mk/bsd.mport.mk
@@ -1,7 +1,7 @@
 #-*- mode: makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $MidnightBSD: mports/Mk/bsd.mport.mk,v 1.117 2008/09/25 03:02:04 laffer1 Exp $
+# $MidnightBSD: mports/Mk/bsd.mport.mk,v 1.118 2008/09/30 23:33:50 laffer1 Exp $
 # $FreeBSD: ports/Mk/bsd.port.mk,v 1.540 2006/08/14 13:24:18 erwin Exp $
 #
 #   bsd.mport.mk - 2007/04/01 Chris Reinhardt
@@ -2917,7 +2917,7 @@ USE_LDCONFIG!=	${ECHO_CMD} ${LDCONFIG_DIRS} | ${SED} ${PLIST_SUB:S/$/!g/:S/^/ -e
 #
 _LICENSES= 	agpl gpl gpl2 gpl3 lgpl lgpl3 bsd4 bsd3 bsd2 python ruby x11 guile artistic artistic2 \
 		bdb mpl npl publicdom zlib apache2 apache1.1 apache1 apsl2 apsl1 php openldap2.8 \
-		restricted perl mit modula3 cddl opera liberation sgi bzip2 unknown other agg
+		restricted perl mit modula3 cddl opera liberation sgi bzip2 infozip unknown other agg
 
 
 
@@ -5262,7 +5262,7 @@ makeplist:
 	@${ECHO_MSG} "===>   Generating packing list"
 	@if [ ! -f ${DESCR} ]; then ${ECHO_MSG} "** Missing pkg-descr for ${PKGNAME}."; exit 1; fi
 	@${MKDIR} `${DIRNAME} ${GENPLIST}`
-	@${ECHO_CMD} '@comment $$MidnightBSD: mports/Mk/bsd.mport.mk,v 1.117 2008/09/25 03:02:04 laffer1 Exp $$' > ${GENPLIST}
+	@${ECHO_CMD} '@comment $$MidnightBSD: mports/Mk/bsd.mport.mk,v 1.118 2008/09/30 23:33:50 laffer1 Exp $$' > ${GENPLIST}
 
 .	if !defined(NO_MTREE)
 		@cd ${FAKE_DESTDIR}${PREFIX}; directories=""; files=""; \
