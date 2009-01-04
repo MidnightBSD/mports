26b1942a761b9b93cbfc3ded2a46cc64c7c8d40f
Chris Reinhardt <github@triv.org>
2009-01-04 04:30:18 +0000

    Make USE_SQLITE a no op when OSVERSION is 0.2.x or greater.

--- Mk/extensions/sqlite.mk
+++ Mk/extensions/sqlite.mk
@@ -1,7 +1,7 @@
 #-*- mode: makefile; tab-width: 4; -*-
 # ex: ts=4
 #
-# $MidnightBSD: mports/Mk/bsd.database.mk,v 1.15 2008/10/20 17:48:49 laffer1 Exp $ 
+# $MidnightBSD: mports/Mk/extensions/sqlite.mk,v 1.1 2008/10/23 22:55:44 ctriv Exp $ 
 # $FreeBSD: ports/Mk/bsd.database.mk,v 1.14 2006/07/05 02:18:08 linimon Exp $
 #
 
@@ -22,15 +22,18 @@ _SQLITE_VER=	3
 _SQLITE_VER=	 ${USE_SQLITE}
 .endif
 
+
+.if ${OSVERSION} < 2000
 # USE_SQLITE is specified incorrectly, so mark this as IGNORE
-.if ${_SQLITE_VER} == "3"
+.	if ${_SQLITE_VER} == "3"
 LIB_DEPENDS+=	sqlite${_SQLITE_VER}:${PORTSDIR}/databases/sqlite${_SQLITE_VER}
 SQLITE_VER=	${_SQLITE_VER}
-.elif ${_SQLITE_VER} == "2"
+.	elif ${_SQLITE_VER} == "2"
 LIB_DEPENDS+=	sqlite.${_SQLITE_VER}:${PORTSDIR}/databases/sqlite${_SQLITE_VER}
 SQLITE_VER=	${_SQLITE_VER}
-.else
+.	else
 IGNORE=	cannot install: unknown sqlite version: ${_SQLITE_VER}
+.	endif
 .endif
 
 .endif # defined(_POSTMKINCLUDED) && !defined(Sqlite_Post_Include)
