fea0bed062205633af7b790f87a163188e8e6172
ctriv <ctriv@c872e0db-cc11-e311-ab8b-002590791c19>
2009-03-11 20:29:07 +0000

    start work on xfce 4.6.0. Remove all hard coded versions, replaced with new XFCE_VERSION var.

--- trunk/Mk/extensions/xfce.mk
+++ trunk/Mk/extensions/xfce.mk
@@ -1,10 +1,12 @@
-# $MidnightBSD: mports/Mk/bsd.xfce.mk,v 1.1 2008/09/25 02:48:42 laffer1 Exp $
+# $MidnightBSD: mports/Mk/extensions/xfce.mk,v 1.1 2008/10/24 20:33:51 ctriv Exp $
 #
 
 .if !defined(_POSTMKINCLUDED) && !defined(Xfce_Pre_Include)
 
 Xfce_Pre_Include=		bsd.xfce.mk
 Xfce_Include_MAINTAINER=	ports@MidnightBSD.org
+
+XFCE_VERSION=	4.6.0
 
 # This file contains some variable definitions that are supposed to
 # make your life easier when dealing with ports related to the Xfce
@@ -14,33 +16,33 @@ Xfce_Include_MAINTAINER=	ports@MidnightBSD.org
 _USE_XFCE_ALL=			configenv libexo libgui libutil libmcs mcsmanager panel \
 				thunar wm xfdev
 
-MASTER_SITE_SUBDIR?=		xfce-4.4.2
+MASTER_SITE_SUBDIR?=		xfce-${XFCE_VERSION}
 
 configenv_CONFIGURE_ENV=	CPPFLAGS="${CPPFLAGS} -I${LOCALBASE}/include -L${LOCALBASE}/lib"
 
 libexo_BUILD_DEPENDS=		libexo>=0.3.4:${PORTSDIR}/x11/libexo
 libexo_RUN_DEPENDS=		libexo>=0.3.4:${PORTSDIR}/x11/libexo
 
-libgui_BUILD_DEPENDS=		libxfce4gui>=4.4.2:${PORTSDIR}/x11-toolkits/libxfce4gui
-libgui_RUN_DEPENDS=		libxfce4gui>=4.4.2:${PORTSDIR}/x11-toolkits/libxfce4gui
+libgui_BUILD_DEPENDS=		libxfce4gui>=${XFCE_VERSION}:${PORTSDIR}/x11-toolkits/libxfce4gui
+libgui_RUN_DEPENDS=		libxfce4gui>=${XFCE_VERSION}:${PORTSDIR}/x11-toolkits/libxfce4gui
 
-libutil_BUILD_DEPENDS=		libxfce4util>=4.4.2:${PORTSDIR}/x11/libxfce4util
-libutil_RUN_DEPENDS=		libxfce4util>=4.4.2:${PORTSDIR}/x11/libxfce4util
+libutil_BUILD_DEPENDS=		libxfce4util>=${XFCE_VERSION}:${PORTSDIR}/x11/libxfce4util
+libutil_RUN_DEPENDS=		libxfce4util>=${XFCE_VERSION}:${PORTSDIR}/x11/libxfce4util
 
-libmcs_BUILD_DEPENDS=		libxfce4mcs>=4.4.2:${PORTSDIR}/x11/libxfce4mcs
-libmcs_RUN_DEPENDS=		libxfce4mcs>=4.4.2:${PORTSDIR}/x11/libxfce4mcs
+libmcs_BUILD_DEPENDS=		libxfce4mcs>=${XFCE_VERSION}:${PORTSDIR}/x11/libxfce4mcs
+libmcs_RUN_DEPENDS=		libxfce4mcs>=${XFCE_VERSION}:${PORTSDIR}/x11/libxfce4mcs
 
-mcsmanager_BUILD_DEPENDS=	xfce4-mcs-manager>=4.4.2:${PORTSDIR}/sysutils/xfce4-mcs-manager
-mcsmanager_RUN_DEPENDS=		xfce4-mcs-manager>=4.4.2:${PORTSDIR}/sysutils/xfce4-mcs-manager
+mcsmanager_BUILD_DEPENDS=	xfce4-mcs-manager>=${XFCE_VERSION}:${PORTSDIR}/sysutils/xfce4-mcs-manager
+mcsmanager_RUN_DEPENDS=		xfce4-mcs-manager>=${XFCE_VERSION}:${PORTSDIR}/sysutils/xfce4-mcs-manager
 
-panel_BUILD_DEPENDS=		xfce4-panel>=4.4.2:${PORTSDIR}/x11-wm/xfce4-panel
-panel_RUN_DEPENDS=		xfce4-panel>=4.4.2:${PORTSDIR}/x11-wm/xfce4-panel
+panel_BUILD_DEPENDS=		xfce4-panel>=${XFCE_VERSION}:${PORTSDIR}/x11-wm/xfce4-panel
+panel_RUN_DEPENDS=		xfce4-panel>=${XFCE_VERSION}:${PORTSDIR}/x11-wm/xfce4-panel
 
 thunar_BUILD_DEPENDS=		Thunar>=0.9.0:${PORTSDIR}/x11-fm/thunar
 thunar_RUN_DEPENDS=		Thunar>=0.9.0:${PORTSDIR}/x11-fm/thunar
 
-wm_BUILD_DEPENDS=		xfce4-wm>=4.4.2:${PORTSDIR}/x11-wm/xfce4-wm
-wm_RUN_DEPENDS=			xfce4-wm>=4.4.2:${PORTSDIR}/x11-wm/xfce4-wm
+wm_BUILD_DEPENDS=		xfce4-wm>=${XFCE_VERSION}:${PORTSDIR}/x11-wm/xfce4-wm
+wm_RUN_DEPENDS=			xfce4-wm>=${XFCE_VERSION}:${PORTSDIR}/x11-wm/xfce4-wm
 
 xfdev_RUN_DEPENDS=		xfce4-dev-tools:${PORTSDIR}/devel/xfce4-dev-tools
 
