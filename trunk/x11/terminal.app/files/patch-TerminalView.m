bc61df3712675e8ad559e61b73894c497fdd361b
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2009-04-14 02:58:49 +0000

    revised fix

--- trunk/x11/terminal.app/files/patch-TerminalView.m
+++ trunk/x11/terminal.app/files/patch-TerminalView.m
@@ -1,28 +1,35 @@
 --- TerminalView.m.orig	2004-07-20 21:12:17 -0400
-+++ TerminalView.m	2009-04-13 22:52:57 -0400
-@@ -28,6 +28,13 @@
++++ TerminalView.m	2009-04-13 22:58:01 -0400
+@@ -15,32 +15,16 @@
+ #include <math.h>
+ #include <unistd.h>
+ 
+-#ifdef __NetBSD__
+-#  include <sys/types.h>
+-#  include <sys/ioctl.h>
+-#  include <termios.h>
+-#  include <pcap.h>
+-#else
+-#ifdef freebsd
+ #  include <sys/types.h>
+ #  include <sys/ioctl.h>
+ #  include <termios.h>
  #  include <libutil.h>
  #  include <pcap.h>
- #else
-+#ifdef __MidnightBSD__
-+#  include <sys/types.h>
-+#  include <sys/ioctl.h>
-+#  include <termios.h>
-+#  include <libutil.h>
-+#  include <pcap.h>
-+#else
- #  include <termio.h>
- #endif
- #endif
-@@ -38,9 +45,11 @@
+-#else
+-#  include <termio.h>
+-#endif
+-#endif
+ 
+ #include <sys/time.h>
+ #include <sys/types.h>
+ #include <unistd.h>
  #include <fcntl.h>
- #ifndef freebsd
- #ifndef __NetBSD__
-+#ifndef __MidnightBSD__
- #  include <pty.h>
- #endif
- #endif
-+#endif
+-#ifndef freebsd
+-#ifndef __NetBSD__
+-#  include <pty.h>
+-#endif
+-#endif
  
  #include <Foundation/NSBundle.h>
  #include <Foundation/NSDebug.h>
