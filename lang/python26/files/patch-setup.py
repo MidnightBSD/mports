642b9fb2836fe0fb269148385d8b4093e665e40a
Lucas Holt <luke@foolishgames.com>
2009-05-08 20:31:42 +0000

    Fix several build bugs with this port.  Seems to work with new infrastructure.
    I've got 2.5 and 2.6 installed concurrently now.

--- lang/python26/files/patch-setup.py
+++ lang/python26/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py.orig	2008-09-30 09:15:45.000000000 +0900
-+++ setup.py	2008-10-09 20:34:35.000000000 +0900
+--- setup.py.orig	Tue Nov  4 15:43:31 2008
++++ setup.py	Fri May  8 12:55:01 2009
 @@ -17,7 +17,7 @@
  from distutils.command.install_lib import install_lib
  
@@ -9,7 +9,7 @@
  
  def add_dir_to_list(dirlist, dir):
      """Add the directory 'dir' to the list 'dirlist' (at the front) if
-@@ -575,7 +575,7 @@
+@@ -576,7 +576,7 @@
  
              readline_libs = ['readline']
              if self.compiler.find_library_file(lib_dirs,
@@ -18,7 +18,7 @@
                  readline_libs.append('ncursesw')
              elif self.compiler.find_library_file(lib_dirs,
                                                   'ncurses'):
-@@ -587,7 +587,7 @@
+@@ -588,7 +588,7 @@
                                                 'termcap'):
                  readline_libs.append('termcap')
              exts.append( Extension('readline', ['readline.c'],
@@ -27,7 +27,7 @@
                                     extra_link_args=readline_extra_link_args,
                                     libraries=readline_libs) )
          else:
-@@ -687,6 +687,8 @@
+@@ -688,6 +688,8 @@
              # OpenSSL doesn't do these until 0.9.8 so we'll bring our own hash
              exts.append( Extension('_sha256', ['sha256module.c']) )
              exts.append( Extension('_sha512', ['sha512module.c']) )
@@ -36,7 +36,7 @@
  
          # Modules that provide persistent dictionary-like semantics.  You will
          # probably want to arrange for at least one of them to be available on
-@@ -987,7 +989,7 @@
+@@ -989,7 +991,7 @@
          # the more recent berkeleydb's db.h file first in the include path
          # when attempting to compile and it will fail.
          f = "/usr/include/db.h"
@@ -45,7 +45,7 @@
              data = open(f).read()
              m = re.search(r"#s*define\s+HASHVERSION\s+2\s*", data)
              if m is not None:
-@@ -1066,7 +1068,7 @@
+@@ -1068,7 +1070,7 @@
          # Curses support, requiring the System V version of curses, often
          # provided by the ncurses library.
          panel_library = 'panel'
@@ -54,7 +54,7 @@
              curses_libs = ['ncursesw']
              # Bug 1464056: If _curses.so links with ncursesw,
              # _curses_panel.so must link with panelw.
-@@ -1076,6 +1078,7 @@
+@@ -1078,6 +1080,7 @@
          elif (self.compiler.find_library_file(lib_dirs, 'ncurses')):
              curses_libs = ['ncurses']
              exts.append( Extension('_curses', ['_cursesmodule.c'],
@@ -62,7 +62,7 @@
                                     libraries = curses_libs) )
          elif (self.compiler.find_library_file(lib_dirs, 'curses')
                and platform != 'darwin'):
-@@ -1097,6 +1100,7 @@
+@@ -1099,6 +1102,7 @@
          if (module_enabled(exts, '_curses') and
              self.compiler.find_library_file(lib_dirs, panel_library)):
              exts.append( Extension('_curses_panel', ['_curses_panel.c'],
@@ -70,7 +70,25 @@
                                     libraries = [panel_library] + curses_libs) )
          else:
              missing.append('_curses_panel')
-@@ -1869,9 +1873,7 @@
+@@ -1261,7 +1265,7 @@
+                 )
+             libraries = []
+ 
+-        elif platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
++        elif platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8','midnightbsd0'):
+             # FreeBSD's P1003.1b semaphore support is very experimental
+             # and has many known problems. (as of June 2008)
+             macros = dict(                  # FreeBSD
+@@ -1317,7 +1321,7 @@
+             missing.append('linuxaudiodev')
+ 
+         if platform in ('linux2', 'freebsd4', 'freebsd5', 'freebsd6',
+-                        'freebsd7', 'freebsd8'):
++                        'freebsd7', 'freebsd8','midnightbsd0'):
+             exts.append( Extension('ossaudiodev', ['ossaudiodev.c']) )
+         else:
+             missing.append('ossaudiodev')
+@@ -1870,9 +1874,7 @@
            ext_modules=[Extension('_struct', ['_struct.c'])],
  
            # Scripts to install
