f16d168c030152a4b0baccaf446ea2da77341599
Lucas Holt <luke@foolishgames.com>
2009-05-31 20:30:03 +0000

    update to .9

--- sysutils/policykit/files/patch-src_kit_kit-spawn.c
+++ sysutils/policykit/files/patch-src_kit_kit-spawn.c
@@ -1,10 +1,13 @@
---- src/kit/kit-spawn.c.orig	2007-12-23 00:22:49.000000000 -0500
-+++ src/kit/kit-spawn.c	2007-12-23 00:22:58.000000000 -0500
-@@ -40,6 +40,7 @@
- #include <unistd.h>
- #include <fcntl.h>
- #include <errno.h>
-+#include <signal.h>
+--- src/kit/kit-spawn.c.orig	2009-03-29 20:11:24.000000000 -0400
++++ src/kit/kit-spawn.c	2009-03-29 20:11:43.000000000 -0400
+@@ -396,7 +396,10 @@ kit_spawn_sync (const char     *working_
+                         }
+                 }
  
- #include <kit/kit.h>
- #include "kit-test.h"
++again:
+                 if (waitpid (pid, out_exit_status, 0) == -1) {
++			if (errno == EINTR)
++				goto again;
+                         goto out;
+                 }
+                 pid = -1;
