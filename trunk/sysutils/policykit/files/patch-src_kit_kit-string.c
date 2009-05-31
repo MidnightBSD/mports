e487a33cf93f852ee07db7d947863470ddda443a
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2009-05-31 20:30:03 +0000

    update to .9

--- trunk/sysutils/policykit/files/patch-src_kit_kit-string.c
+++ trunk/sysutils/policykit/files/patch-src_kit_kit-string.c
@@ -1,31 +1,27 @@
---- src/kit/kit-string.c.orig	2008-04-08 15:44:50.000000000 -0400
-+++ src/kit/kit-string.c	2008-04-22 01:00:49.000000000 -0400
-@@ -113,6 +113,28 @@ out:
+--- src/kit/kit-string.c.orig	2008-05-30 17:24:44.000000000 -0400
++++ src/kit/kit-string.c	2008-07-24 01:21:34.000000000 -0400
+@@ -123,13 +123,18 @@ static char
+         if ( !s )
+                 return NULL;
  
- #else
- 
-+#ifndef HAVE_STRNDUP
-+static char
-+*strndup ( const char *s, size_t n)
-+{
-+        size_t nAvail;
-+        char *p;
-+
-+        if ( !s )
+-        if ( strlen(s) > n )
+-                nAvail = n + 1;
+-        else
+-                nAvail = strlen(s) + 1;
+-        p = malloc ( nAvail );
++        if (memchr(s, '\0', n) != NULL) {
++                nAvail = strlen(s);
++                if ( nAvail > n )
++                        nAvail = n;
++        } else {
++                nAvail = n;
++        }
++        p = malloc ( nAvail + 1 );
++        if (p == NULL)
 +                return NULL;
-+
-+        if ( strlen(s) > n )
-+                nAvail = n + 1;
-+        else
-+                nAvail = strlen(s) + 1;
-+        p = malloc ( nAvail );
-+        memcpy ( p, s, nAvail );
-+        p[nAvail - 1] = '\0';
-+
-+        return p;
-+}
-+#endif
-+
- char *
- kit_strdup (const char *s)
- {
+         memcpy ( p, s, nAvail );
+-        p[nAvail - 1] = '\0';
++        p[nAvail] = '\0';
+ 
+         return p;
+ }
