b0b31e78a0f46799b8ec75060fb6b7c9b2cf7345
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2009-12-25 17:23:13 +0000

    libunrar

--- trunk/archivers/libunrar/files/patch-extract.cpp
+++ trunk/archivers/libunrar/files/patch-extract.cpp
@@ -1,5 +1,5 @@
---- extract.cpp.orig    2009-06-30 09:18:53.000000000 +0300
-+++ extract.cpp 2009-07-09 10:02:21.000000000 +0300
+--- extract.cpp.orig	2009-06-30 09:18:53.000000000 +0300
++++ extract.cpp	2009-07-09 10:02:21.000000000 +0300
 @@ -354,6 +354,7 @@
      char CurVolName[NM];
      strcpy(CurVolName,ArcName);
