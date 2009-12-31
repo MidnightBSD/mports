081c5f4ba8c3f7ddb469208de241cd7a429a9e09
Lucas Holt <luke@foolishgames.com>
2009-12-31 23:58:21 +0000

    update to 15.7a

--- devel/cscope/files/patch-main.c
+++ devel/cscope/files/patch-main.c
@@ -1,27 +1,10 @@
---- src/main.c.orig	Sat Sep 30 01:13:00 2006
-+++ src/main.c	Thu Oct  5 15:21:30 2006
-@@ -359,7 +359,7 @@ cscope: TMPDIR to a valid directory\n");
-     /* create the temporary file names */
-     orig_umask = umask(S_IRWXG|S_IRWXO);
-     pid = getpid();
--    sprintf(tempdirpv, "%s/cscope.%d", tmpdir, pid);
-+    snprintf(tempdirpv, sizeof(tempdirpv), "%s/cscope.%d", tmpdir, pid);
-     if(mkdir(tempdirpv,S_IRWXU)) {
- 	fprintf(stderr, "\
- cscope: Could not create private temp dir %s\n",
-@@ -368,8 +368,13 @@ cscope: Could not create private temp di
+--- src/main.c.orig	2006-09-30 03:13:00.000000000 -0500
++++ src/main.c	2008-01-18 15:01:59.000000000 -0600
+@@ -379,6 +384,7 @@
      }
-     umask(orig_umask);
+     /* cleanup on the hangup signal */
+     signal(SIGHUP, myexit);
++    signal(SIGTERM, myexit);
  
--    sprintf(temp1, "%s/cscope.1", tempdirpv);
--    sprintf(temp2, "%s/cscope.2", tempdirpv);
-+    if ((strlen(tempdirpv) + strlen("/cscope.X")) > PATHLEN) {
-+ 	fprintf(stderr, "cscope: Could not create private temp files\n");
-+        myexit(1);
-+    }
-+
-+    snprintf(temp1, sizeof(temp1), "%s/cscope.1", tempdirpv);
-+    snprintf(temp2, sizeof(temp2), "%s/cscope.2", tempdirpv);
- 
-     /* if running in the foreground */
-     if (signal(SIGINT, SIG_IGN) != SIG_IGN) {
+     /* if the database path is relative and it can't be created */
+     if (reffile[0] != '/' && access(".", WRITE) != 0) {
