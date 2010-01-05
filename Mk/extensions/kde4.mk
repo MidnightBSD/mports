38df19493148d3c8b9a384dd41b3a6d79f593e0a
Lucas Holt <luke@foolishgames.com>
2010-01-05 02:01:43 +0000

    Oh the insanity!  Start the vile kde 4.1.3. to 4.3.1 process.

--- Mk/extensions/kde4.mk
+++ Mk/extensions/kde4.mk
@@ -1,10 +1,7 @@
-# $MidnightBSD: mports/Mk/extensions/kde4.mk,v 1.4 2008/12/15 19:37:29 ctriv Exp $
-#
+# $MidnightBSD$
 
 .if !defined(_POSTMKINCLUDED) && !defined(Kde_Pre_Include)
 
-# Please make sure all changes to this file are past through the maintainer.
-# Do not commit them yourself (unless of course you're the Port's Wraith ;).
 Kde_Pre_Include=	kde4.mk
 Kde_Include_MAINTAINER=	ports@MidnightBSD.org
 
@@ -27,10 +24,13 @@ Kde_Include_MAINTAINER=	ports@MidnightBSD.org
 # akonadi	- Akonadi PIM storage service
 # automoc4	- automoc4 tool
 # kdebase	- Basic KDE applications (Konqueror, Dolphin)
+# kdeexp	- experimental libraries (with non-stable ABI/API)
 # kdehier	- Provides common KDE directories
 # kdelibs	- The base set of KDE4 libraries
 # kdeprefix	- If set, port will be installed into ${KDE4_PREFIX} instead of ${LOCALBASE}
+# oxygen	- icon themes
 # pimlibs	- KDE4 PIM libraries
+# pimruntime	- KDE4 PIM runtime services
 # runtime	- More KDE applications
 # sharedmime	- share-mime-info wrapper for KDE4 ports
 # workspace	- More KDE applications (Plasma, kwin, etc.)
@@ -40,17 +40,11 @@ Kde_Include_MAINTAINER=	ports@MidnightBSD.org
 # MASTER_SITE_KDE_kde
 #		- MASTER_SITE_KDE_kde is equivalent to MASTER_SITE_KDE
 #		with :kde tag. It could be used when port needs multiple
-#		distfiles from different sites. See for details porters-handbook:
-#		http://www.freebsd.org/doc/en_US.ISO8859-1/books/porters-handbook/makefile-distfiles.html
-# KDE4_PREFIX	- The place where KDE4 ports live. Currently it is ${LOCALBASE},
-#		but this could be changed in a future.
-#
+#		distfiles from different sites.
+# KDE4_PREFIX	- The place where KDE4 ports live.
 
-KDE4_VERSION=		4.1.3
-
-#
-# KDE4 is installed into its own prefix to not conflict with KDE3
-#
+KDE4_VERSION=		4.3.1
+KDE4_BRANCH?=		stable
 KDE4_PREFIX?=		${LOCALBASE}
 
 #
@@ -67,14 +61,16 @@ MASTER_SITE_KDE_kde=	${kmaster:S@%SUBDIR%/@${ksub}/@g}
 #
 # KDE4 modules
 #
-_USE_KDE4_ALL=			akonadi automoc4 kdebase kdehier kdelibs kdeprefix \
-				pimlibs runtime sharedmime workspace
+_USE_KDE4_ALL=	akonadi automoc4 kdebase kdeexp kdehier kdelibs kdeprefix \
+		oxygen pimlibs pimruntime runtime sharedmime workspace
 
 akonadi_LIB_DEPENDS=		akonadiprotocolinternals.1:${PORTSDIR}/databases/akonadi
 
-automoc4_BUILD_DEPENDS=		${KDE4_PREFIX}/bin/automoc4:${PORTSDIR}/devel/automoc4
+automoc4_BUILD_DEPENDS=		${LOCALBASE}/bin/automoc4:${PORTSDIR}/devel/automoc4
 
 kdebase_LIB_DEPENDS=		konq.7:${PORTSDIR}/x11/kdebase4
+
+kdeexp_LIB_DEPENDS=		knotificationitem-1.1:${PORTSDIR}/x11/kdelibs4-experimental
 
 kdehier_RUN_DEPENDS=		kdehier4>=1:${PORTSDIR}/misc/kdehier4
 
@@ -82,7 +78,11 @@ kdelibs_LIB_DEPENDS=		kimproxy.5:${PORTSDIR}/x11/kdelibs4
 
 kdeprefix_PREFIX=		${KDE4_PREFIX}
 
+oxygen_RUN_DEPENDS=		${KDE4_PREFIX}/share/icons/oxygen/index.theme:${PORTSDIR}/x11-themes/kde4-icons-oxygen
+
 pimlibs_LIB_DEPENDS=		kpimutils.5:${PORTSDIR}/deskutils/kdepimlibs4
+
+pimruntime_LIB_DEPENDS=		kdepim-copy.5:${PORTSDIR}/deskutils/kdepim4-runtime
 
 runtime_BUILD_DEPENDS=		${KDE4_PREFIX}/bin/kdebugdialog:${PORTSDIR}/x11/kdebase4-runtime
 runtime_RUN_DEPENDS=		${KDE4_PREFIX}/bin/kdebugdialog:${PORTSDIR}/x11/kdebase4-runtime
@@ -93,31 +93,53 @@ sharedmime_RUN_DEPENDS=		kde4-shared-mime-info>=1:${PORTSDIR}/misc/kde4-shared-m
 workspace_LIB_DEPENDS=		kscreensaver.5:${PORTSDIR}/x11/kdebase4-workspace
 
 
+PLIST_SUB+=	KDE4_PREFIX="${KDE4_PREFIX}"
+
 #
 # Common build related stuff for kde4 ports. It's not intended for usage
 # in KDE4-dependent ports
 #
 .if defined(KDE4_BUILDENV)
+
+.if ${OSVERSION} < 3000
+BROKEN=		does not build on 0.2
+.endif
+
+.if ${KDE4_BRANCH} == "unstable"
+WITH_DEBUG=yes
+.endif
+
+.if defined(WITH_DEBUG)
+CMAKE_BUILD_TYPE=	debug
+.else
+CMAKE_BUILD_TYPE=	release
+.endif
+
+PLIST_SUB+=	KDE4_VERSION="${KDE4_VERSION}" \
+		KDE4_BUILD_TYPE="${CMAKE_BUILD_TYPE}"
+
+USE_LDCONFIG=	yes
+
 USE_CMAKE=	yes
-CMAKE_DEBUG=	yes
-CMAKE_VERBOSE=	yes
+CMAKE_SOURCE_PATH=	${WRKSRC}
+CONFIGURE_WRKSRC?=	${BUILD_WRKSRC}
+BUILD_WRKSRC?=		${WRKSRC}/build
+INSTALL_WRKSRC?=	${BUILD_WRKSRC}
 
-USE_LDCONFIG=	${KDE4_PREFIX}/lib ${KDE4_PREFIX}/lib/kde4
+post-extract:	kde-create-builddir
 
-WRKSRC=		${WRKDIR}/${PORTNAME}-${PORTVERSION}/build
-CMAKE_SOURCE_PATH=	..
+kde-create-builddir:
+	${MKDIR} ${BUILD_WRKSRC}
 
 .endif # KDE4_BUILDENV
 
+.endif #!defined(_POSTMKINCLUDED) && !defined(Kde_Pre_Include)
 
-
-.endif
+CONFLICTS+= kdelibs-3* kdeutils-3* kdebase-3*
 
 .if defined(_POSTMKINCLUDED) && !defined(Kde_Post_Include)
 
 Kde_Post_Include=	kde4.mk
-
-CONFLICTS+= kdelibs-3* kdeutils-3* kdebase-3*
 
 .for component in ${USE_KDE4}
 . if ${_USE_KDE4_ALL:M${component}}!=""
@@ -137,5 +159,4 @@ IGNORE=	cannot install: Unknown component ${component}
 . endif
 .endfor
 
-.endif # !defined(_POSTMKINCLUDED) && !defined(Kde_Pre_Include)
-
+.endif #defined(_POSTMKINCLUDED) && !defined(Kde_Post_Include)
