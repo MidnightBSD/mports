cd794d74c051efcc421fe1d9dc7b86190a98e1c2
Lucas Holt <luke@foolishgames.com>
2010-10-30 20:58:07 +0000

    update to 3.12.8.  need a newer build for seamonkey.

--- security/nss/files/patch-..::coreconf::config.mk
+++ security/nss/files/patch-..::coreconf::config.mk
@@ -1,11 +1,11 @@
---- ../coreconf/config.mk.orig	Tue Sep 19 20:00:58 2006
-+++ ../coreconf/config.mk	Tue Sep 19 20:01:06 2006
+--- ../coreconf/config.mk.orig	2010-10-30 15:50:01 -0400
++++ ../coreconf/config.mk	2010-10-30 15:50:21 -0400
 @@ -62,7 +62,7 @@
  #       one for each OS release.                                      #
  #######################################################################
  
 -TARGET_OSES = FreeBSD BSD_OS NetBSD OpenUNIX OS2 QNX Darwin BeOS OpenBSD \
-+TARGET_OSES = FreeBSD MidnightBSD BSD_OS NetBSD OpenUNIX OS2 QNX Darwin BeOS OpenBSD \
-               OpenVMS AIX
++TARGET_OSES = FreeBSD BSD_OS MidnightBSD NetBSD OpenUNIX OS2 QNX Darwin BeOS OpenBSD \
+               AIX RISCOS WINNT WIN95 WINCE
  
  ifeq (,$(filter-out $(TARGET_OSES),$(OS_TARGET)))
