086c2b91caa972567624fa2dcc6178869d3e0d3c
Lucas Holt <luke@foolishgames.com>
2010-12-27 14:09:02 +0000

    add epl and owl licenses

--- Mk/bsd.mport.mk
+++ Mk/bsd.mport.mk
@@ -1,7 +1,7 @@
 #-*- mode: makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $MidnightBSD: mports/Mk/bsd.mport.mk,v 1.170 2010/12/25 12:44:35 laffer1 Exp $
+# $MidnightBSD: mports/Mk/bsd.mport.mk,v 1.171 2010/12/27 13:58:29 laffer1 Exp $
 # $FreeBSD: ports/Mk/bsd.port.mk,v 1.540 2006/08/14 13:24:18 erwin Exp $
 #
 #   bsd.mport.mk - 2007/04/01 Chris Reinhardt
@@ -1789,8 +1789,8 @@ USE_LDCONFIG!=	${ECHO_CMD} ${LDCONFIG_DIRS} | ${SED} ${PLIST_SUB:S/$/!g/:S/^/ -e
 #
 _LICENSES= 	agpl gpl gpl2 gpl3 lgpl lgpl2.1 lgpl3 fdl1.1 fdl1.2 fdl1.3 bsd4 bsd3 bsd2 \
 		python ruby x11 guile ilm artistic artistic2 perl \
-		bdb mpl npl zlib apache2 apache1.1 apache1 apsl2 apsl1 php openldap2.8 \
-		mit modula3 cddl opera liberation sgi bzip2 infozip subversion \
+		bdb mpl epl npl zlib apache2 apache1.1 apache1 apsl2 apsl1 php openldap2.8 \
+		mit modula3 cddl opera liberation sgi bzip2 iscl infozip owl subversion \
 		publicdom unknown other agg restricted
 
 
