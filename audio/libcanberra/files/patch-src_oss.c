65b66cb294585149cfb2a8b1688b6b4afcfa9501
Lucas Holt <luke@foolishgames.com>
2011-01-15 03:57:54 +0000

    update libcanberra to 0.25

--- audio/libcanberra/files/patch-src_oss.c
+++ audio/libcanberra/files/patch-src_oss.c
@@ -1,28 +1,11 @@
---- src/oss.c.orig	2008-09-01 19:53:47.000000000 -0400
-+++ src/oss.c	2008-12-21 13:51:09.000000000 -0500
-@@ -235,7 +235,7 @@ static int open_oss(ca_context *c, struc
-     ca_return_val_if_fail(c, CA_ERROR_INVALID);
-     ca_return_val_if_fail(c->private, CA_ERROR_STATE);
-     ca_return_val_if_fail(out, CA_ERROR_INVALID);
--    ca_return_val_if_fail(ca_sound_file_get_nchannels(out->file) > 2, CA_ERROR_NOTSUPPORTED);
-+    ca_return_val_if_fail(ca_sound_file_get_nchannels(out->file) <= 2, CA_ERROR_NOTSUPPORTED);
- 
-     p = PRIVATE(c);
- 
-@@ -258,7 +258,7 @@ static int open_oss(ca_context *c, struc
-             val = AFMT_S16_NE;
-             break;
+--- src/oss.c.orig	2010-02-19 18:39:40.000000000 -0500
++++ src/oss.c	2010-03-14 15:04:16.000000000 -0400
+@@ -262,7 +262,7 @@ static int open_oss(ca_context *c, struc
+                 val = AFMT_S16_NE;
+                 break;
          case CA_SAMPLE_S16RE:
 -#if __BYTE_ORDER == __LITTLE_ENDIAN
 +#if _BYTE_ORDER == _LITTLE_ENDIAN
-             val = AFMT_S16_BE;
+                 val = AFMT_S16_BE;
  #else
-             val = AFMT_S16_LE;
-@@ -423,6 +423,7 @@ int driver_play(ca_context *c, uint32_t 
-     out->callback = cb;
-     out->userdata = userdata;
-     out->pipe_fd[0] = out->pipe_fd[1] = -1;
-+    out->pcm = -1;
- 
-     if (pipe(out->pipe_fd) < 0) {
-         ret = CA_ERROR_SYSTEM;
+                 val = AFMT_S16_LE;
