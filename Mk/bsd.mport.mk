c3fe4bd03b8d3c262478cded7889b76f8e21f345
Lucas Holt <luke@foolishgames.com>
2011-03-08 13:44:12 +0000

    Don't include xz dependancy when os version is greater than 4003 when we introduced it in the base.

--- Mk/bsd.mport.mk
+++ Mk/bsd.mport.mk
@@ -1,7 +1,7 @@
 #-*- mode: makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $MidnightBSD: mports/Mk/bsd.mport.mk,v 1.175 2010/12/30 02:25:04 laffer1 Exp $
+# $MidnightBSD: mports/Mk/bsd.mport.mk,v 1.176 2010/12/31 03:41:06 laffer1 Exp $
 # $FreeBSD: ports/Mk/bsd.port.mk,v 1.540 2006/08/14 13:24:18 erwin Exp $
 #
 #   bsd.mport.mk - 2007/04/01 Chris Reinhardt
@@ -562,7 +562,7 @@ PATCH_DEPENDS+=		${LOCALBASE}/bin/unzip:${PORTSDIR}/archivers/unzip
 .if defined(USE_ZIP)
 EXTRACT_DEPENDS+=	${LOCALBASE}/bin/unzip:${PORTSDIR}/archivers/unzip
 .endif
-.if defined(USE_XZ)
+.if defined(USE_XZ) && (${OSVERSION} > 4003)
 EXTRACT_DEPENDS+=	${LOCALBASE}/bin/xz:${PORTSDIR}/archivers/xz
 .endif
 .if defined(USE_GMAKE)
