a5cdd23243f92572b33c44978f228965f04dfec9
Lucas Holt <luke@foolishgames.com>
2011-07-08 02:38:27 +0000

    Update curl to the next major series.  (a year has passed)

--- ftp/curl/files/patch-lib::url.c
+++ ftp/curl/files/patch-lib::url.c
@@ -1,18 +1,18 @@
 Description: Different handling of signals and threads.
 Forwarded: not-needed
 Author: Peter Pentchev <roam@FreeBSD.org>
-Last-Update: 2010-03-30
+Last-Update: 2010-12-18
 
---- lib/url.c
-+++ lib/url.c
-@@ -831,6 +831,10 @@
+--- a/lib/url.c
++++ b/lib/url.c
+@@ -857,6 +857,10 @@
      data->progress.flags |= PGRS_HIDE;
      data->state.current_speed = -1; /* init to negative == impossible */
  
-+#if defined(__MidnightBSD_version)
++#if defined(__FreeBSD_version)
 +    data->set.no_signal = TRUE; /* different handling of signals and threads */
 +#endif /* __FreeBSD_version */
 +
-     /* This no longer creates a connection cache here. It is instead made on
-        the first call to curl_easy_perform() or when the handle is added to a
-        multi stack. */
+     data->wildcard.state = CURLWC_INIT;
+     data->wildcard.filelist = NULL;
+     data->set.fnmatch = ZERO_NULL;
