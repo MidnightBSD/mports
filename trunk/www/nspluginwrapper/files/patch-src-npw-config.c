5b59f786b9b35d580b0210cf2d45b7d5241616d1
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2011-08-31 02:57:37 +0000

    1.4.4 based on freebsd port

--- trunk/www/nspluginwrapper/files/patch-src-npw-config.c
+++ trunk/www/nspluginwrapper/files/patch-src-npw-config.c
@@ -1,49 +1,38 @@
---- ./src/npw-config.c.orig	Thu Jun 26 16:50:28 2008
-+++ ./src/npw-config.c	Sun Oct  5 23:16:57 2008
-@@ -119,11 +119,19 @@
- #if defined(__FreeBSD__)
+--- src/npw-config.c.orig	2011-06-30 23:18:57 -0400
++++ src/npw-config.c	2011-08-30 22:51:16 -0400
+@@ -130,11 +130,12 @@
+   if (dir == NULL) {
+ 	const char **dirs = NULL;
+ 
+-#if defined(__FreeBSD__)
++#if defined(__MidnightBSD__)
  	{
  	  static const char *freebsd_dirs[] = {
 -		"/usr/X11R6/" LIB "/browser_plugins",
 -		"/usr/X11R6/" LIB "/firefox/plugins",
 +		"/usr/local/" LIB "/browser_plugins",
 +		"/usr/local/" LIB "/firefox/plugins",
++		"/usr/local/" LIB "/seamonkey/plugins",
  	  };
  	  dirs = freebsd_dirs;
  	}
-+#elif defined(__MidnightBSD__)
-+        {
-+          static const char *freebsd_dirs[] = {
-+                "/usr/local/" LIB "/browser_plugins",
-+                "/usr/local/" LIB "/firefox/plugins",
-+          };
-+          dirs = freebsd_dirs;
-+        }
- #elif defined(__DragonFly__)
- 	{
- 	  static const char *dragonfly_dirs[] = {
-@@ -208,11 +216,20 @@
+@@ -231,12 +232,14 @@
+ 	"/usr/lib/nsbrowser/plugins",
  	"/usr/lib32/nsbrowser/plugins",				// XXX how unfortunate
  	"/usr/lib64/nsbrowser/plugins",
- #if defined(__FreeBSD__)
+-#if defined(__FreeBSD__)
 -	"/usr/X11R6/lib/browser_plugins",
 -	"/usr/X11R6/lib/firefox/plugins",
 -	"/usr/X11R6/lib/linux-mozilla/plugins",
++#if defined(__MidnightBSD__)
 +	"/usr/local/lib/browser_plugins",
 +	"/usr/local/lib/firefox/plugins",
-+	"/usr/local/lib/browser/plugins",
-+	"/usr/local/lib/linux-mozilla/plugins",
++	"/usr/local/lib/seamonkey/plugins",
  	"/usr/local/lib/npapi/linux-flashplugin",
 -	"/usr/X11R6/Adobe/Acrobat7.0/ENU/Browser/intellinux",
++	"/usr/local/lib/npapi/linux-f10-flashplugin",
 +	"/usr/local/Adobe/Acrobat7.0/ENU/Browser/intellinux",
-+#endif
-+#if defined(__MidnightBSD__)
-+        "/usr/local/lib/browser_plugins",
-+	"/usr/local/lib/browser/plugins",
-+        "/usr/local/lib/firefox/plugins",
-+        "/usr/local/lib/linux-mozilla/plugins",
-+        "/usr/local/lib/npapi/linux-flashplugin",
-+        "/usr/local/Adobe/Acrobat7.0/ENU/Browser/intellinux",
++	"/usr/local/Adobe/Reader8/ENU/Adobe/Reader8/Browser/intellinux",
  #endif
  #if defined(__DragonFly__)
  	"/usr/pkg/lib/netscape/plugins",
