4b82ac000add6e54ce23ed0e769533af63472724
Lucas Holt <luke@foolishgames.com>
2011-09-18 03:15:14 +0000

    2.6.7

--- lang/python26/files/patch-setup.py
+++ lang/python26/files/patch-setup.py
@@ -1,7 +1,7 @@
---- setup.py.orig	Tue Nov  4 15:43:31 2008
-+++ setup.py	Fri May  8 12:55:01 2009
-@@ -17,7 +17,7 @@
- from distutils.command.install_lib import install_lib
+--- setup.py.orig	2010-07-17 20:31:09.000000000 +0800
++++ setup.py	2010-09-03 08:59:33.000000000 +0800
+@@ -18,7 +18,7 @@
+ from distutils.spawn import find_executable
  
  # This global variable is used to hold the list of modules to be disabled.
 -disabled_module_list = []
@@ -9,25 +9,25 @@
  
  def add_dir_to_list(dirlist, dir):
      """Add the directory 'dir' to the list 'dirlist' (at the front) if
-@@ -576,7 +576,7 @@
- 
-             readline_libs = ['readline']
-             if self.compiler.find_library_file(lib_dirs,
--                                                 'ncursesw'):
-+                                                 'xxxncursesw'):
-                 readline_libs.append('ncursesw')
-             elif self.compiler.find_library_file(lib_dirs,
-                                                  'ncurses'):
-@@ -588,7 +588,7 @@
+@@ -627,7 +627,7 @@
+         #    curses_library = readline_termcap_library
+         # elif self.compiler.find_library_file(lib_dirs, 'ncursesw'):
+         # (...)
+-        if self.compiler.find_library_file(lib_dirs, 'ncursesw'):
++        if self.compiler.find_library_file(lib_dirs, 'xxxncursesw'):
+             curses_library = 'ncursesw'
+         elif self.compiler.find_library_file(lib_dirs, 'ncurses'):
+             curses_library = 'ncurses'
+@@ -666,7 +666,7 @@
                                                 'termcap'):
                  readline_libs.append('termcap')
              exts.append( Extension('readline', ['readline.c'],
 -                                   library_dirs=['/usr/lib/termcap'],
-+                                   library_dirs=['/usr/lib', '/usr/lib/termcap'],
++                                   library_dirs=['/usr/lib','/usr/lib/termcap'],
                                     extra_link_args=readline_extra_link_args,
                                     libraries=readline_libs) )
          else:
-@@ -688,6 +688,8 @@
+@@ -766,6 +766,8 @@
              # OpenSSL doesn't do these until 0.9.8 so we'll bring our own hash
              exts.append( Extension('_sha256', ['sha256module.c']) )
              exts.append( Extension('_sha512', ['sha512module.c']) )
@@ -36,59 +36,57 @@
  
          # Modules that provide persistent dictionary-like semantics.  You will
          # probably want to arrange for at least one of them to be available on
-@@ -989,7 +991,7 @@
-         # the more recent berkeleydb's db.h file first in the include path
-         # when attempting to compile and it will fail.
-         f = "/usr/include/db.h"
+@@ -1101,7 +1103,7 @@
+                 sysroot = macosx_sdk_root()
+                 f = os.path.join(sysroot, f[1:])
+ 
 -        if os.path.exists(f) and not db_incs:
 +        if os.path.exists(f):
              data = open(f).read()
              m = re.search(r"#s*define\s+HASHVERSION\s+2\s*", data)
              if m is not None:
-@@ -1068,7 +1070,7 @@
-         # Curses support, requiring the System V version of curses, often
+@@ -1193,12 +1195,13 @@
          # provided by the ncurses library.
          panel_library = 'panel'
--        if (self.compiler.find_library_file(lib_dirs, 'ncursesw')):
-+        if (self.compiler.find_library_file(lib_dirs, 'xxxncursesw')):
-             curses_libs = ['ncursesw']
-             # Bug 1464056: If _curses.so links with ncursesw,
-             # _curses_panel.so must link with panelw.
-@@ -1078,6 +1080,7 @@
-         elif (self.compiler.find_library_file(lib_dirs, 'ncurses')):
-             curses_libs = ['ncurses']
+         if curses_library.startswith('ncurses'):
+-            if curses_library == 'ncursesw':
++            if curses_library == 'xxxncursesw':
+                 # Bug 1464056: If _curses.so links with ncursesw,
+                 # _curses_panel.so must link with panelw.
+                 panel_library = 'panelw'
+             curses_libs = [curses_library]
              exts.append( Extension('_curses', ['_cursesmodule.c'],
 +                                   library_dirs = ['/usr/lib'],
                                     libraries = curses_libs) )
-         elif (self.compiler.find_library_file(lib_dirs, 'curses')
-               and platform != 'darwin'):
-@@ -1099,6 +1102,7 @@
-         if (module_enabled(exts, '_curses') and
-             self.compiler.find_library_file(lib_dirs, panel_library)):
-             exts.append( Extension('_curses_panel', ['_curses_panel.c'],
+         elif curses_library == 'curses' and platform != 'darwin':
+                 # OSX has an old Berkeley curses, not good enough for
+@@ -1211,6 +1214,7 @@
+                 curses_libs = ['curses']
+ 
+             exts.append( Extension('_curses', ['_cursesmodule.c'],
 +                                   library_dirs = ['/usr/lib'],
-                                    libraries = [panel_library] + curses_libs) )
+                                    libraries = curses_libs) )
          else:
-             missing.append('_curses_panel')
-@@ -1261,7 +1265,7 @@
+             missing.append('_curses')
+@@ -1381,7 +1385,7 @@
                  )
              libraries = []
  
 -        elif platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
-+        elif platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8','midnightbsd0'):
++        elif platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8', 'midnightbsd0'):
              # FreeBSD's P1003.1b semaphore support is very experimental
              # and has many known problems. (as of June 2008)
              macros = dict(                  # FreeBSD
-@@ -1317,7 +1321,7 @@
+@@ -1450,7 +1454,7 @@
              missing.append('linuxaudiodev')
  
          if platform in ('linux2', 'freebsd4', 'freebsd5', 'freebsd6',
 -                        'freebsd7', 'freebsd8'):
-+                        'freebsd7', 'freebsd8','midnightbsd0'):
++                        'freebsd7', 'freebsd8', 'midnightbsd0'):
              exts.append( Extension('ossaudiodev', ['ossaudiodev.c']) )
          else:
              missing.append('ossaudiodev')
-@@ -1870,9 +1874,7 @@
+@@ -2026,9 +2030,7 @@
            ext_modules=[Extension('_struct', ['_struct.c'])],
  
            # Scripts to install
