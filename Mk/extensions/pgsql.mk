0077d21a74f6cfbfb426ad264df984218b17be5a
Lucas Holt <luke@foolishgames.com>
2012-01-29 16:19:58 +0000

    8.3 is ancient too.. let's drop it

--- Mk/extensions/pgsql.mk
+++ Mk/extensions/pgsql.mk
@@ -1,4 +1,4 @@
-# $MidnightBSD: mports/Mk/extensions/pgsql.mk,v 1.4 2011/05/27 22:23:30 laffer1 Exp $ 
+# $MidnightBSD: mports/Mk/extensions/pgsql.mk,v 1.5 2012/01/29 16:18:19 laffer1 Exp $ 
 # $FreeBSD: ports/Mk/bsd.database.mk,v 1.14 2006/07/05 02:18:08 linimon Exp $
 
 .if defined(_POSTMKINCLUDED) && !defined(Pgsql_Post_Include)
@@ -27,7 +27,6 @@ Pgsql_Include_MAINTAINER=	ports@MidnightBSD.org
 
 
 DEFAULT_PGSQL_VER?=	84
-PGSQL83_LIBVER=		5
 PGSQL84_LIBVER=		5
 PGSQL90_LIBVER=		5
 
