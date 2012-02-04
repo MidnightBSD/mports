44b5426cd7d93a4ea2ee8ca6a92c0667436fa1a5
Lucas Holt <luke@foolishgames.com>
2012-02-04 05:21:59 +0000

    get rid of legacy pthread option, cleanup

--- Mk/extensions/cmake.mk
+++ Mk/extensions/cmake.mk
@@ -1,4 +1,4 @@
-# $MidnightBSD: mports/Mk/extensions/cmake.mk,v 1.4 2010/03/13 23:43:11 laffer1 Exp $
+# $MidnightBSD: mports/Mk/extensions/cmake.mk,v 1.5 2010/06/26 17:44:47 laffer1 Exp $
 #
 
 .if !defined(_POSTMKINCLUDED) && !defined(Cmake_Pre_Include)
@@ -7,24 +7,30 @@ Cmake_Pre_Include = cmake.mk
 
 # USE_CMAKE			- If set, this port uses cmake.
 #
-# CMAKE_ENV			- Environment passed to cmake.
-#					Default: ${CONFIGURE_ENV}
+# CMAKE_ENV		- Environment passed to cmake.
+#				Default: ${CONFIGURE_ENV}
 # CMAKE_ARGS		- Arguments passed to cmake
-#					Default: see below
-# CMAKE_USE_PTHREAD	- Instruct cmake to use pthreads when
-#					compiling/linking
-#					Default: not set
-# CMAKE_BUILD_TYPE	- Type of build (cmake predefined build types),
-#					affects on CFLAGS and thus should not be set.
-#					Default: none (which respects CFLAGS)
+#				Default: see below
+# CMAKE_BUILD_TYPE	- Type of build (cmake predefined build types).
+#				Projects may have their own build profiles.
+#				CMake supports the following types: Debug,
+#				Release, RelWithDebInfo and MinSizeRel.
+#				Debug and Release profiles respect system
+#				CFLAGS, RelWithDebInfo and MinSizeRel will set
+#				CFLAGS to "-O2 -g" and "-Os -DNDEBUG".
+#				Default: Release, if WITH_DEBUG is not set,
+#				Debug otherwise
 # CMAKE_VERBOSE		- Verbose build
-#					Default: not set
+#				Default: not set
 # CMAKE_OUTSOURCE	- Instruct to perform an out-of-source build
-# 					Default: not set
+# 				Default: not set
 # CMAKE_SOURCE_PATH	- Path to sourcedir for cmake
-#					Default: ${WRKSRC}
+#				Default: ${WRKSRC}
 # CMAKE_INSTALL_PREFIX	- prefix for cmake to use for installation.
-#					Default: ${PREFIX}
+#				Default: ${PREFIX}
+#
+#
+# $FreeBSD: ports/Mk/bsd.cmake.mk,v 1.13 2011/11/10 19:50:26 rakuco Exp $
 
 CMAKE_MAINTAINER=  ports@MidnightBSD.org
 
@@ -49,7 +55,14 @@ CMAKE_ENV?=		${CONFIGURE_ENV}
 CMAKE_ARGS+=	-DCMAKE_C_COMPILER:STRING="${CC}" \
 				-DCMAKE_CXX_COMPILER:STRING="${CXX}" \
 				-DCMAKE_C_FLAGS:STRING="${CFLAGS}" \
+				-DCMAKE_C_FLAGS_DEBUG:STRING="${CFLAGS}" \
+				-DCMAKE_C_FLAGS_RELEASE:STRING="${CFLAGS}" \
 				-DCMAKE_CXX_FLAGS:STRING="${CXXFLAGS}" \
+				-DCMAKE_CXX_FLAGS_DEBUG:STRING="${CXXFLAGS}" \
+				-DCMAKE_CXX_FLAGS_RELEASE:STRING="${CXXFLAGS}" \
+				-DCMAKE_EXE_LINKER_FLAGS:STRING="${LDFLAGS}" \
+				-DCMAKE_MODULE_LINKER_FLAGS:STRING="${LDFLAGS}" \
+				-DCMAKE_SHARED_LINKER_FLAGS:STRING="${LDFLAGS}" \
 				-DCMAKE_INSTALL_PREFIX:PATH="${CMAKE_INSTALL_PREFIX}" \
 				-DCMAKE_BUILD_TYPE:STRING="${CMAKE_BUILD_TYPE}" \
 				-DTHREADS_HAVE_PTHREAD_ARG:BOOL=YES
@@ -64,26 +77,18 @@ BUILD_WRKSRC=		${CONFIGURE_WRKSRC}
 INSTALL_WRKSRC=		${CONFIGURE_WRKSRC}
 .endif
 CMAKE_INSTALL_PREFIX?=	${PREFIX}
-CMAKE_BUILD_TYPE?=	#none
 
-#
-# Instruct cmake to compile/link with pthreads
-#
-.if defined(CMAKE_USE_PTHREAD)
-CFLAGS+=		${PTHREAD_CFLAGS}
-CXXFLAGS+=		${PTHREAD_CFLAGS}
-
-CMAKE_ARGS+=	-DCMAKE_THREAD_LIBS:STRING="${PTHREAD_LIBS}" \
-				-DCMAKE_USE_PTHREADS:BOOL=ON \
-				-DCMAKE_EXE_LINKER_FLAGS:STRING="${PTHREAD_LIBS}"
+.if defined(WITH_DEBUG)
+CMAKE_BUILD_TYPE?=	Debug
+.else
+CMAKE_BUILD_TYPE?=	Release
 .endif
 
-#
-# Strip binaries
-#
-.if !defined(WITH_DEBUG)
+.if defined(STRIP) && ${STRIP} != ""
 INSTALL_TARGET?=	install/strip
 .endif
+
+PLIST_SUB+=	CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:L}"
 
 #
 # Force makefile verbosity if needed
