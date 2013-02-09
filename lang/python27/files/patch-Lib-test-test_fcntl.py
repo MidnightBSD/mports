bc8fa0f1c57656d33dfb4c12a9353d9475ce575c
Lucas Holt <luke@foolishgames.com>
2013-02-09 03:02:21 +0000

    2.7.3 part 3

--- lang/python27/files/patch-Lib-test-test_fcntl.py
+++ lang/python27/files/patch-Lib-test-test_fcntl.py
@@ -1,11 +1,11 @@
---- Lib/test/test_fcntl.py.orig	2010-08-14 16:56:31.290525837 +0800
-+++ Lib/test/test_fcntl.py	2010-08-14 16:56:39.007508167 +0800
-@@ -30,7 +30,7 @@
-     if sys.platform in ('netbsd1', 'netbsd2', 'netbsd3',
-                         'Darwin1.2', 'darwin',
-                         'freebsd2', 'freebsd3', 'freebsd4', 'freebsd5',
--                        'freebsd6', 'freebsd7', 'freebsd8',
-+                        'freebsd6', 'freebsd7', 'freebsd8', 'freebsd9', 'midnightbsd0'
-                         'bsdos2', 'bsdos3', 'bsdos4',
-                         'openbsd', 'openbsd2', 'openbsd3', 'openbsd4'):
+--- Lib/test/test_fcntl.py.orig	2012-04-09 23:07:31.000000000 +0000
++++ Lib/test/test_fcntl.py	2013-02-08 21:51:55.000000000 +0000
+@@ -27,7 +27,7 @@
+         else:
+             start_len = "qq"
+ 
+-    if (sys.platform.startswith(('netbsd', 'freebsd', 'openbsd', 'bsdos'))
++    if (sys.platform.startswith(('netbsd', 'midnightbsd', 'freebsd', 'openbsd', 'bsdos'))
+         or sys.platform == 'darwin'):
          if struct.calcsize('l') == 8:
+             off_t = 'l'
