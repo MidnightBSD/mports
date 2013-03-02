8ecca3a6e7dc9800d8307cd3a215fc2d0edad220
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2013-03-02 19:48:57 +0000

    Add support for percona and mariadb.  We're going to add ports for those in the future.

--- trunk/Mk/extensions/mysql.mk
+++ trunk/Mk/extensions/mysql.mk
@@ -1,9 +1,7 @@
-# -*- mode: Makefile; tab-width: 4; -*-
+# -*- tab-width: 4; -*-
 # ex: ts=4
 #
-# $MidnightBSD: mports/Mk/extensions/mysql.mk,v 1.6 2011/05/27 02:02:43 laffer1 Exp $ 
-# $FreeBSD: ports/Mk/bsd.database.mk,v 1.14 2006/07/05 02:18:08 linimon Exp $
-#
+# $MidnightBSD: mports/Mk/extensions/mysql.mk,v 1.7 2011/05/27 02:11:44 laffer1 Exp $ 
 
 .if defined(_POSTMKINCLUDED) && !defined(Mysql_Post_Include)
 
@@ -12,40 +10,58 @@ Mysql_Include_MAINTAINER=	ports@MidnightBSD.org
 
 
 ##
-# USE_MYSQL		- Add MySQL client dependency.
+# USE_MYSQL		- Add MySQL (client/server/embedded) dependency (default:
+#				  client).
 #				  If no version is given (by the maintainer via the port or
 #				  by the user via defined variable), try to find the
 #				  currently installed version.  Fall back to default if
-#				  necessary (MySQL5.1 = 51).
+#				  necessary (MySQL-5.5 = 55).
 # DEFAULT_MYSQL_VER
-#				- MySQL default version. Can be overriden within a port.
+#				- MySQL default version.  Can be overridden within a port.
 #				  Default: 55.
 # WANT_MYSQL_VER
-#				- Maintainer can set an arbitrary version of MySQL by using it.
+#				- Maintainer can set an arbitrary version of MySQL to always
+#				  build this port with (overrides WITH_MYSQL_VER).
 # IGNORE_WITH_MYSQL
-#				- This variable can be defined if the ports doesn't support
-#				  one or more version of MySQL.
+#				- This variable can be defined if the ports does not support
+#				  one or more versions of MySQL.
 # WITH_MYSQL_VER
 #				- User defined variable to set MySQL version.
 # MYSQL_VER
 #				- Detected MySQL version.
 ##
 
+.if defined(USE_MYSQL)
 DEFAULT_MYSQL_VER?=	55
 # MySQL client version currently supported.
 MYSQL41_LIBVER=		14
 MYSQL50_LIBVER=		15
 MYSQL51_LIBVER=		16
+MYSQL53m_LIBVER=	16
 MYSQL55_LIBVER=		18
+MYSQL55m_LIBVER=	18
+MYSQL55p_LIBVER=	18
+MYSQL56_LIBVER=		18
+MYSQL56p_LIBVER=	18
 
 # Setting/finding MySQL version we want.
 .if exists(${LOCALBASE}/bin/mysql) && !defined(PACKAGE_BUILDING)
-_MYSQL_VER!=	${LOCALBASE}/bin/mysql --version | ${SED} -e 's/.*Distrib \([0-9]\)\.\([0-9]*\).*/\1\2/'
+_MYSQL!=	${LOCALBASE}/bin/mysql --version | ${SED} -e 's/.*Distrib \([0-9]\)\.\([0-9]*\).*/\1\2/'
+_PERCONA!=	${LOCALBASE}/bin/mysql --version | ${GREP} Percona | wc -l
+_MARIADB!=	${LOCALBASE}/bin/mysql --version | ${GREP} MariaDB | wc -l
+
+.if ${_PERCONA} == 1
+_MYSQL_VER=	${_MYSQL}p
+.elif ${_MARIADB} == 1
+_MYSQL_VER=	${_MYSQL}m
+.else
+_MYSQL_VER=	${_MYSQL}
+.endif
 .endif
 
 .if defined(WANT_MYSQL_VER)
 .if defined(WITH_MYSQL_VER) && ${WITH_MYSQL_VER} != ${WANT_MYSQL_VER}
-IGNORE=		cannot install: the port wants mysql${WANT_MYSQL_VER}-client and you try to install mysql${WITH_MYSQL_VER}-client.
+IGNORE=		cannot install: the port wants mysql${WANT_MYSQL_VER}-client and you try to install mysql${WITH_MYSQL_VER}-client
 .endif
 MYSQL_VER=	${WANT_MYSQL_VER}
 .elif defined(WITH_MYSQL_VER)
@@ -60,8 +76,25 @@ MYSQL_VER=	${DEFAULT_MYSQL_VER}
 
 .if defined(_MYSQL_VER)
 .if ${_MYSQL_VER} != ${MYSQL_VER}
-IGNORE=	cannot install: MySQL versions mismatch: mysql${_MYSQL_VER}-client is installed and wanted version is mysql${MYSQL_VER}-client
+IGNORE=		cannot install: MySQL versions mismatch: mysql${_MYSQL_VER}-client is installed and wanted version is mysql${MYSQL_VER}-client
 .endif
+.endif
+
+.if (${MYSQL_VER} == "53m")
+_MYSQL_CLIENT=	databases/mariadb-client
+_MYSQL_SERVER=	databases/mariadb-server
+.elif (${MYSQL_VER} == "55m")
+_MYSQL_CLIENT=	databases/mariadb55-client
+_MYSQL_SERVER=	databases/mariadb55-server
+.elif (${MYSQL_VER} == "55p")
+_MYSQL_CLIENT=	databases/percona55-client
+_MYSQL_SERVER=	databases/percona55-server
+.elif (${MYSQL_VER} == "56p")
+_MYSQL_CLIENT=	databases/percona56-client
+_MYSQL_SERVER=	databases/percona56-server
+.else
+_MYSQL_CLIENT=	databases/mysql${MYSQL_VER}-client
+_MYSQL_SERVER=	databases/mysql${MYSQL_VER}-server
 .endif
 
 .if (${USE_MYSQL} == "embedded")
@@ -70,27 +103,25 @@ IGNORE_WITH_MYSQL=	41
 
 # And now we are checking if we can use it
 .if defined(MYSQL${MYSQL_VER}_LIBVER)
-# compatability shim
-.if defined(BROKEN_WITH_MYSQL)
-IGNORE_WITH_MYSQL=${BROKEN_WITH_MYSQL}
-.endif
 .if defined(IGNORE_WITH_MYSQL)
 .	for VER in ${IGNORE_WITH_MYSQL}
 .		if (${MYSQL_VER} == "${VER}")
-IGNORE=		cannot install: doesn't work with MySQL version : ${MYSQL_VER} (Doesn't support MySQL ${IGNORE_WITH_MYSQL})
+IGNORE=		cannot install: does not work with MySQL version ${MYSQL_VER} (MySQL ${IGNORE_WITH_MYSQL} not supported)
 .		endif
 .	endfor
 .endif # IGNORE_WITH_MYSQL
 .if (${USE_MYSQL} == "server" || ${USE_MYSQL} == "embedded")
-RUN_DEPENDS+=	${LOCALBASE}/libexec/mysqld:${PORTSDIR}/databases/mysql${MYSQL_VER}-server
+RUN_DEPENDS+=	${LOCALBASE}/libexec/mysqld:${PORTSDIR}/${_MYSQL_SERVER}
 .if (${USE_MYSQL} == "embedded")
-BUILD_DEPENDS+=	${LOCALBASE}/libexec/mysqld:${PORTSDIR}/databases/mysql${MYSQL_VER}-server
+BUILD_DEPENDS+=	${LOCALBASE}/lib/mysql/libmysqld.a:${PORTSDIR}/${_MYSQL_SERVER}
 .endif
 .else
-LIB_DEPENDS+=	mysqlclient.${MYSQL${MYSQL_VER}_LIBVER}:${PORTSDIR}/databases/mysql${MYSQL_VER}-client
+LIB_DEPENDS+=	mysqlclient.${MYSQL${MYSQL_VER}_LIBVER}:${PORTSDIR}/${_MYSQL_CLIENT}
 .endif
 .else
 IGNORE=		cannot install: unknown MySQL version: ${MYSQL_VER}
 .endif # Check for correct libs
+
+.endif # USE_MYSQL
 
 .endif # defined(_POSTMKINCLUDED) && !defined(Mysql_Post_Include)
