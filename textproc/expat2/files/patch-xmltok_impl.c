2af5f8a1e244c367a6a8ae15092f07c76a14c003
Lucas Holt <luke@foolishgames.com>
2013-03-23 16:44:08 +0000

    bug fixes

--- textproc/expat2/files/patch-xmltok_impl.c
+++ textproc/expat2/files/patch-xmltok_impl.c
@@ -1,5 +1,5 @@
---- lib/xmltok_impl.c.orig	2009-12-13 20:16:41 -0500
-+++ lib/xmltok_impl.c	2009-12-13 20:16:59 -0500
+--- lib/xmltok_impl.c.orig	2009-12-08 10:16:58.047943029 +0900
++++ lib/xmltok_impl.c	2009-12-08 10:17:13.228143919 +0900
 @@ -1744,7 +1744,7 @@
                         const char *end,
                         POSITION *pos)
