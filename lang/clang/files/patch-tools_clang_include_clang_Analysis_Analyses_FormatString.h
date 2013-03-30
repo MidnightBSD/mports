d0011dce63760b50ed75a080731c24af2f879ed1
Lucas Holt <luke@foolishgames.com>
2013-03-30 13:26:34 +0000

    3.2

--- lang/clang/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
+++ lang/clang/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
@@ -1,16 +1,24 @@
 
-$FreeBSD: ports/lang/clang/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h,v 1.1 2010/10/08 14:00:31 brooks Exp $
+$FreeBSD$
 
 --- tools/clang/include/clang/Analysis/Analyses/FormatString.h.orig
 +++ tools/clang/include/clang/Analysis/Analyses/FormatString.h
-@@ -139,6 +139,10 @@
-     SArg,
-     
+@@ -117,7 +117,7 @@
+       // C99 conversion specifiers.
+     cArg,
+     dArg,
+-    DArg, // Apple extension
++    DArg, // Apple and FreeBSD extension
+     iArg,
+     IntArgBeg = dArg, IntArgEnd = iArg,
+ 
+@@ -148,6 +148,9 @@
+ 
      // ** Printf-specific **
-+
+ 
 +    // FreeBSD specific specifiers
 +    bArg,
-+    DArg,
-   
++
      // Objective-C specific specifiers.
      ObjCObjArg,  // '@'
+     ObjCBeg = ObjCObjArg, ObjCEnd = ObjCObjArg,
