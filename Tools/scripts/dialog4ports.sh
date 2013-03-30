dd55f4b6c55be86fb14fc1cf169ef19d5d309a44
Lucas Holt <luke@foolishgames.com>
2013-03-30 15:42:41 +0000

    clear the environment so this actually works

--- Tools/scripts/dialog4ports.sh
+++ Tools/scripts/dialog4ports.sh
@@ -1,5 +1,5 @@
-#! /bin/sh
-# $MidnightBSD$
+#!/bin/sh
+# $MidnightBSD: mports/Tools/scripts/dialog4ports.sh,v 1.1 2013/03/30 12:05:44 laffer1 Exp $
 set -e
 
 if [ -z "${DIALOG4PORTS}" -o -z "${PORTSDIR}" -o -z "${MAKE}" ]; then
@@ -24,14 +24,14 @@ if ! [ -e $DIALOG4PORTS ]; then
 		DIALOG4PORTS=$(${MAKE} -C ${PORTSDIR}/${DIALOGPORT} -V DIALOG4PORTS)
 		if ! [ -e "${DIALOG4PORTS}" ]; then
 			echo "===> Building ${DIALOGNAME} as it is required for the config dialog"
-			${MAKE} -C ${PORTSDIR}/${DIALOGPORT} -D NO_DIALOG clean build
+			env - ${MAKE} -C ${PORTSDIR}/${DIALOGPORT} -D NO_DIALOG clean build
 		fi
 	else
 		# Build+install through su-install as normal
 		echo "===> Building/installing ${DIALOGNAME} as it is required for the config dialog"
-		${MAKE} -C ${PORTSDIR}/${DIALOGPORT} -D NO_DIALOG clean install
+		cd ${PORTSDIR}/${DIALOGPORT} && env - ${MAKE} -DNO_DIALOG clean install
 		# Need to clean again as it can't run twice in 1 call above
-		${MAKE} -C ${PORTSDIR}/${DIALOGPORT} -D NO_DIALOG clean
+		cd ${PORTSDIR}/${DIALOGPORT} && env - ${MAKE} clean
 	fi
 fi
 
