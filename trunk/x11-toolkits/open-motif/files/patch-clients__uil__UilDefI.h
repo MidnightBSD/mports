8ef2070ec3f4a1ba077714dd3b2f7167603c836e
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2013-06-13 11:16:58 +0000

    2.3.4

--- trunk/x11-toolkits/open-motif/files/patch-clients__uil__UilDefI.h
+++ trunk/x11-toolkits/open-motif/files/patch-clients__uil__UilDefI.h
@@ -1,11 +1,11 @@
---- clients/uil/UilDefI.h.orig	2011-01-16 01:16:45.000000000 +0300
-+++ clients/uil/UilDefI.h	2011-01-16 01:17:35.000000000 +0300
-@@ -283,7 +283,7 @@
+--- clients/uil/UilDefI.h.orig	2012-10-22 07:50:39.000000000 -0700
++++ clients/uil/UilDefI.h	2012-10-26 15:18:31.064731493 -0700
+@@ -270,7 +270,7 @@
  #include "UilIODef.h"
  #include "UilDiagDef.h"
  #include "UilSarDef.h"
--#if defined(linux)
-+#if defined(linux) || defined(__FreeBSD__)
+-#if defined(linux) || defined(__APPLE__)
++#if defined(linux) || defined(__APPLE__) || defined(__FreeBSD__)
  #define YYSTYPE yystype
  #endif
  #include "UilLexPars.h"
