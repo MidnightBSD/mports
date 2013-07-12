a56af1ed65e45e60fc627c1666eb4cd65bab1746
Lucas Holt <luke@foolishgames.com>
2013-07-12 23:14:58 +0000

    0.9.23

--- audio/pulseaudio/files/patch-src_pulsecore_svolume_mmx.c
+++ audio/pulseaudio/files/patch-src_pulsecore_svolume_mmx.c
@@ -1,20 +1,26 @@
---- src/pulsecore/svolume_mmx.c.orig	2010-02-20 13:53:25.000000000 +0100
-+++ src/pulsecore/svolume_mmx.c	2010-02-20 13:54:22.000000000 +0100
-@@ -153,7 +153,7 @@ pa_volume_s16ne_mmx (int16_t *samples, i
+--- src/pulsecore/svolume_mmx.c.orig	2011-06-23 20:18:54.484125000 +0000
++++ src/pulsecore/svolume_mmx.c	2013-04-18 09:22:42.000000000 +0000
+@@ -154,9 +154,9 @@
          "6:                             \n\t"
          " emms                          \n\t"
  
 -        : "+r" (samples), "+r" (volumes), "+r" (length), "=D" ((pa_reg_x86)channel), "=&r" (temp)
 +        : "+r" (samples), "+r" (volumes), "+r" (length), "=D" (channel), "=&r" (temp)
-         : "rm" ((pa_reg_x86)channels)
-         : "cc"
-     );
-@@ -229,7 +229,7 @@ pa_volume_s16re_mmx (int16_t *samples, i
+ #if defined (__i386__)
+-        : "m" ((pa_reg_x86)channels)
++        : "m" (channels)
+ #else
+         : "r" ((pa_reg_x86)channels)
+ #endif
+@@ -235,9 +235,9 @@
          "6:                             \n\t"
          " emms                          \n\t"
  
 -        : "+r" (samples), "+r" (volumes), "+r" (length), "=D" ((pa_reg_x86)channel), "=&r" (temp)
 +        : "+r" (samples), "+r" (volumes), "+r" (length), "=D" (channel), "=&r" (temp)
-         : "rm" ((pa_reg_x86)channels)
-         : "cc"
-     );
+ #if defined (__i386__)
+-        : "m" ((pa_reg_x86)channels)
++        : "m" (channels)
+ #else
+         : "r" ((pa_reg_x86)channels)
+ #endif
