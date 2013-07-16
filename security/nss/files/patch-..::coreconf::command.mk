308fdf2d474e7ba0ee4e782645606d93cf9badb7
Lucas Holt <luke@foolishgames.com>
2013-07-16 02:24:13 +0000

    3.14.3

--- security/nss/files/patch-..::coreconf::command.mk
+++ security/nss/files/patch-..::coreconf::command.mk
@@ -1,5 +1,3 @@
-$FreeBSD: ports/security/nss/files/patch-..::coreconf::command.mk,v 1.2 2006/01/24 23:37:03 marcus Exp $
-
 --- ../coreconf/command.mk	Mon Oct 10 19:46:12 2005
 +++ ../coreconf/command.mk	Wed Jan 18 17:23:28 2006
 @@ -46,5 +46,5 @@
