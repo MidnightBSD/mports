d215e9f74ccc607f95416207b70e80a8d4c8025e
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2013-07-16 02:24:13 +0000

    3.14.3

--- trunk/security/nss/files/patch-..::coreconf::config.mk
+++ trunk/security/nss/files/patch-..::coreconf::config.mk
@@ -1,11 +1,11 @@
---- ../coreconf/config.mk.orig	2010-10-30 15:50:01 -0400
-+++ ../coreconf/config.mk	2010-10-30 15:50:21 -0400
-@@ -62,7 +62,7 @@
+--- ../../security/coreconf/config.mk.orig	2013-07-15 21:55:47.000000000 -0400
++++ ../../security/coreconf/config.mk	2013-07-15 21:56:08.000000000 -0400
+@@ -30,7 +30,7 @@
  #       one for each OS release.                                      #
  #######################################################################
  
 -TARGET_OSES = FreeBSD BSD_OS NetBSD OpenUNIX OS2 QNX Darwin BeOS OpenBSD \
-+TARGET_OSES = FreeBSD BSD_OS MidnightBSD NetBSD OpenUNIX OS2 QNX Darwin BeOS OpenBSD \
-               AIX RISCOS WINNT WIN95 WINCE
++TARGET_OSES = MidnightBSD FreeBSD BSD_OS NetBSD OpenUNIX OS2 QNX Darwin BeOS OpenBSD \
+               AIX RISCOS WINNT WIN95 Linux Android
  
  ifeq (,$(filter-out $(TARGET_OSES),$(OS_TARGET)))
