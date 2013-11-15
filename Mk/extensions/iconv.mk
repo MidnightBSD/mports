57e7778a37ad7e5c0b06c6b033e784e7de274036
Lucas Holt <luke@foolishgames.com>
2013-11-15 00:20:24 +0000

    update iconv handling

--- Mk/extensions/iconv.mk
+++ Mk/extensions/iconv.mk
@@ -3,17 +3,41 @@
 # handle dependency on the iconv port
 #
 # Feature:	iconv
-# Usage:	USES=iconv
-# Valid ARGS:	does not require args
+# Usage:	USES=iconv or USES=iconv:ARGS
+# Valid ARGS:	lib (default, implicit), build, patch
 #
 #
 .if !defined(_INCLUDE_USES_ICONV_MK)
 _INCLUDE_USES_ICONV_MK=	yes
 
-.if defined(iconv_ARGS)
-IGNORE=	USES=iconv does not require args
+.if !defined(iconv_ARGS)
+iconv_ARGS=     lib
 .endif
 
+.if !exists(/usr/include/iconv.h)
+
+ICONV_CMD=	${LOCALBASE}/bin/iconv
+ICONV_LIB=	-liconv
+ICONV_PREFIX=	${LOCALBASE}
+ICONV_CONFIGURE_ARG=	--with-libiconv-prefix=${LOCALBASE}
+ICONV_CONFIGURE_BASE=	--with-libiconv=${LOCALBASE}
+
+.if ${iconv_ARGS} == "lib"
 LIB_DEPENDS+=	iconv.3:${PORTSDIR}/converters/libiconv
+.elif ${iconv_ARGS} == "build"
+BUILD_DEPENDS+=	${ICONV_CMD}:${PORTSDIR}/converters/libiconv
+.elif ${iconv_ARGS} == "patch"
+PATCH_DEPENDS+=	${ICONV_CMD}:${PORTSDIR}/converters/libiconv
+.endif
+
+.else
+
+ICONV_CMD=	/usr/bin/iconv
+ICONV_LIB=
+ICONV_PREFIX=	/usr
+ICONV_CONFIGURE_ARG=
+ICONV_CONFIGURE_BASE=
+
+.endif
 
 .endif
