69e1f444c6d0a1587b8055704a9f8ab68bd7b477
Lucas Holt <luke@foolishgames.com>
2013-12-07 21:08:59 +0000

    introduce compiler.mk from FreeBSD ports.

--- Mk/bsd.mport.mk
+++ Mk/bsd.mport.mk
@@ -333,7 +333,7 @@ _LOAD_${EXT:U}_EXT=	yes
 # This is the order that we used before the extensions where refactored. 
 # in the future if things could be fixed to work when loaded alphabetacally, then
 # we could go back to the above approach.
-_ALL_EXT=	charsetfix pathfix pkgconfig linux_rpm linux_apps xorg fortran \
+_ALL_EXT=	charsetfix pathfix pkgconfig compiler linux_rpm linux_apps xorg fortran \
 		gcc fmake gmake bison local perl5 openssl \
 		emacs gnustep php python java ruby tcl apache kde qt \
 		autotools gnome lua wx gstreamer sdl xfce kde4 cmake mysql \
