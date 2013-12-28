d7cc17196ab88852746f7e5da0c2ac27380e3f58
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2013-12-28 14:20:01 +0000

    update netpbm

--- trunk/graphics/netpbm/files/patch-pnmtopng.c
+++ trunk/graphics/netpbm/files/patch-pnmtopng.c
@@ -1,6 +1,26 @@
---- converter/other/pnmtopng.c.orig	2007-07-04 05:26:21.000000000 +0200
-+++ converter/other/pnmtopng.c	2010-03-28 12:17:40.000000000 +0200
-@@ -1863,7 +1863,7 @@
+--- converter/other/pnmtopng.c.orig	2011-09-25 20:51:44.000000000 +0200
++++ converter/other/pnmtopng.c	2012-04-23 20:23:10.000000000 +0200
+@@ -61,7 +61,9 @@
+ #include <assert.h>
+ #include <string.h> /* strcat() */
+ #include <limits.h>
+-#include <png.h>    /* includes zlib.h and setjmp.h */
++#include <png.h>
++#include <zlib.h>
++#include <pngpriv.h>
+ #include "pnm.h"
+ #include "pngtxt.h"
+ #include "shhopt.h"
+@@ -69,7 +71,7 @@
+ #include "nstring.h"
+ #include "version.h"
+ 
+-#if PNG_LIBPNG_VER >= 10400
++#if 0
+ #error Your PNG library (<png.h>) is incompatible with this Netpbm source code.
+ #error You need either an older PNG library (older than 1.4)
+ #error newer Netpbm source code (at least 10.47.04)
+@@ -2630,7 +2632,7 @@
      info_ptr->num_palette = palette_size;
      if (trans_size > 0) {
          info_ptr->valid |= PNG_INFO_tRNS;
@@ -9,7 +29,7 @@
          info_ptr->num_trans = trans_size;   /* omit opaque values */
      }
      /* creating hIST chunk */
-@@ -1900,7 +1900,7 @@
+@@ -2667,7 +2669,7 @@
          info_ptr->color_type == PNG_COLOR_TYPE_RGB) {
          if (transparent > 0) {
              info_ptr->valid |= PNG_INFO_tRNS;
@@ -18,7 +38,7 @@
                  xelToPngColor_16(transcolor, maxval, png_maxval);
          }
      } else {
-@@ -1912,10 +1912,10 @@
+@@ -2679,10 +2681,10 @@
          if (info_ptr->valid && PNG_INFO_tRNS) 
              pm_message("Transparent color {gray, red, green, blue} = "
                         "{%d, %d, %d, %d}",
