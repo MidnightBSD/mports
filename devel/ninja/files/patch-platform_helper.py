c9663d2d926cf9542a50a20599b44066c880d108
Lucas Holt <luke@foolishgames.com>
2014-02-21 20:33:09 +0000

    ninja 1.4.0

--- devel/ninja/files/patch-platform_helper.py
+++ devel/ninja/files/patch-platform_helper.py
@@ -1,12 +1,12 @@
---- platform_helper.py.orig	2013-06-04 14:47:31.000000000 -0400
-+++ platform_helper.py	2013-09-18 21:39:49.000000000 -0400
+--- platform_helper.py.orig	2013-09-11 22:15:28.000000000 -0400
++++ platform_helper.py	2014-02-21 15:28:18.130559482 -0500
 @@ -18,7 +18,7 @@
  import sys
  
  def platforms():
 -    return ['linux', 'darwin', 'freebsd', 'openbsd', 'solaris', 'sunos5',
 +    return ['linux', 'darwin', 'freebsd', 'midnightbsd', 'openbsd', 'solaris', 'sunos5',
-             'mingw', 'msvc', 'gnukfreebsd8']
+             'mingw', 'msvc', 'gnukfreebsd8', 'bitrig']
  
  class Platform( object ):
 @@ -33,6 +33,8 @@
@@ -18,7 +18,7 @@
          elif self._platform.startswith('openbsd'):
              self._platform = 'openbsd'
          elif self._platform.startswith('solaris'):
-@@ -64,6 +66,9 @@
+@@ -65,6 +67,9 @@
      def is_freebsd(self):
          return self._platform == 'freebsd'
  
