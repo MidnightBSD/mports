97adc7c3181a778ff7dcb5f3e225c9dac3056613
Lucas Holt <luke@foolishgames.com>
2014-03-01 01:00:56 +0000

    add libtool extension

---
+++ Mk/extensions/libtool.mk
@@ -0,0 +1,49 @@
+# $MidnightBSD$
+#
+# Bring libtool scripts up to date.
+#
+# Feature:	libtool
+# Usage:	USES=libtool
+
+.if !defined(_INCLUDE_USES_LIBTOOL_MK)
+_INCLUDE_USES_LIBTOOL_MK=	yes
+_USES_POST+=	libtool
+libtool_ARGS?=
+.endif
+
+.if defined(_POSTMKINCLUDED) && !defined(_INCLUDE_USES_LIBTOOL_POST_MK)
+_INCLUDE_USES_LIBTOOL_POST_MK=	yes
+
+patch-libtool:
+	@${FIND} ${WRKDIR} \( -name configure -or -name ltconfig \)	\
+		-type f | ${XARGS} ${REINPLACE_CMD}			\
+		-e '/link_all_deplibs[0-9A-Z_]*=/s/=unknown/=no/'	\
+		-e 's,freebsd\*),freebsd\*|dragonfly\*),g'		\
+		-e '/objformat=/s/echo aout/echo elf/'			\
+		-e "/freebsd-elf\\*)/,/;;/ {				\
+		    /deplibs_check_method=/s/=.*/=pass_all/; }"	
+
+.if ! ${libtool_ARGS:Moldver}
+	@${FIND} ${WRKDIR} \( -name configure -or -name ltconfig \)	\
+		-type f | ${XARGS} ${REINPLACE_CMD}			\
+		-e "/freebsd-elf\\*)/,/;;/ {				\
+		    /library_names_spec=.*\\.so/			\
+		    s/=.*/='\$$libname\$$release.so\$$versuffix		\
+			\$$libname\$$release.so\$$major \$$libname.so'	\
+		    soname_spec='\$$libname\$$release.so\$$major'/;	\
+		    /library_names_spec=.*shared_ext/			\
+		    s/=.*/='\$$libname\$$release\$$shared_ext\$$versuffix \
+			\$$libname\$$release\$$shared_ext\$$major	\
+			\$$libname\$$shared_ext'			\
+		    soname_spec='\$$libname\$$release\$$shared_ext\$$major'/; }"
+
+	@${FIND} ${WRKDIR} -type f -name ltmain.sh |			\
+		${XARGS} ${REINPLACE_CMD}				\
+		-e '/case $$version_type in/,+2				\
+		    s/darwin|linux|/darwin|freebsd-elf|linux|/'		\
+		-e '/freebsd-elf)/,+2 {					\
+		    /major=/s/=.*/=.$$(($$current - $$age))/;		\
+		    /versuffix=/s/=.*/="$$major.$$age.$$revision"/; }'
+.endif
+
+.endif
