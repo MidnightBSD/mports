3558c2c33abd65c506822ad4ae2f8f028cb676f4
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2014-03-01 02:54:06 +0000

    imake 1.0.6

--- trunk/devel/imake/files/patch-imake.c
+++ trunk/devel/imake/files/patch-imake.c
@@ -1,38 +1,50 @@
---- imake.c.orig	2006-05-15 16:28:14 -0400
-+++ imake.c	2009-05-25 20:45:58 -0400
-@@ -1161,34 +1161,7 @@
- static void
- get_binary_format(FILE *inFile)
- {
--  int mib[2];
--  size_t len;
--  int osrel = 0;
+--- imake.c.orig	2013-07-23 06:53:04.000000000 +0200
++++ imake.c	2013-08-31 14:57:48.000000000 +0200
+@@ -531,6 +531,14 @@
+ 				AddCppArg(p);
+ 			}
+ 	}
++	if ((p = getenv("IMAKECPPFLAGS"))) {
++		AddCppArg(p);
++		for (; *p; p++)
++			if (*p == ' ') {
++				*p++ = '\0';
++				AddCppArg(p);
++			}
++	}
+ 	if ((p = getenv("IMAKECPP")))
+ 		cpp = p;
+ 	if ((p = getenv("IMAKEMAKE")))
+@@ -1142,29 +1150,17 @@
+   int mib[2];
+   size_t len;
+   int osrel = 0;
 -  FILE *objprog = NULL;
--  int iself = 0;
+   int iself = 0;
 -  char buf[10];
 -  char cmd[PATH_MAX];
--
--  mib[0] = CTL_KERN;
--  mib[1] = KERN_OSRELDATE;
--  len = sizeof(osrel);
--  sysctl(mib, 2, &osrel, &len, NULL, 0);
+ 
+   mib[0] = CTL_KERN;
+   mib[1] = KERN_OSRELDATE;
+   len = sizeof(osrel);
+   sysctl(mib, 2, &osrel, &len, NULL, 0);
 -  if (CrossCompiling) {
 -      strcpy (cmd, CrossCompileDir);
 -      strcat (cmd, "/");
 -      strcat (cmd,"objformat");
 -  } else
 -      strcpy (cmd, "objformat");
--
+ 
 -  if (osrel >= 300004 &&
 -      (objprog = popen(cmd, "r")) != NULL &&
 -      fgets(buf, sizeof(buf), objprog) != NULL &&
 -      strncmp(buf, "elf", 3) == 0)
--    iself = 1;
++  if (osrel < 300004)
++    iself = 0;
++  else
+     iself = 1;
 -  if (objprog)
 -    pclose(objprog);
--
--  fprintf(inFile, "#define DefaultToElfFormat %s\n", iself ? "YES" : "NO");
-+  fprintf(inFile, "#define DefaultToElfFormat %s\n", "YES");
- }
- #endif
  
+   fprintf(inFile, "#define DefaultToElfFormat %s\n", iself ? "YES" : "NO");
+ }
