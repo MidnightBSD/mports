47697e1eb1c09bd9b36fbf0c72b8cb6c6fd8af91
Lucas Holt <luke@foolishgames.com>
2014-08-06 02:07:30 +0000

    stop using system iconv until we solve the issues with ports

--- Mk/extensions/iconv.mk
+++ Mk/extensions/iconv.mk
@@ -13,7 +13,7 @@ _INCLUDE_USES_ICONV_MK=	yes
 
 iconv_ARGS:=	${iconv_ARGS:S/,/ /g}
 
-.if !exists(/usr/include/iconv.h) || ${iconv_ARGS:Mwchar_t} || ${iconv_ARGS:Mtranslit}
+.if !exists(/usr/include/iconv.h) || ${iconv_ARGS:Mwchar_t} || ${iconv_ARGS:Mtranslit} || ${OSVERSION} < 6000
 
 ICONV_CMD=	${LOCALBASE}/bin/iconv
 ICONV_LIB=	-liconv
