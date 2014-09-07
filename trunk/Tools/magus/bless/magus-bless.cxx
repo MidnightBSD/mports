153b97ecc51650761ed879da1bc93bafeebc59f9
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2014-09-07 02:57:21 +0000

    first pass at pg rewrite

--- trunk/Tools/magus/bless/magus-bless.cxx
+++ trunk/Tools/magus/bless/magus-bless.cxx
@@ -1,5 +1,5 @@
 /*-
-Copyright (C) 2008, 2010, 2011, 2013 Lucas Holt All rights reserved.
+Copyright (C) 2008, 2010, 2011, 2013, 2014 Lucas Holt All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -25,41 +25,37 @@ SUCH DAMAGE.
 $MidnightBSD: mports/Tools/magus/bless/magus-bless.c,v 1.7 2013/03/17 17:08:40 laffer1 Exp $
 */
 
+#include <iostream>
+#include <pqxx/pqxx>
+#include <cstring>
+
+using namespace std;
+using namespace pqxx;
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <err.h>
 
 #include <sys/types.h>
 #include <sha256.h>
 
-#include "mysql.h"
 #include "sqlite3.h"
 
-#define DB_HOST "db.midnightbsd.org"
-#define DB_DATABASE "magus"
-
-/* MYSQL */
-int mysql_exec_sql(MYSQL *, const char *);
+const string DB_HOST = "70.91.226.203";
+const string DB_DATABASE = "magus";
 
 /* SQLITE3 */
 sqlite3* open_indexdb(int);
 void close_indexdb(sqlite3 *);
 int exec_indexdb(sqlite3 *, const char *, ...);
-static void create_indexdb(sqlite3 *);
-static void load_depends(sqlite3 *, MYSQL *, int, const char *, const char *);
+void create_indexdb(sqlite3 *);
+void load_depends(sqlite3 *, connection &, int, const char *, const char *);
 
 int
 main(int argc, char *argv[])
 {
-    MYSQL mysql;
-    MYSQL mysql2; /* we need two connections */
-    MYSQL_RES *result;
-    MYSQL_ROW row;
-    unsigned int num_fields;
     char query_def[1000];
-    char *ln;
     int runid;
     sqlite3 *db;
     sqlite3_stmt *stmt;
@@ -79,71 +75,45 @@ main(int argc, char *argv[])
         exit(1);
     }
 
-    if (mysql_init(&mysql) == NULL)
-    {
-         fprintf(stderr, "Failed to initate MySQL connection\n");
-         exit(1);
+    string connect_string = "dbname=magus user=" + string(argv[2]) + " password=" + string(argv[3]) + " hostaddr=" + DB_HOST + " port=5432";
+    connection C(connect_string);
+    connection C2(connect_string);
+
+    if (C.is_open()) {
+		cout << "We are connected to " << C.dbname() << endl;
+    } else {
+		cout << "We are not connected! Check username and password." << endl;
+		return 0;
     }
 
-    if (mysql_init(&mysql2) == NULL)
-    {
-         fprintf(stderr, "Failed to initate MySQL connection\n");
-         exit(1);
-    }
-
-    if (!mysql_real_connect(&mysql, DB_HOST, argv[2], argv[3], NULL, 0, NULL, 0)) 
-    {
-        fprintf( stderr, "Failed to connect: Error: %s\n", mysql_error(&mysql));
-        exit(1);
-    }
-
-    if (!mysql_real_connect(&mysql2, DB_HOST, argv[2], argv[3], NULL, 0, NULL, 0))
-    {
-        fprintf( stderr, "Failed to connect: Error: %s\n", mysql_error(&mysql2));
-        exit(1);
-    }
-
-    if (mysql_select_db(&mysql, DB_DATABASE) != 0) {
-        fprintf(stderr, "Failed to connect to Database: Error: %s\n", mysql_error(&mysql));
-	exit(2);
-    }
-
-    if (mysql_select_db(&mysql2, DB_DATABASE) != 0) {
-        fprintf(stderr, "Failed to connect to Database: Error: %s\n", mysql_error(&mysql2));
-	exit(2);
-    }
-    
     sprintf(query_def,
       "select pkgname, name, license, description, CONCAT(CONCAT_WS( '-', pkgname, version),'.mport'), version  from ports where run=%d AND status!='internal' AND status!='untested' AND status!='fail' ORDER BY pkgname;",
       runid);
 
-    if (mysql_exec_sql(&mysql, query_def) == 0)
+    nontransaction N(C);
+
+    result R(N.exec(string(query_def)));
+
+    if (!R.empty()) 
     {
-        //printf("%ld Record Found\n",(long) mysql_affected_rows(&mysql));
-        result = mysql_store_result(&mysql);
     
-        if (result)
-        {
             db = open_indexdb(runid);
             create_indexdb(db);
 
-            num_fields = mysql_num_fields(result);
-            while ((row = mysql_fetch_row(result))) 
-            { 
-               if (num_fields == 6 && row[0] && row[1] && row[2] && row[3] && row[4])
-               {
-                   asprintf(&ln, "%s: %s %s %s %s %s", row[0], row[1], row[2], row[3], row[5], row[4]);
-                   asprintf(&filePath, "%s/%s", argv[4], row[4]);
+	    for (result::const_iterator row = R.begin(); row != R.end(); ++row) 
+            {
+	   	   string ln = row[0].as(string()) + ": " + row[1].as(string()) +  row[2].as(string()) + " " + row[3].as(string()) + " " + row[5].as(string()) + " " + row[4].as(string());
+//                   asprintf(&ln, "%s: %s %s %s %s %s", row[0].as(string()).c_str(), row[1].as(string()).c_str(), row[2].as(string()), row[3].as(string()), row[5].as(string()), row[4].as(string()));
+                   asprintf(&filePath, "%s/%s", argv[4], row[4].as(string()).c_str());
                    fileHash = SHA256_File(filePath, NULL);
                    if (fileHash == NULL)
                    {
                        fprintf(stderr, "Could not locate file %s\n", filePath);
-                       free(ln);
                        free(filePath);
                        continue;
                    }
 
-                   if (ln) 
+                   if (ln.c_str()) 
                    {
                       if (sqlite3_prepare_v2(db, 
                        "INSERT INTO packages (pkg, version, license, comment, bundlefile, hash) VALUES(?,?,?,?,?,?)",
@@ -151,11 +121,11 @@ main(int argc, char *argv[])
                        {
                           errx(1, "Could not prepare statement");
                        }
-                       sqlite3_bind_text(stmt, 1, row[0], strlen(row[0]), SQLITE_TRANSIENT);
-                       sqlite3_bind_text(stmt, 2, row[5], strlen(row[5]), SQLITE_TRANSIENT);
-                       sqlite3_bind_text(stmt, 3, row[2], strlen(row[2]), SQLITE_TRANSIENT);
-                       sqlite3_bind_text(stmt, 4, row[3], strlen(row[3]), SQLITE_TRANSIENT);
-                       sqlite3_bind_text(stmt, 5, row[4], strlen(row[4]), SQLITE_TRANSIENT);
+                       sqlite3_bind_text(stmt, 1, row[0].as(string()).c_str(), row[0].as(string()).length(), SQLITE_TRANSIENT);
+                       sqlite3_bind_text(stmt, 2, row[5].as(string()).c_str(), row[5].as(string()).length(), SQLITE_TRANSIENT);
+                       sqlite3_bind_text(stmt, 3, row[2].as(string()).c_str(), row[2].as(string()).length(), SQLITE_TRANSIENT);
+                       sqlite3_bind_text(stmt, 4, row[3].as(string()).c_str(), row[3].as(string()).length(), SQLITE_TRANSIENT);
+                       sqlite3_bind_text(stmt, 5, row[4].as(string()).c_str(), row[4].as(string()).length(), SQLITE_TRANSIENT);
                        sqlite3_bind_text(stmt, 6, fileHash, strlen(fileHash), SQLITE_TRANSIENT);
 
                        if (sqlite3_step(stmt) != SQLITE_DONE)
@@ -172,31 +142,28 @@ main(int argc, char *argv[])
                           errx(1, "Could not prepare statement");
                        }
 
-                       sqlite3_bind_text(stmt, 1, row[1], strlen(row[1]), SQLITE_TRANSIENT);
-                       sqlite3_bind_text(stmt, 2, row[0], strlen(row[0]), SQLITE_TRANSIENT); 
+                       sqlite3_bind_text(stmt, 1, row[1].as(string()).c_str(), row[1].as(string()).length(), SQLITE_TRANSIENT);
+                       sqlite3_bind_text(stmt, 2, row[0].as(string()).c_str(), row[0].as(string()).length(), SQLITE_TRANSIENT); 
 
                        if (sqlite3_step(stmt) != SQLITE_DONE)
                           errx(1,"Could not execute query");
                        sqlite3_reset(stmt);
                        sqlite3_finalize(stmt);
 
-                       puts(ln);
-                       free(ln);
+                       puts(ln.c_str());
 
-			load_depends(db, &mysql2, runid, row[0], row[5]);
+			load_depends(db, C2, runid, row[0].as(string()).c_str(), row[5].as(string()).c_str());
                    }
                }
                printf("\n"); 
             }
-            mysql_free_result(result);
 
+	    nontransaction Q(C);
             sprintf(query_def, "SELECT * FROM mirrors order by country");
-            if (mysql_exec_sql(&mysql, query_def) == 0)
+	    result R2(Q.exec(string(query_def)));
+	    if (!R2.empty())
             {
-                result = mysql_store_result(&mysql);
-                if (result)
-                {
-                    while ((row = mysql_fetch_row(result)))
+		   for (result::const_iterator c = R.begin(); c != R.end(); ++c) 
                     {
                          if (sqlite3_prepare_v2(db,
                           "INSERT INTO mirrors (country, mirror) VALUES(?,?)",
@@ -204,37 +171,19 @@ main(int argc, char *argv[])
                          {
                              errx(1, "Could not prepare statement");
                          }
-                         sqlite3_bind_text(stmt, 1, row[1], strlen(row[1]), SQLITE_TRANSIENT);
-                         sqlite3_bind_text(stmt, 2, row[2], strlen(row[2]), SQLITE_TRANSIENT);
+                         sqlite3_bind_text(stmt, 1, c[1].as(string()).c_str(), c[1].as(string()).length(), SQLITE_TRANSIENT);
+                         sqlite3_bind_text(stmt, 2, c[2].as(string()).c_str(), c[2].as(string()).length(), SQLITE_TRANSIENT);
 
                          if (sqlite3_step(stmt) != SQLITE_DONE)
                              errx(1,"Could not execute query");
                          sqlite3_reset(stmt);
                          sqlite3_finalize(stmt);
                     }
-                    mysql_free_result(result);
-                }
             }
 
             close_indexdb(db);
-        }
-        else
-            if(mysql_field_count(&mysql) > 0)
-                fprintf( stderr, "Error getting records: %s\n", mysql_error(&mysql));
-    }
-    else
-        fprintf( stderr, "Failed to find any records and caused an error: %s\n", mysql_error(&mysql));
     
-    mysql_close(&mysql);
-    mysql_close(&mysql2);
-   
     return 0;
-}
-
-int
-mysql_exec_sql(MYSQL *mysql, const char *create_definition)
-{
-	return mysql_real_query(mysql, create_definition, strlen(create_definition));
 }
 
 sqlite3*
@@ -294,7 +243,7 @@ exec_indexdb(sqlite3 *db, const char *fmt, ...)
   return 0;
 }
 
-static void
+void
 create_indexdb(sqlite3 *db)
 {
 	exec_indexdb(db, "CREATE TABLE IF NOT EXISTS mirrors (country text NOT NULL, mirror text NOT NULL)");
@@ -305,32 +254,26 @@ create_indexdb(sqlite3 *db)
 	exec_indexdb(db, "CREATE TABLE IF NOT EXISTS depends (pkg text NOT NULL, version text NOT NULL, d_pkg text NOT NULL, d_version text NOT NULL)");
 }
 
-static void
-load_depends(sqlite3 *db, MYSQL *mysql, int runid, const char *pkg_name, const char *version)
+void
+load_depends(sqlite3 *db, connection & C, int runid, const char *pkg_name, const char *version)
 {
 	char query_def[1000];
-	MYSQL_RES *result;
-	MYSQL_ROW row;
 	sqlite3_stmt *stmt;
-	int num_fields;
 
 	printf("---->\tProcessing dependencies for %s - %s\n", pkg_name, version);
 
 	sprintf(query_def, "SELECT distinct p2.pkgname, p2.version from ports as p1 left join depends d on p1.id = d.port left join ports p2 on d.dependency = p2.id where p2.run = %d and p1.run = %d and ((p1.status = 'pass' or p1.status = 'warn') and (p2.status = 'pass' or p2.status = 'warn')) and p1.pkgname = '%s' and p1.version = '%s'",
 		runid, runid, pkg_name, version); 
 
-	if (mysql_exec_sql(mysql, query_def) == 0)
-	{
-		//printf("     \t%ld Depends Found\n",(long) mysql_affected_rows(mysql));
-		result = mysql_store_result(mysql);
+if (C.is_open()) {
 
-	        if (result)
+	nontransaction N(C);
+	result R(N.exec(string(query_def)));
+
+		if (!R.empty()) 
 		{
-			num_fields = mysql_num_fields(result);
-			while ((row = mysql_fetch_row(result)))
+			for (result::const_iterator row = R.begin(); row != R.end(); ++row) 
 			{
-				if (num_fields == 2 && row[0] && row[1])
-				{
 					if (sqlite3_prepare_v2(db,
 						"INSERT INTO depends (pkg, version, d_pkg, d_version) VALUES(?,?,?,?)",
 						-1, &stmt, 0) != SQLITE_OK)
@@ -339,16 +282,14 @@ load_depends(sqlite3 *db, MYSQL *mysql, int runid, const char *pkg_name, const c
 					}
 					sqlite3_bind_text(stmt, 1, pkg_name, strlen(pkg_name), SQLITE_TRANSIENT);
 					sqlite3_bind_text(stmt, 2, version, strlen(version), SQLITE_TRANSIENT);
-					sqlite3_bind_text(stmt, 3, row[0], strlen(row[0]), SQLITE_TRANSIENT);
-					sqlite3_bind_text(stmt, 4, row[1], strlen(row[1]), SQLITE_TRANSIENT);
+					sqlite3_bind_text(stmt, 3, row[0].as(string()).c_str(), row[0].as(string()).length(), SQLITE_TRANSIENT);
+					sqlite3_bind_text(stmt, 4, row[1].as(string()).c_str(), row[1].as(string()).length(), SQLITE_TRANSIENT);
 
 					if (sqlite3_step(stmt) != SQLITE_DONE)
 						errx(1,"Could not execute query");
 					sqlite3_reset(stmt);
 					sqlite3_finalize(stmt);
-				}
 			}
-			mysql_free_result(result);
 		}
 	}
 }
