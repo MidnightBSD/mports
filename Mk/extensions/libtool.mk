2bc27e364fcefa0c454ccdc675a6b7a796d43673
Lucas Holt <luke@foolishgames.com>
2014-09-13 21:17:14 +0000

    handle la files

--- Mk/extensions/libtool.mk
+++ Mk/extensions/libtool.mk
@@ -10,13 +10,20 @@
 #			refer to .la libraries in this port.  As soon as all
 #			those dependent ports have some form of USES=libtool
 #			keepla can be removed.
+#		build	Add a build dependency on devel/libtool.  This can
+#			be used when a port does not generate its own libtool
+#			script and relies on the system to provide one.
 #
 # MAINTAINER:	ports@MidnightBSD.org
 
 .if !defined(_INCLUDE_USES_LIBTOOL_MK)
 _INCLUDE_USES_LIBTOOL_MK=	yes
 _USES_POST+=	libtool
-libtool_ARGS?=
+libtool_ARGS:=	${libtool_ARGS:C/,/ /}
+
+.if ${libtool_ARGS:Mbuild}
+BUILD_DEPENDS+=	libtool:${PORTSDIR}/devel/libtool
+.endif
 .endif
 
 .if defined(_POSTMKINCLUDED) && !defined(_INCLUDE_USES_LIBTOOL_POST_MK)
@@ -24,14 +31,15 @@ _INCLUDE_USES_LIBTOOL_POST_MK=	yes
 
 patch-libtool:
 	@${FIND} ${WRKDIR} \( -name configure -or -name ltconfig \)	\
-		-type f | ${XARGS} ${REINPLACE_CMD}			\
+		-type f | while read i; do ${SED} -i.bak		\
 		-e '/dragonfly\*/!s/^ *freebsd\*[ )]/dragonfly* | &/'	\
 		-e '/gcc_dir=\\`/s/gcc /$$CC /'				\
 		-e '/gcc_ver=\\`/s/gcc /$$CC /'				\
 		-e '/link_all_deplibs[0-9A-Z_]*=/s/=unknown/=no/'	\
 		-e '/objformat=/s/echo aout/echo elf/'			\
 		-e "/freebsd-elf\\*)/,/;;/ {				\
-		    /deplibs_check_method=/s/=.*/=pass_all/; }"	
+		    /deplibs_check_method=/s/=.*/=pass_all/; }"		\
+		$${i} && ${TOUCH} -mr $${i}.bak $${i}; done
 
 	@${FIND} ${WRKDIR} -type f -name ltmain.sh |			\
 		${XARGS} ${REINPLACE_CMD}				\
@@ -41,7 +49,7 @@ patch-libtool:
 
 .if ! ${libtool_ARGS:Moldver}
 	@${FIND} ${WRKDIR} \( -name configure -or -name ltconfig \)	\
-		-type f | ${XARGS} ${REINPLACE_CMD}			\
+		-type f | while read i; do ${SED} -i.bak		\
 		-e "/freebsd-elf\\*)/,/;;/ {				\
 		    /library_names_spec=.*\\.so/			\
 		    s/=.*/='\$$libname\$$release.so\$$versuffix		\
@@ -51,7 +59,8 @@ patch-libtool:
 		    s/=.*/='\$$libname\$$release\$$shared_ext\$$versuffix \
 			\$$libname\$$release\$$shared_ext\$$major	\
 			\$$libname\$$shared_ext'			\
-		    soname_spec='\$$libname\$$release\$$shared_ext\$$major'/; }"
+		    soname_spec='\$$libname\$$release\$$shared_ext\$$major'/; \
+		    }" $${i} && ${TOUCH} -mr $${i}.bak $${i}; done
 
 	@${FIND} ${WRKDIR} -type f -name ltmain.sh |			\
 		${XARGS} ${REINPLACE_CMD}				\
@@ -63,10 +72,14 @@ patch-libtool:
 .endif
 
 patch-lafiles:
-.if ${libtool_ARGS} == keepla || ${libtool_ARGS} == oldver
+.if ${libtool_ARGS:Mkeepla} || ${libtool_ARGS:Moldver}
 	@${FIND} ${FAKE_DESTDIR} -type f -name '*.la' |			\
 		${XARGS} ${SED} -i '' -e "/dependency_libs=/s/=.*/=''/"
 .else
+	@${FIND} ${FAKE_DESTDIR} -type l -exec ${SH} -c			\
+		'case `${READLINK_CMD} -f "{}"` in			\
+			*.la) ${ECHO_CMD} "{}" ;; esac' \; |		\
+		${XARGS} ${GREP} -l 'libtool library' | ${XARGS} ${RM}
 	@${FIND} ${FAKE_DESTDIR} -type f -name '*.la' |			\
 		${XARGS} ${GREP} -l 'libtool library' | ${XARGS} ${RM}
 .endif
