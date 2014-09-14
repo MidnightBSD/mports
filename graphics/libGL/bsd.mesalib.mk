645d5a7b28338bd4dd667427a518bb04438e4efe
Lucas Holt <luke@foolishgames.com>
2014-09-14 00:46:10 +0000

    patch libGL for mesa update

--- graphics/libGL/bsd.mesalib.mk
+++ graphics/libGL/bsd.mesalib.mk
@@ -1,26 +1,21 @@
-#-*- mode: Fundamental; tab-width: 4; -*-
-# ex:ts=4
-#
-# bsd.mesa.mk - an attempt to refactor MesaLib ports.
-#
-# Created by: Florent Thoumie <flz@FreeBSD.org>
+# bsd.mesalib.mk - shared code between MesaLib ports.
 #
 # !!! Here be dragons !!! (they seem to be everywhere these days)
 #
 # Remember to upgrade the following ports everytime you bump MESAVERSION:
 #
+#    - graphics/libEGL
 #    - graphics/libGL
-#    - graphics/libGLU
-#    - graphics/libGLw
+#    - grahpics/libglesv2
 #    - graphics/dri
 #
-# $MidnightBSD: mports/graphics/libGL/bsd.mesalib.mk,v 1.10 2010/05/30 22:45:18 laffer1 Exp $
+# $FreeBSD: head/graphics/libGL/bsd.mesalib.mk 358152 2014-06-17 17:23:07Z jkim $
 
 MESAVERSION=	${MESABASEVERSION}${MESASUBVERSION:C/^(.)/.\1/}
 MESADISTVERSION=${MESABASEVERSION}${MESASUBVERSION:C/^(.)/-\1/}
 
 .if defined(WITH_NEW_XORG)
-MESABASEVERSION=	7.11.2
+MESABASEVERSION=	9.1.7
 # if there is a subversion, include the '-' between 7.11-rc2 for example.
 MESASUBVERSION=		
 PLIST_SUB+=	OLD="@comment " NEW=""
@@ -30,90 +25,105 @@ MESASUBVERSION=
 PLIST_SUB+=	OLD="" NEW="@comment "
 .endif
 
-MASTER_SITES=	ftp://ftp.freedesktop.org/pub/mesa/${MESABASEVERSION}/:mesa,glut
-DISTFILES=	MesaLib-${MESADISTVERSION}${EXTRACT_SUFX}:mesa
-MAINTAINER?=	ports@MidnightBSD.org
+MASTER_SITES=	ftp://ftp.freedesktop.org/pub/mesa/older-versions/${MESABASEVERSION:R:R}.x/${MESABASEVERSION}/
+DISTFILES=	MesaLib-${MESADISTVERSION}${EXTRACT_SUFX}
+MAINTAINER=	x11@FreeBSD.org
 
 BUILD_DEPENDS+=	makedepend:${PORTSDIR}/devel/makedepend \
+		python2:${PORTSDIR}/lang/python2 \
 		${PYTHON_SITELIBDIR}/libxml2.py:${PORTSDIR}/textproc/py-libxml2
 
-USE_BISON=	build
-USE_PYTHON_BUILD=yes
-USES+=		gmake tar:bzip2
+USES+=		bison gmake pathfix pkgconfig shebangfix tar:bzip2
+USE_PYTHON_BUILD=2
 USE_LDCONFIG=	yes
 GNU_CONFIGURE=	yes
-MAKE_JOBS_UNSAFE=	yes
 
-CPPFLAGS+=	-I${LOCALBASE}/include
-LDFLAGS+=	-L${LOCALBASE}/lib
-CONFIGURE_ARGS+=--enable-gallium-llvm=no --without-gallium-drivers \
-		--disable-egl --disable-glut
+CPPFLAGS+=	-isystem${LOCALBASE}/include
+LDFLAGS+=	-Wl,-Y${LOCALBASE}/lib
+
+.if ${OSVERSION} < 1000033
+BUILD_DEPENDS+=	${LOCALBASE}/bin/flex:${PORTSDIR}/textproc/flex
+CONFIGURE_ENV+=ac_cv_prog_LEX=${LOCALBASE}/bin/flex
+.endif
 
 .if defined(WITH_NEW_XORG)
-EXTRA_PATCHES+=	${PATCHDIR}/extra-configure \
-		${PATCHDIR}/extra-mach64_context.h \
-		${PATCHDIR}/extra-sis_context.h \
-		${PATCHDIR}/extra-src-glsl_ir_constant_expression.cpp \
-		${PATCHDIR}/extra-src__gallium__include__pipe__p_config.h \
-		${PATCHDIR}/extra-src__mesa__drivers__dri__nouveau__nouveau_array.c \
-		${PATCHDIR}/extra-src__mesa__drivers__dri__nouveau__nouveau_render_t.c \
-		${PATCHDIR}/extra-src__mesa__drivers__dri__radeon__radeon_span.c
+INSTALL_TARGET=	install-strip
+USES+=		libtool:keepla
+
+python_OLD_CMD=	"/usr/bin/env[[:space:]]python"
+python_CMD=	${LOCALBASE}/bin/python2
+SHEBANG_FILES=	src/gallium/*/*/*.py src/gallium/tools/trace/*.py \
+		src/gallium/drivers/svga/svgadump/svga_dump.py \
+		src/glsl/tests/compare_ir src/mapi/glapi/gen/*.py \
+		src/mapi/mapi/mapi_abi.py
+
+# i386 triggers clang bug 19778. This happens with clang 3.4.1 and older. 
+. if ${ARCH} == i386
+USE_GCC=yes
+. endif
 .else
-EXTRA_PATCHES+=	${PATCHDIR}/extra-configure-old \
-		${PATCHDIR}/extra-mach64_context.h-old \
-		${PATCHDIR}/extra-src__mesa__x86-64__glapi_x86-64.S \
-		${PATCHDIR}/extra-src__mesa__x86-64__xform4.S \
-		${PATCHDIR}/extra-src__mesa__x86__glapi_x86.S \
-		${PATCHDIR}/extra-src__mesa__x86__read_rgba_span_x86.S
-.endif
+CONFIGURE_ARGS+=--disable-glut --disable-glw --disable-glu
 
 ALL_TARGET=		default
-
-PATCHDIR=		${.CURDIR}/../../graphics/libGL/files
-WRKSRC=			${WRKDIR}/Mesa-${MESABASEVERSION}${MESASUBVERSION}
-
-.if !defined(ARCH)
-ARCH!=			uname -p
 .endif
 
-COMPONENT=		${PORTNAME:L:C/^lib//:C/mesa-//}
-
-.if ${COMPONENT:Mglu} == ""
-CONFIGURE_ARGS+=	--disable-glu
-.endif
-
-.if ${COMPONENT:Mglw} == ""
-CONFIGURE_ARGS+=	--disable-glw
+MASTERDIR=		${.CURDIR}/../../graphics/libGL
+.if defined(WITH_NEW_XORG)
+PATCHDIR=		${MASTERDIR}/files
 .else
-CONFIGURE_ARGS+=	--enable-motif
+PATCHDIR=		${MASTERDIR}/files-old
+.endif
+DESCR=			${.CURDIR}/pkg-descr
+PLIST=			${.CURDIR}/pkg-plist
+WRKSRC=			${WRKDIR}/Mesa-${MESADISTVERSION}
+
+COMPONENT=		${PORTNAME:tl:C/^lib//:C/mesa-//}
+
+.if ${COMPONENT:Mglesv2} == ""
+CONFIGURE_ARGS+=	--disable-gles2
+.else
+CONFIGURE_ARGS+=	--enable-gles2
+.endif
+
+.if ${COMPONENT:Megl} == ""
+CONFIGURE_ARGS+=	--disable-egl
+.else
+CONFIGURE_ARGS+=	--enable-egl
 .endif
 
 .if ${COMPONENT:Mdri} == ""
-CONFIGURE_ARGS+=	--with-dri-drivers=no
+CONFIGURE_ARGS+=--with-dri-drivers=no
+CONFIGURE_ARGS+=--enable-gallium-llvm=no --without-gallium-drivers
+.else
+# done in the dri port
 .endif
 
+.if !defined(WITH_NEW_XORG)
 .if defined(WITHOUT_XCB)
 CONFIGURE_ARGS+=	--disable-xcb
 .else
 CONFIGURE_ARGS+=	--enable-xcb
 .endif
+.endif
 
 post-patch:
 	@${REINPLACE_CMD} -e 's|-ffast-math|${FAST_MATH}|' -e 's|x86_64|amd64|' \
-		-e 's|-lpthread|${PTHREAD_LIBS}|g' ${WRKSRC}/configure
+		${WRKSRC}/configure
+	@${REINPLACE_CMD} -e 's|/etc/|${PREFIX}/etc/|g' \
+		${WRKSRC}/src/mesa/drivers/dri/common/xmlconfig.c
+.if !defined(WITH_NEW_XORG)
+	@${REINPLACE_CMD} -e 's|python|${PYTHON_CMD}|' \
+		${WRKSRC}/src/gallium/auxiliary/util/Makefile
 	@${REINPLACE_CMD} -e 's|[$$](INSTALL_LIB_DIR)/pkgconfig|${PREFIX}/libdata/pkgconfig|' \
 		${WRKSRC}/src/glu/Makefile \
-		${WRKSRC}/src/glw/Makefile \
 		${WRKSRC}/src/mesa/Makefile \
 		${WRKSRC}/src/mesa/drivers/dri/Makefile
-.if defined(WITH_NEW_XORG)
-# replace hardlinks with patched radeon_span.c
-.for i in r200 r300 r600
-	@${CP} -fp ${WRKSRC}/src/mesa/drivers/dri/radeon/radeon_span.c \
-		${WRKSRC}/src/mesa/drivers/dri/${i}/
-.endfor
-.endif
-.if ${COMPONENT:Mglut} != ""
-	@${REINPLACE_CMD} -e 's|[$$](INSTALL_LIB_DIR)/pkgconfig|${PREFIX}/libdata/pkgconfig|' \
-		${WRKSRC}/src/glut/glx/Makefile
+.else
+	@${REINPLACE_CMD} -e 's|#!/usr/bin/python|#!${PYTHON_CMD}|g' \
+		${WRKSRC}/src/mesa/drivers/dri/common/xmlpool/gen_xmlpool.py \
+		${WRKSRC}/src/glsl/builtins/tools/*.py
+	@${REINPLACE_CMD} -e 's|!/usr/bin/python2|!${PYTHON_CMD}|g' \
+		${WRKSRC}/src/mesa/main/get_hash_generator.py \
+		${WRKSRC}/src/mapi/glapi/gen/gl_enums.py \
+		${WRKSRC}/src/mapi/glapi/gen/gl_table.py
 .endif
