0015a7710b2a24b9f938cd9bed70e68a53deb24a
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2014-09-20 15:34:51 +0000

    teach makepatch about time zones

--- trunk/Mk/components/maintainer.mk
+++ trunk/Mk/components/maintainer.mk
@@ -13,13 +13,16 @@ makepatch:
 	@cd ${.CURDIR} && ${MKDIR} ${FILESDIR}
 	@(cd ${WRKSRC}; \
 		for i in `find . -type f -name '*.orig'`; do \
-			ORG=$$i; \
+			ORIG=$$i; \
 			NEW=$${i%.orig}; \
-			OUT=${FILESDIR}`${ECHO} $${NEW} | \
-				${SED} -e 's|/|__|g' \
-					-e 's|^\.__|/patch-|'`; \
-			${ECHO} ${DIFF} -ud $${ORG} $${NEW} '>' $${OUT}; \
-			${DIFF} -ud $${ORG} $${NEW} > $${OUT} || ${TRUE}; \
+			cmp -s $${ORIG} $${NEW} && continue; \
+			PATCH=`${ECHO} $${NEW} | ${SED} -e 's|/|__|g'`; \
+			OUT=${FILESDIR}/patch-$${PATCH}; \
+			${ECHO} ${DIFF} -ud $${ORIG} $${NEW} '>' $${OUT}; \
+			TZ=UTC ${DIFF} -ud $${ORIG} $${NEW} | ${SED} -e \
+				'/^---/s|\.[0-9]* +0000$$| UTC|' -e \
+				'/^+++/s|\([[:blank]][-0-9:.+]*\)*\)*$$||' \
+				> $${OUT} || ${TRUE}; \
 		done \
 	)
 .endif
