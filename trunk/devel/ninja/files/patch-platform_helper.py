fac116594fcac9b7a3c2d5bb291a8b61d11ae8a4
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2014-10-15 00:53:32 +0000

    ninja 1.5.1

--- trunk/devel/ninja/files/patch-platform_helper.py
+++ trunk/devel/ninja/files/patch-platform_helper.py
@@ -1,24 +1,24 @@
---- platform_helper.py.orig	2013-09-11 22:15:28.000000000 -0400
-+++ platform_helper.py	2014-02-21 15:28:18.130559482 -0500
-@@ -18,7 +18,7 @@
- import sys
+--- platform_helper.py.orig	2014-06-27 19:37:05.000000000 -0400
++++ platform_helper.py	2014-10-14 20:53:44.000000000 -0400
+@@ -19,7 +19,7 @@
  
  def platforms():
--    return ['linux', 'darwin', 'freebsd', 'openbsd', 'solaris', 'sunos5',
-+    return ['linux', 'darwin', 'freebsd', 'midnightbsd', 'openbsd', 'solaris', 'sunos5',
-             'mingw', 'msvc', 'gnukfreebsd8', 'bitrig']
+     return ['linux', 'darwin', 'freebsd', 'openbsd', 'solaris', 'sunos5',
+-            'mingw', 'msvc', 'gnukfreebsd', 'bitrig']
++            'mingw', 'msvc', 'gnukfreebsd', 'bitrig', 'midnightbsd']
  
- class Platform( object ):
+ class Platform(object):
+     def __init__(self, platform):
 @@ -33,6 +33,8 @@
              self._platform = 'freebsd'
-         elif self._platform.startswith('gnukfreebsd8'):
+         elif self._platform.startswith('gnukfreebsd'):
              self._platform = 'freebsd'
 +        elif self._platform.startswith('midnightbsd'):
 +            self._platform = 'midnightbsd'
          elif self._platform.startswith('openbsd'):
              self._platform = 'openbsd'
          elif self._platform.startswith('solaris'):
-@@ -65,6 +67,9 @@
+@@ -73,6 +75,9 @@
      def is_freebsd(self):
          return self._platform == 'freebsd'
  
