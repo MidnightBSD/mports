f666172adf897edf6daeb39722e0d10f209bbab7
Lucas Holt <luke@foolishgames.com>
2014-12-18 04:01:50 +0000

    teach libtool about midnightbsd

--- devel/libtool/files/patch-build-aux_ltmain.sh
+++ devel/libtool/files/patch-build-aux_ltmain.sh
@@ -1,6 +1,6 @@
---- build-aux/ltmain.sh.orig	2013-10-26 22:53:58 UTC
-+++ build-aux/ltmain.sh
-@@ -8598,13 +8598,13 @@ func_mode_link ()
+--- build-aux/ltmain.sh.orig	2013-10-26 18:53:58.000000000 -0400
++++ build-aux/ltmain.sh	2014-12-17 22:53:02.964009896 -0500
+@@ -8598,13 +8598,13 @@
  	  #
  	  case $version_type in
  	  # correct linux to gnu/linux during the next big refactor
@@ -16,12 +16,14 @@
  	    current=$number_major
  	    revision=$number_minor
  	    age=0
-@@ -8690,8 +8690,9 @@ func_mode_link ()
+@@ -8689,9 +8689,10 @@
+ 	  versuffix=.$current.$revision
  	  ;;
  
- 	freebsd-elf)
+-	freebsd-elf)
 -	  major=.$current
 -	  versuffix=.$current
++	freebsd-elf|midnightbsd)
 +	  func_arith $current - $age
 +	  major=.$func_arith_result
 +	  versuffix=$major.$age.$revision
