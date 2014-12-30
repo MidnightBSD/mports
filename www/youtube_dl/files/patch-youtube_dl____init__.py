04aadabc85b88b88f1da2f781d41527e69fbe0fc
Lucas Holt <luke@foolishgames.com>
2014-12-30 15:32:49 +0000

    youtube downloader 2014.11.24

--- www/youtube_dl/files/patch-youtube_dl____init__.py
+++ www/youtube_dl/files/patch-youtube_dl____init__.py
@@ -1,37 +1,29 @@
---- ./youtube_dl/__init__.py.orig	2014-02-25 09:29:35.000000000 +0000
-+++ ./youtube_dl/__init__.py	2014-02-25 09:29:49.000000000 +0000
-@@ -76,7 +76,6 @@
+--- youtube_dl/__init__.py.orig	2014-11-23 17:50:21.000000000 +0800
++++ youtube_dl/__init__.py	2014-11-23 21:17:37.918929006 +0800
+@@ -34,7 +34,6 @@
      std_headers,
      write_string,
  )
 -from .update import update_self
- from .FileDownloader import (
+ from .downloader import (
      FileDownloader,
  )
-@@ -206,8 +205,6 @@
-             action='help', help='print this help text and exit')
-     general.add_option('-v', '--version',
-             action='version', help='print program version and exit')
--    general.add_option('-U', '--update',
--            action='store_true', dest='update_self', help='update this program to latest version. Make sure that you have sufficient permissions (run with sudo if needed)')
-     general.add_option('-i', '--ignore-errors',
-             action='store_true', dest='ignoreerrors', help='continue on download errors, for example to skip unavailable videos in a playlist', default=False)
-     general.add_option('--abort-on-error',
-@@ -777,17 +774,6 @@
-         if opts.xattrs:
-             ydl.add_post_processor(XAttrMetadataPP())
+@@ -324,17 +323,13 @@
+             ydl.add_post_processor(ExecAfterDownloadPP(
+                 verboseOutput=opts.verbose, exec_cmd=opts.exec_cmd))
  
 -        # Update version
 -        if opts.update_self:
 -            update_self(ydl.to_screen, opts.verbose)
 -
--        # Maybe do nothing
--        if (len(all_urls) < 1) and (opts.load_info_filename is None):
--            if not opts.update_self:
--                parser.error(u'you must provide at least one URL')
--            else:
--                sys.exit()
--
-         try:
-             if opts.load_info_filename is not None:
-                 retcode = ydl.download_with_info_file(opts.load_info_filename)
+         # Remove cache dir
+         if opts.rm_cachedir:
+             ydl.cache.remove()
+ 
+         # Maybe do nothing
+         if (len(all_urls) < 1) and (opts.load_info_filename is None):
+-            if opts.update_self or opts.rm_cachedir:
++            if opts.rm_cachedir:
+                 sys.exit()
+ 
+             ydl.warn_if_short_id(sys.argv[1:] if argv is None else argv)
