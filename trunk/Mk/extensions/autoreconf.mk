a6ab2347310ed39643ee9b4aa31776c9ae3bae85
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2015-03-27 02:29:54 +0000

    update for automake 1.15

--- trunk/Mk/extensions/autoreconf.mk
+++ trunk/Mk/extensions/autoreconf.mk
@@ -53,19 +53,15 @@
 
 .if !defined(_INCLUDE_USES_AUTORECONF_MK)
 _INCLUDE_USES_AUTORECONF_MK=	yes
-_USES_POST+=		autoreconf
+_USES_POST+=	autoreconf
 
 BUILD_DEPENDS+=	autoconf-2.69:${PORTSDIR}/devel/autoconf \
 		autoheader-2.69:${PORTSDIR}/devel/autoconf \
 		autoreconf-2.69:${PORTSDIR}/devel/autoconf \
-		aclocal-1.14:${PORTSDIR}/devel/automake \
-		automake-1.14:${PORTSDIR}/devel/automake
+		aclocal-1.15:${PORTSDIR}/devel/automake \
+		automake-1.15:${PORTSDIR}/devel/automake
 
-.if ${USES:Mgettext} && empty(gettext_ARGS:Mrun)
-BUILD_DEPENDS+=	autopoint:${PORTSDIR}/devel/gettext
-.endif
-
-.if ${USES:Mlibtool} && empty(libtool_ARGS:Mbuild)
+.if defined(libtool_ARGS) && empty(libtool_ARGS:Mbuild)
 BUILD_DEPENDS+=	libtoolize:${PORTSDIR}/devel/libtool
 .endif
 
@@ -80,6 +76,13 @@ do-autoreconf:
 # Don't modify time stamps if the files already exist
 	@test -e ${CONFIGURE_WRKSRC}/${f} || ${TOUCH} ${CONFIGURE_WRKSRC}/${f}
 .endfor
+.if defined(_USE_GNOME) && ${_USE_GNOME:Mintltool}
+	@(cd ${CONFIGURE_WRKSRC} && \
+		if test -f configure.ac; then configure=configure.ac; \
+		else configure=configure.in; fi && \
+		if ${EGREP} -q '^(AC|IT)_PROG_INTLTOOL' $${configure}; \
+		then ${LOCALBASE}/bin/intltoolize -f -c; fi)
+.endif
 	@(cd ${CONFIGURE_WRKSRC} && ${LOCALBASE}/bin/autoreconf -f -i)
 .endif
 
