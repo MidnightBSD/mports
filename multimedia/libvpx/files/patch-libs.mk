3ba1a20613a072b7b5541f6482d3adaa321c8741
Lucas Holt <luke@foolishgames.com>
2015-04-12 06:41:12 +0000

    libvpx 1.3.0

--- multimedia/libvpx/files/patch-libs.mk
+++ multimedia/libvpx/files/patch-libs.mk
@@ -1,11 +1,11 @@
 
-$FreeBSD$
+$FreeBSD: head/multimedia/libvpx/files/patch-libs.mk 336806 2013-12-18 09:05:36Z ashish $
 
 --- libs.mk.orig
 +++ libs.mk
-@@ -242,8 +242,8 @@
- 	$(qexec)echo 'Conflicts:' >> $@
+@@ -232,8 +232,8 @@
  	$(qexec)echo 'Libs: -L$${libdir} -lvpx' >> $@
+ 	$(qexec)echo 'Libs.private: -lm -pthread' >> $@
  	$(qexec)echo 'Cflags: -I$${includedir}' >> $@
 -INSTALL-LIBS-yes += $(LIBSUBDIR)/pkgconfig/vpx.pc
 -INSTALL_MAPS += $(LIBSUBDIR)/pkgconfig/%.pc %.pc
