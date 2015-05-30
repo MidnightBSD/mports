9d257e03b5f8cf078c9e8ccbce0c3c7537af74a8
Lucas Holt <luke@foolishgames.com>
2015-05-30 15:38:27 +0000

    change png

--- print/ghostscript8/files/patch-epag-ert.c
+++ print/ghostscript8/files/patch-epag-ert.c
@@ -1,7 +1,7 @@
 --- epag-3.09/ert.c.orig	2000-07-08 13:33:09.000000000 +0900
 +++ epag/ert.c	2012-02-13 03:38:07.000000000 +0900
 @@ -1,6 +1,8 @@
- /* $Id: patch-epag-ert.c,v 1.1 2012-09-08 21:15:03 laffer1 Exp $ */
+ /* $Id: ert.c,v 1.3 1998/11/20 10:51:12 nari Exp $ */
  
  #include <stdio.h>
 +#include <stdlib.h>
