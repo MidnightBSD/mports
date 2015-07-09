066ac5ae8c1c4f51838085ebf765f7742ac7655b
Lucas Holt <luke@foolishgames.com>
2015-07-09 01:08:35 +0000

    update most print ports

--- print/ttf2pt1/files/patch-ft.c
+++ print/ttf2pt1/files/patch-ft.c
@@ -1,12 +1,20 @@
---- ./ft.c.orig	Wed Dec 31 16:30:50 2003
-+++ ./ft.c	Thu Nov  1 01:02:09 2007
-@@ -12,7 +12,8 @@
+--- ft.c.orig	2003-12-31 22:30:50.000000000 +0100
++++ ft.c	2013-12-16 16:50:37.000000000 +0100
+@@ -12,11 +12,12 @@
  #include <stdlib.h>
  #include <ctype.h>
  #include <sys/types.h>
 -#include <freetype/freetype.h>
+-#include <freetype/ftglyph.h>
+-#include <freetype/ftsnames.h>
+-#include <freetype/ttnameid.h>
+-#include <freetype/ftoutln.h>
 +#include <ft2build.h>
 +#include FT_FREETYPE_H
- #include <freetype/ftglyph.h>
- #include <freetype/ftsnames.h>
- #include <freetype/ttnameid.h>
++#include FT_GLYPH_H
++#include FT_SFNT_NAMES_H
++#include FT_TRUETYPE_IDS_H
++#include FT_OUTLINE_H
+ #include "pt1.h"
+ #include "global.h"
+ 
