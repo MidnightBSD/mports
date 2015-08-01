6bfb821b31b9a03a963f0649124816ac015c1d2c
Lucas Holt <luke@foolishgames.com>
2015-08-01 13:32:27 +0000

    fix path

--- Mk/extensions/libedit.mk
+++ Mk/extensions/libedit.mk
@@ -9,7 +9,7 @@
 
 .if !defined(_INCLUDE_USES_LIBEDIT_MK)
 _INCLUDE_USES_LIBEDIT_MK=	yes
-.include "${EXTENSIONSDIR}/localbase.mk"
+.include "${MPORTEXTENSIONS}/localbase.mk"
 
 LIB_DEPENDS+=	libedit.so.0:${PORTSDIR}/devel/libedit
 .endif
