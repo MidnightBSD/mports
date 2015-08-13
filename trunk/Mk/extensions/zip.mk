812ca6fd272f3efa15f457443a536215d21dd3bd
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2015-08-13 02:41:03 +0000

    change handling of infozip extension and fix typo in include check so we dont do it twice.

--- trunk/Mk/extensions/zip.mk
+++ trunk/Mk/extensions/zip.mk
@@ -5,22 +5,21 @@
 # Feature:	zip
 # Usage:	USES=zip[:infozip]
 
-.if !defined(_INCLUDE_USES_ZIP_Mk)
+.if !defined(_INCLUDE_USES_ZIP_MK)
 _INCLUDE_USES_ZIP_MK=	yes
-
-zip_ARGS?=	none
 
 EXTRACT_SUFX?=	.zip
 
 EXTRACT_BEFORE_ARGS?=	-qo
 EXTRACT_AFTER_ARGS?=	-d ${WRKDIR}
 
-.if !defined(zip_ARGS) || ${zip_ARGS} == none || ${zip_ARGS}x == x
+.if empty(zip_ARGS)
 EXTRACT_CMD?=		${UNZIP_NATIVE_CMD}
-.elif ${zip_ARGS} == 'infozip'
+.elif ${zip_ARGS} == "infozip"
 EXTRACT_DEPENDS+=	${UNZIP_CMD}:${PORTSDIR}/archivers/unzip
 EXTRACT_CMD?=		${UNZIP_CMD}
 .else
 IGNORE=	Incorrect 'USES+=zip:${zip_ARGS}' expecting 'USES+=zip[:infozip]'
 .endif
+
 .endif
