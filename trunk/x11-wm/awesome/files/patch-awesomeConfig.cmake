dc0562e6b051ef8fb160248515a6299b33515ede
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2015-08-19 01:10:13 +0000

    awesome 3.5.6

--- trunk/x11-wm/awesome/files/patch-awesomeConfig.cmake
+++ trunk/x11-wm/awesome/files/patch-awesomeConfig.cmake
@@ -1,16 +1,14 @@
---- awesomeConfig.cmake.orig	2010-01-04 06:50:02.000000000 -0200
-+++ awesomeConfig.cmake	2010-01-04 08:06:18.000000000 -0200
-@@ -13,7 +13,8 @@
- option(WITH_DBUS "build with D-BUS" ON)
+--- awesomeConfig.cmake.orig	2012-12-21 21:18:18.000000000 -0200
++++ awesomeConfig.cmake	2013-03-08 11:46:07.000000000 -0300
+@@ -14,6 +14,7 @@
  option(GENERATE_MANPAGES "generate manpages" ON)
  option(COMPRESS_MANPAGES "compress manpages" ON)
--option(GENERATE_LUADOC "generate luadoc" ON)
-+option(GENERATE_LUADOC "generate luadoc" OFF)
+ option(GENERATE_DOC "generate API documentation" ON)
 +option(INSTALL_PORTDOCS "install port docs" ON)
  
- link_directories(/usr/local/lib)
- 
-@@ -61,7 +62,7 @@
+ # {{{ CFLAGS
+ add_definitions(-O1 -std=gnu99 -ggdb3 -rdynamic -fno-strict-aliasing -Wall -Wextra
+@@ -58,7 +59,7 @@
  # theme graphics
  a_find_program(CONVERT_EXECUTABLE convert TRUE)
  # doxygen
@@ -19,29 +17,12 @@
  # pkg-config
  include(FindPkgConfig)
  # lua 5.1
-@@ -167,6 +168,8 @@
- 
- # Check for libev
- a_find_library(LIB_EV ev)
-+# Check for libexecinfo on non Glibc system
-+a_find_library(LIB_EXECINFO execinfo)
- 
- # Error check
- if(NOT LUA51_FOUND AND NOT LUA50_FOUND) # This is a workaround to a cmake bug
-@@ -177,6 +180,7 @@
-     ${AWESOME_COMMON_REQUIRED_LDFLAGS}
-     ${AWESOME_REQUIRED_LIBRARIES}
-     ${LIB_EV}
-+    ${LIB_EXECINFO}
-     ${LUA_LIBRARIES})
- 
- set(AWESOME_REQUIRED_INCLUDE_DIRS
-@@ -245,7 +249,7 @@
+@@ -256,7 +257,7 @@
  if(DEFINED AWESOME_MAN_PATH)
     set(AWESOME_MAN_PATH ${AWESOME_MAN_PATH} CACHE PATH "awesome manpage directory")
  else()
--   set(AWESOME_MAN_PATH ${PREFIX}/share/man CACHE PATH "awesome manpage directory")
-+   set(AWESOME_MAN_PATH ${PREFIX}/man CACHE PATH "awesome manpage directory")
+-   set(AWESOME_MAN_PATH ${CMAKE_INSTALL_PREFIX}/share/man CACHE PATH "awesome manpage directory")
++   set(AWESOME_MAN_PATH ${CMAKE_INSTALL_PREFIX}/man CACHE PATH "awesome manpage directory")
  endif()
  
  # Hide to avoid confusion
