7c4094f1ff8539b850abc70c4bf9b499e6272c84
Lucas Holt <luke@foolishgames.com>
2015-08-29 19:16:50 +0000

    empty check ninja

--- Mk/extensions/ninja.mk
+++ Mk/extensions/ninja.mk
@@ -11,7 +11,7 @@
 .if !defined(_INCLUDE_USES_NINJA_MK)
 _INCLUDE_USES_NINJA_MK=	yes
 
-.if defined(ninja_ARGS)
+.if !empty(ninja_ARGS)
 IGNORE=	Incorrect 'USES+= ninja:${ninja_ARGS}' ninja takes no arguments
 .endif
 
@@ -25,6 +25,8 @@ CMAKE_ARGS+=	-GNinja
 MAKEFILE=
 MAKE_CMD=	ninja
 MAKE_FLAGS=
+# Set a minimal job of 1
+_MAKE_JOBS=	-j${MAKE_JOBS_NUMBER}
 _DESTDIR_VIA_ENV=	yes
 
 .endif
