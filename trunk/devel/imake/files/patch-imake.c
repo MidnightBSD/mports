2ace3bfedca441c3b5c2e6dbb570f759f37f576a
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2015-09-09 01:48:10 +0000

    update imake to 1.0.7

--- trunk/devel/imake/files/patch-imake.c
+++ trunk/devel/imake/files/patch-imake.c
@@ -1,5 +1,14 @@
---- imake.c.orig	2013-07-23 06:53:04.000000000 +0200
-+++ imake.c	2013-08-31 14:57:48.000000000 +0200
+--- imake.c.orig	2013-08-17 06:11:50.000000000 -0400
++++ imake.c	2015-09-08 21:46:29.953635469 -0400
+@@ -146,7 +146,7 @@
+ 
+ #include "config.h"
+ 
+-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
++#if defined(__MidnightBSD__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+ /* This needs to be before _POSIX_SOURCE gets defined */
+ # include <sys/param.h>
+ # include <sys/types.h>
 @@ -531,6 +531,14 @@
  				AddCppArg(p);
  			}
@@ -15,36 +24,61 @@
  	if ((p = getenv("IMAKECPP")))
  		cpp = p;
  	if ((p = getenv("IMAKEMAKE")))
-@@ -1142,29 +1150,17 @@
-   int mib[2];
-   size_t len;
-   int osrel = 0;
+@@ -1135,36 +1143,11 @@
+ }
+ #endif
+ 
+-#if defined __FreeBSD__
++#if defined(__FreeBSD__) || defined(__MidnightBSD__)
+ static void
+ get_binary_format(FILE *inFile)
+ {
+-  int mib[2];
+-  size_t len;
+-  int osrel = 0;
 -  FILE *objprog = NULL;
-   int iself = 0;
+-  int iself = 0;
 -  char buf[10];
 -  char cmd[PATH_MAX];
- 
-   mib[0] = CTL_KERN;
-   mib[1] = KERN_OSRELDATE;
-   len = sizeof(osrel);
-   sysctl(mib, 2, &osrel, &len, NULL, 0);
+-
+-  mib[0] = CTL_KERN;
+-  mib[1] = KERN_OSRELDATE;
+-  len = sizeof(osrel);
+-  sysctl(mib, 2, &osrel, &len, NULL, 0);
 -  if (CrossCompiling) {
 -      strcpy (cmd, CrossCompileDir);
 -      strcat (cmd, "/");
 -      strcat (cmd,"objformat");
 -  } else
 -      strcpy (cmd, "objformat");
- 
+-
 -  if (osrel >= 300004 &&
 -      (objprog = popen(cmd, "r")) != NULL &&
 -      fgets(buf, sizeof(buf), objprog) != NULL &&
 -      strncmp(buf, "elf", 3) == 0)
-+  if (osrel < 300004)
-+    iself = 0;
-+  else
-     iself = 1;
+-    iself = 1;
 -  if (objprog)
 -    pclose(objprog);
++  int  iself = 1;
  
    fprintf(inFile, "#define DefaultToElfFormat %s\n", iself ? "YES" : "NO");
  }
+@@ -1342,7 +1325,8 @@
+ #if defined(linux) || \
+      defined(__NetBSD__) || \
+      defined(__OpenBSD__) || \
+-     defined(__FreeBSD__) || \
++     defined(__FreeBSD__) || \ 
++     defined(__MidnightBSD__) || \
+      defined(__DragonFly__) || \
+      defined(__APPLE__) || \
+      defined(__CYGWIN__) || \
+@@ -1450,7 +1434,7 @@
+ 	  name = &uts_name;
+       }
+ #  endif
+-#  ifdef __FreeBSD__
++#  if defined(__FreeBSD__) || defined(__MidnightBSD__)
+        /* Override for compiling in chroot of other OS version, such as
+         * in the bento build cluster.
+         */
--- trunk/devel/imake/files/patch-imakemdep.h
+++ trunk/devel/imake/files/patch-imakemdep.h
@@ -1,11 +1,70 @@
---- ./imakemdep.h.orig	2012-03-08 06:47:32.000000000 +0100
-+++ ./imakemdep.h	2013-06-12 20:22:52.058999526 +0200
-@@ -380,7 +383,7 @@
+--- imakemdep.h.orig	2013-08-17 06:11:06.000000000 -0400
++++ imakemdep.h	2015-09-08 21:38:58.150230168 -0400
+@@ -301,7 +301,7 @@
+ #   if defined(__386BSD__)
+ #    define DEFAULT_CPP "/usr/libexec/cpp"
+ #   endif
+-#   if defined(__FreeBSD__)  || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
++#   if defined(__FreeBSD__)  || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__MidnightBSD__)
+ #    define USE_CC_E
+ #   endif
+ #   if defined(__sgi) && defined(__ANSI_CPP__)
+@@ -361,11 +361,12 @@
+ #   endif
+ #   if defined(__386BSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || \
+     defined(__FreeBSD__) || defined(__DragonFly__) || defined(MACH) || \
++    defined(__MidnightBSD__) || \
+     defined(linux) || defined(__GNU__) || defined(__bsdi__) || \
      defined(__GNUC__) || defined(__GLIBC__)
  #    ifdef __i386__
  	"-D__i386__",
 -#     if defined(__GNUC__) && (__GNUC__ >= 3)
-+#     if defined(__GNUC__) && (__GNUC__ >= 3) && !defined(__FreeBSD__)
++#     if defined(__GNUC__) && (__GNUC__ >= 3) && !defined(__FreeBSD__) && !defined(__MidnightBSD__)
  	"-m32",
  #     endif
  #    endif
+@@ -839,14 +840,14 @@
+ #    define DEFAULT_OS_MINOR_REV   "r %*d.%[0-9]"
+ #    define DEFAULT_OS_TEENY_REV   "v %[0-9]"
+ /* # define DEFAULT_OS_NAME        "srm %[^\n]" */ /* Not useful on ISC */
+-#   elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__OpenBSD__) || defined(__DragonFly__)
++#   elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__MidnightBSD__)
+ /* BSD/OS too? */
+ /* uname -r returns "x.y[.z]-mumble", e.g. "2.1.5-RELEASE" or "2.2-0801SNAP" */
+ #    define DEFAULT_OS_MAJOR_REV   "r %[0-9]"
+ #    define DEFAULT_OS_MINOR_REV   "r %*d.%[0-9]"
+ #    define DEFAULT_OS_TEENY_REV   "r %*d.%*d.%[0-9]"
+ #    define DEFAULT_OS_NAME        "srm %[^\n]"
+-#    if defined(__FreeBSD__) || defined(__DragonFly__)
++#    if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__MidnightBSD__)
+ /* Use an alternate way to find the teeny version for -STABLE, -SNAP versions */
+ #     ifndef CROSSCOMPILE_CPP
+ #      define DEFAULT_OS_TEENY_REV_FROB(buf, size)			\
+@@ -1281,6 +1282,9 @@
+ #   ifdef __DragonFly__
+ 	{"__DragonFly__", "1"},
+ #   endif
++#   ifdef __MidnightBSD__
++	{"__MidnightBSD__", "1"}, 
++#   endif
+ #   ifdef __FreeBSD__
+ 	{"__FreeBSD__", "1"},
+ #   endif
+@@ -1466,13 +1470,16 @@
+   LinuX,
+   emx,
+   win32,
+-  dragonfly
++  dragonfly,
++  midnightbsd
+ } System;
+ 
+ #   if defined(linux) || defined(__GLIBC__)
+ System sys = LinuX;
+ #   elif defined __FreeBSD__
+ System sys = freebsd;
++#   elif defined __MidnightBSD__
++System sys = freebsd;
+ #   elif defined __NetBSD__
+ System sys = netBSD;
+ #   elif defined __EMX__
