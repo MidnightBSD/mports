c21faf64a4a036e19c1a243a9f7742431bfd9618
Lucas Holt <luke@foolishgames.com>
2015-09-12 02:43:10 +0000

    sdl mixer 1.2.12

--- audio/sdl_mixer/files/patch-SDL_mixer.h
+++ audio/sdl_mixer/files/patch-SDL_mixer.h
@@ -1,6 +1,3 @@
-
-$FreeBSD: ports/audio/sdl_mixer/files/patch-SDL_mixer.h,v 1.1 2001/05/08 08:20:10 sobomax Exp $
-
 --- SDL_mixer.h	2001/05/08 07:44:56	1.1
 +++ SDL_mixer.h	2001/05/08 07:48:13
 @@ -222,6 +222,8 @@
