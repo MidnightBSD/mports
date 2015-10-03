7e5f5571b52d323424bdfb9986fce0dff32c21d1
Lucas Holt <luke@foolishgames.com>
2015-10-03 19:44:36 +0000

    fix w3m build

--- www/w3m/files/patch-w3mimg_fb_fb.h
+++ www/w3m/files/patch-w3mimg_fb_fb.h
@@ -1,7 +1,7 @@
 --- w3mimg/fb/fb.h.orig	2003-07-08 00:48:17.000000000 +0900
 +++ w3mimg/fb/fb.h	2007-11-23 02:43:22.000000000 +0900
 @@ -1,7 +1,11 @@
- /* $Id: patch-w3mimg_fb_fb.h,v 1.1 2013-07-16 12:32:55 laffer1 Exp $ */
+ /* $Id: fb.h,v 1.7 2003/07/07 15:48:17 ukai Exp $ */
  #ifndef fb_header
  #define fb_header
 +#if defined(__linux__)
