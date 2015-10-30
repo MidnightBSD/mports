60b95b5e8fc374d3f44271edcca65ec0b3f42373
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2015-10-30 01:42:56 +0000

    libev 4.20

--- trunk/devel/libev/files/patch-ev.c
+++ trunk/devel/libev/files/patch-ev.c
@@ -1,15 +1,11 @@
---- ./ev.c.orig	2011-02-09 00:17:37.000000000 +0100
-+++ ./ev.c	2011-08-19 17:01:35.000000000 +0200
-@@ -1591,10 +1591,12 @@
+--- ev.c.orig	2015-07-13 11:32:52 UTC
++++ ev.c
+@@ -2603,7 +2603,7 @@ ev_recommended_backends (void) EV_THROW
+ {
    unsigned int flags = ev_supported_backends ();
  
- #ifndef __NetBSD__
-+#ifndef __FreeBSD__
+-#ifndef __NetBSD__
++#if !defined(__NetBSD__) && !defined(__FreeBSD__) && !defined(__MidnightBSD__)
    /* kqueue is borked on everything but netbsd apparently */
    /* it usually doesn't work correctly on anything but sockets and pipes */
    flags &= ~EVBACKEND_KQUEUE;
- #endif
-+#endif
- #ifdef __APPLE__
-   /* only select works correctly on that "unix-certified" platform */
-   flags &= ~EVBACKEND_KQUEUE; /* horribly broken, even for sockets */
