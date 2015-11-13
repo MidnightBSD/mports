174319537769ac2ca0687906a487e3940b14ec9a
Lucas Holt <luke@foolishgames.com>
2015-11-13 02:37:34 +0000

    python twitter tools 1.15.0

--- net/py-twitter-tools/files/patch-setup.py
+++ net/py-twitter-tools/files/patch-setup.py
@@ -1,11 +1,22 @@
---- ./setup.py.orig	2013-05-25 10:38:02.218078478 +1000
-+++ ./setup.py	2013-05-25 10:38:10.792279089 +1000
-@@ -33,7 +33,7 @@
-       license='MIT License',
-       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+--- setup.py.orig	2014-12-26 02:42:01 UTC
++++ setup.py
+@@ -9,6 +9,10 @@ install_requires = [
+ if sys.version_info < (2,7):
+     install_requires.append('argparse')
+ 
++tests_require = [
++    'nose',
++]
++
+ setup(name='twitter',
+       version=version,
+       description="An API and command-line toolset for Twitter (twitter.com)",
+@@ -39,6 +43,8 @@ setup(name='twitter',
        include_package_data=True,
--      zip_safe=True,
-+      zip_safe=False,
+       zip_safe=True,
        install_requires=install_requires,
++      tests_require=tests_require,
++      test_suite = 'nose.collector',
        entry_points="""
        # -*- Entry points: -*-
+       [console_scripts]
