e3a96ad1876ca3005414500c7bd073afbe7432ef
Lucas Holt <luke@foolishgames.com>
2016-02-05 16:33:08 +0000

    fusefs libs 2.9.5

--- sysutils/fusefs-libs/files/patch-lib_mount__util.h
+++ sysutils/fusefs-libs/files/patch-lib_mount__util.h
@@ -1,6 +1,6 @@
---- lib/mount_util.h.orig	2008-02-04 00:24:07.000000000 -0500
-+++ lib/mount_util.h	2008-02-04 00:24:20.000000000 -0500
-@@ -14,4 +14,3 @@
+--- lib/mount_util.h.orig	2015-05-22 09:24:02 UTC
++++ lib/mount_util.h
+@@ -16,4 +16,3 @@ int fuse_mnt_umount(const char *progname
  char *fuse_mnt_resolve_path(const char *progname, const char *orig);
  int fuse_mnt_check_empty(const char *progname, const char *mnt,
  			 mode_t rootmode, off_t rootsize);
