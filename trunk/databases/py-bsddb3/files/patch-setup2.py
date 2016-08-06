5440a1f17282b96a9c7a3f1296b909c02dcb81e1
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2016-08-06 00:49:54 +0000

    6.1.1

--- trunk/databases/py-bsddb3/files/patch-setup2.py
+++ trunk/databases/py-bsddb3/files/patch-setup2.py
@@ -1,6 +1,6 @@
---- setup2.py.orig	2011-03-08 22:16:14.000000000 +0800
-+++ setup2.py	2011-03-15 16:00:42.000000000 +0800
-@@ -53,10 +53,7 @@
+--- setup2.py.orig	2016-02-08 21:14:26 UTC
++++ setup2.py
+@@ -81,11 +81,7 @@ if (sys.version_info[0] < 3) and (sys.ve
              category=DeprecationWarning)
  
  
@@ -8,11 +8,12 @@
 -  from setuptools import setup, Extension
 -except :
 -  from distutils.core import setup, Extension
+-
 +from distutils.core import setup, Extension
- 
  from distutils.dep_util import newer
  import distutils.ccompiler
-@@ -247,8 +244,8 @@
+ 
+@@ -288,8 +284,8 @@ if os.name == 'posix':
          incdir = os.path.join(BERKELEYDB_DIR, 'include')
      if not libdir:
          libdir = os.path.join(BERKELEYDB_DIR, 'lib')
@@ -23,3 +24,12 @@
      else:
          if debug: print "LIBS already contains '-ldb' not adding our own", "'-l"+dblib+"'"
          libname = []
+@@ -328,7 +324,7 @@ if os.name == 'posix':
+ 
+     # read db.h to figure out what version of Berkeley DB this is
+     ver = None
+-    with open(os.path.join(incdir, 'db.h'), 'r') as f :
++    with open('%%BDB_INCLUDE_DIR%%/db.h', 'r') as f :
+         db_h_lines = f.readlines()
+     db_ver_re = re.compile(
+         r'^#define\s+DB_VERSION_STRING\s.*Berkeley DB (\d+\.\d+).*')
