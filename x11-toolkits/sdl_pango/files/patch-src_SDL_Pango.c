ee9b27cfc9b61b664c785c2a2e48478d6233648e
Lucas Holt <luke@foolishgames.com>
2016-08-12 07:41:29 +0000

    attempt to patch patch

--- x11-toolkits/sdl_pango/files/patch-src_SDL_Pango.c
+++ x11-toolkits/sdl_pango/files/patch-src_SDL_Pango.c
@@ -1,5 +1,5 @@
---- src/SDL_Pango.c~	2013-12-31 14:26:31.000000000 +0000
-+++ src/SDL_Pango.c	2013-12-31 14:22:36.000000000 +0000
+--- src/SDL_Pango.c.orig	2016-08-12 03:36:39.011601549 -0400
++++ src/SDL_Pango.c	2016-08-12 03:37:47.862601477 -0400
 @@ -330,6 +330,11 @@
      @param *rect [in] Draw on this area
      @param baseline [in] Horizontal location of glyphs
