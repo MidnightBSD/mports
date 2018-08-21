b8a80cbbf54d2687a058726e2027a92428d0b1e9
Lucas Holt <luke@foolishgames.com>
2018-08-21 18:19:52 +0000

    modernize

--- Mk/extensions/gcc.mk
+++ Mk/extensions/gcc.mk
@@ -38,7 +38,7 @@ Gcc_Include_MAINTAINER=		ports@MidnightBSD.org
 
 # All GCC versions supported by the ports framework.  Keep them in
 # ascending order and in sync with the table below. 
-GCCVERSIONS=	040200 040600 040700 040800 040900 050000 060000
+GCCVERSIONS=	040200 040600 040700 040800 040900 050000 060000 070000
 
 # The first field is the OSVERSION in which it disappeared from the base.
 # The second field is the version as USE_GCC would use.
