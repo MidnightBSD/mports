7c2018c437aad59cd7841b183b2984e184fddfe0
laffer1 <laffer1@c872e0db-cc11-e311-ab8b-002590791c19>
2018-08-29 15:07:39 +0000

    Implement target ordering similarly to the method used in FreeBSD ports. This allows us some flexibility in how we handle ports that need to inject custom targets.

--- trunk/Mk/scripts/functions.sh
+++ trunk/Mk/scripts/functions.sh
@@ -23,7 +23,7 @@ parse_plist() {
 	cwd=${PREFIX}
 	cwd_save=
 	commented_cwd=
-	while read line; do
+	while read -r line; do
 		# Handle deactivated OPTIONS. Treat "@comment file" as being in
 		# the plist so it does not show up as an orphan. PLIST_SUB uses
 		# a @comment to deactive files. XXX: It would be better to
@@ -69,39 +69,44 @@ parse_plist() {
 			esac
 		;;
 		# Handle [file] Keywords
-		@info\ *|@shell\ *)
+		@info\ *|@shell\ *|@xmlcatmgr\ *)
 			set -- $line
 			shift
-			case "$@" in
-			/*) echo "${comment}$@" ;;
-			*) echo "${comment}${cwd}/$@" ;;
+			case "$*" in
+			/*) echo "${comment}$*" ;;
+			*) echo "${comment}${cwd}/$*" ;;
 			esac
 		;;
 		@sample\ *)
 			set -- $line
 			shift
-			# Ignore the actual file if it is in stagedir
-			case "$@" in
-			/*)
-			echo "@comment ${@%.sample}"
-			echo "${comment}$@"
-			;;
-			*)
-			echo "@comment ${cwd}/${@%.sample}"
-			echo "${comment}${cwd}/$@"
-			;;
+			sample_file=$1
+			target_file=${1%.sample}
+			if [ $# -eq 2 ]; then
+				target_file=$2
+			fi
+			case "${sample_file}" in
+			/*) ;;
+			*) sample_file=${cwd}/${sample_file} ;;
 			esac
+			case "${target_file}" in
+			/*) ;;
+			*) target_file=${cwd}/${target_file} ;;
+			esac
+			# Ignore the actual file if it is in stagedir
+			echo "@comment ${target_file}"
+			echo "${comment}${sample_file}"
 		;;
 		# Handle [dir] Keywords
 		@fc\ *|@fcfontsdir\ *|@fontsdir\ *)
 			set -- $line
 			shift
-			case "$@" in
+			case "$*" in
 			/*)
-			echo >&3 "${comment}$@"
+			echo >&3 "${comment}$*"
 			;;
 			*)
-			echo >&3 "${comment}${cwd}/$@"
+			echo >&3 "${comment}${cwd}/$*"
 			;;
 			esac
 		;;
@@ -150,4 +155,115 @@ validate_env() {
 		| fmt >&2
 		exit 1
 	fi
+}
+
+export_ports_env() {
+	local export_vars make_cmd make_env var value uses
+
+	if [ -n "${HAVE_PORTS_ENV:-}" ]; then
+		return 0
+	fi
+
+	validate_env MAKE PORTSDIR
+
+	uses="python compiler:features objc"
+
+	make_env="\
+		_PORTS_ENV_CHECK=1 \
+		PACKAGE_BUILDING=1 \
+		GNU_CONFIGURE=1 \
+		USE_JAVA=1 \
+		USE_LINUX=1 \
+	"
+
+	make_cmd="${make_env}"
+
+	export_vars="$(${MAKE} -f ${PORTSDIR}/Mk/bsd.mport.mk \
+	    -V PORTS_ENV_VARS ${make_env} USES="${uses}")"
+
+	for var in ${export_vars}; do
+		make_cmd="${make_cmd}${make_cmd:+ }-V ${var}=\${${var}:Q}"
+	done
+
+	# Bring in all the vars, but not empty ones.
+	eval "$(${MAKE} -f ${PORTSDIR}/Mk/bsd.port.mk ${make_cmd} \
+		USES="${uses}" | grep -v '=$' | sed -e 's,\\ $,,')"
+	for var in ${export_vars}; do
+		# Export and display non-empty ones.  This is not redundant
+		# with above since we're looping on all vars here; do not
+		# export a var we didn't eval in.
+		value="$(eval echo \$${var})"
+
+		if [ -n "${value}" ]; then
+			# shellcheck disable=SC2163
+			# We want to export the variable which name is in var.
+			export ${var}
+			echo "export ${var}=\"${value}\""
+		fi
+	done
+	export HAVE_PORTS_ENV=1
+	echo "export HAVE_PORTS_ENV=1"
+}
+
+distinfo_data() {
+	local alg file
+
+	alg=$1
+	file=$2
+
+	if [ \( -n "${dp_DISABLE_SIZE}" -a -n "${dp_NO_CHECKSUM}" \) -o ! -f "${dp_DISTINFO_FILE}" ]; then
+		exit
+	fi
+	awk -v alg="$alg" -v file="${file}" \
+		'$1 == alg && $2 == "(" file ")" {print $4}' "${dp_DISTINFO_FILE}"
+}
+
+check_checksum_algorithms() {
+	for alg in ${dp_CHECKSUM_ALGORITHMS}; do
+		eval "alg_executable=\$dp_$alg"
+		if [ -z "$alg_executable" ]; then
+			${dp_ECHO_MSG} "Checksum algorithm $alg: Couldn't find the executable."
+			${dp_ECHO_MSG} "Set $alg=/path/to/$alg in /etc/make.conf and try again."
+			exit 1
+		elif [ ! -x "$alg_executable" ]; then
+			${dp_ECHO_MSG} "Checksum algorithm $alg: $alg_executable is not executable."
+			${dp_ECHO_MSG} "Fix modes, or change $alg=$alg_executable in /etc/make.conf and try again."
+			exit 1
+		fi
+	done
+}
+escape() {
+	echo "$1" | sed -e 's/[&;()!#]/\\&/g'
+}
+unescape() {
+	echo "$1" | sed -e 's/\\//g'
+}
+
+# Fetch vars from the Makefile and set them locally.
+# port_var_fetch ports-mgmt/pkg "" PKGNAME pkgname PKGBASE pkgbase ...
+# the 2nd variable is for passing any wanted make arguments, such as
+# DEPENDS_ARGS.
+port_var_fetch() {
+	local origin="$1"
+	local make_args="$2"
+	local _makeflags _vars
+	local _portvar _var _line
+
+	_makeflags=
+	_vars=
+	shift 2
+	while [ $# -ge 2 ]; do
+		_portvar="$1"
+		_var="$2"
+		_makeflags="${_makeflags}${_makeflags:+ }-V${_portvar}"
+		_vars="${_vars}${_vars:+ }${_var}"
+		shift 2
+	done
+	set -- ${_vars}
+	while read -r _line; do
+		setvar "$1" "${_line}"
+		shift
+	done <<-EOF
+	$(${dp_MAKE} -C "${origin}" ${make_args} ${_makeflags} || echo)
+	EOF
 }
