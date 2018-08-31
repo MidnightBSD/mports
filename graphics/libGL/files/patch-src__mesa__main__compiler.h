0309cff3620e2fb0b0bfbf717f92da34b7ce5d97
Lucas Holt <luke@foolishgames.com>
2018-08-31 19:01:44 +0000

    update mesa ports

--- graphics/libGL/files/patch-src__mesa__main__compiler.h
+++ graphics/libGL/files/patch-src__mesa__main__compiler.h
@@ -1,10 +1,10 @@
---- ./src/mesa/main/compiler.h.orig	2014-01-23 18:02:42.000000000 +0100
-+++ ./src/mesa/main/compiler.h	2014-02-23 22:01:50.000000000 +0100
-@@ -162,6 +162,9 @@
+--- src/mesa/main/compiler.h.orig	2016-11-10 22:05:17 UTC
++++ src/mesa/main/compiler.h
+@@ -56,6 +56,9 @@ extern "C" {
  #if defined(__linux__)
  #include <byteswap.h>
  #define CPU_TO_LE32( x )	bswap_32( x )
-+#elif defined(__MidnightBSD__)
++#elif defined(__FreeBSD__)
 +#include <sys/endian.h>
 +#define CPU_TO_LE32( x )	bswap32( x )
  #elif defined(__APPLE__)
