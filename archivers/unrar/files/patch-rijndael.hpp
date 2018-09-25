ff171e43983fa4d4aff80b5b71dff6b273799fc5
Lucas Holt <luke@foolishgames.com>
2018-09-25 20:36:10 +0000

    update to 5.50

--- archivers/unrar/files/patch-rijndael.hpp
+++ archivers/unrar/files/patch-rijndael.hpp
@@ -1,24 +1,20 @@
---- rijndael.hpp.orig	2012-01-09 14:46:08.000000000 +0100
-+++ rijndael.hpp	2012-04-05 22:42:56.000000000 +0200
-@@ -18,15 +18,21 @@ class Rijndael
-   public:
-     enum Direction { Encrypt , Decrypt };
+--- rijndael.hpp.orig	2017-04-28 17:28:47 UTC
++++ rijndael.hpp
+@@ -16,6 +16,9 @@
+ class Rijndael
+ { 
    private:
-+#ifndef OPENSSL_AES
-     void keySched(byte key[_MAX_KEY_COLUMNS][4]);
-     void keyEncToDec();
-     void encrypt(const byte a[16], byte b[16]);
-     void decrypt(const byte a[16], byte b[16]);
-     void GenerateTables();
-+#endif
- 
 +#ifdef OPENSSL_AES
 +    EVP_CIPHER_CTX ctx;
-+#else
-     Direction m_direction;
-     byte     m_initVector[MAX_IV_SIZE];
-     byte     m_expandedKey[_MAX_ROUNDS+1][4][4];
-+#endif
-   public:
-     Rijndael();
-     void init(Direction dir,const byte *key,byte *initVector);
++#else // OPENSSL_AES
+ #ifdef USE_SSE
+     void blockEncryptSSE(const byte *input,size_t numBlocks,byte *outBuffer);
+     void blockDecryptSSE(const byte *input, size_t numBlocks, byte *outBuffer);
+@@ -25,6 +28,7 @@ class Rijndael
+     void keySched(byte key[_MAX_KEY_COLUMNS][4]);
+     void keyEncToDec();
+     void GenerateTables();
++#endif // OPENSSL_AES
+ 
+     // RAR always uses CBC, but we may need to turn it off when calling
+     // this code from other archive formats with CTR and other modes.
