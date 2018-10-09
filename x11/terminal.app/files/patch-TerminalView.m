20d1999bd43567cfc5457813e80fa1bf966bc052
Lucas Holt <luke@foolishgames.com>
2018-10-09 00:20:02 +0000

    update terminal.app to 0.9.9

--- x11/terminal.app/files/patch-TerminalView.m
+++ x11/terminal.app/files/patch-TerminalView.m
@@ -1,15 +1,15 @@
---- TerminalView.m.orig	2012-03-06 17:54:33.000000000 -0500
-+++ TerminalView.m
-@@ -33,7 +33,7 @@ activated */
- #  include <termios.h>
+--- TerminalView.m.orig	2017-08-02 13:17:43.000000000 -0400
++++ TerminalView.m	2018-10-08 20:18:21.177517000 -0400
+@@ -34,7 +34,7 @@
  #  include <pcap.h>
+ #  include <util.h>
  #define TCSETS TIOCSETA
 -#elif defined(__FreeBSD__)
 +#elif defined(__FreeBSD__) || defined(__DragonFly__) || defined(__MidnightBSD__)
  #  include <sys/types.h>
  #  include <sys/ioctl.h>
  #  include <termios.h>
-@@ -52,7 +52,7 @@ activated */
+@@ -53,7 +53,7 @@
  #include <sys/types.h>
  #include <unistd.h>
  #include <fcntl.h>
@@ -18,11 +18,3 @@
  #if !(defined (__NetBSD__)) && !(defined (__SOLARIS__)) && !(defined(__OpenBSD__))
  #  include <pty.h>
  #endif
-@@ -73,6 +73,7 @@ activated */
- #include <AppKit/NSGraphics.h>
- #include <AppKit/NSScroller.h>
- #include <AppKit/DPSOperators.h>
-+#include <AppKit/NSButtonCell.h>
- 
- #include "TerminalView.h"
- 
