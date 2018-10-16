4f11022014560ae93b23dc8b374aa3e20cd85a29
Lucas Holt <luke@foolishgames.com>
2018-10-16 15:02:08 +0000

    update to 3.39

--- security/nss/files/patch-lib_softoken_pkcs11c.c
+++ security/nss/files/patch-lib_softoken_pkcs11c.c
@@ -1,22 +1,22 @@
---- lib/softoken/pkcs11c.c.orig	2010-05-05 14:36:05.000000000 +0000
-+++ lib/softoken/pkcs11c.c	2010-05-05 14:37:25.000000000 +0000
-@@ -4602,9 +4602,6 @@
- 	    break;
+--- lib/softoken/pkcs11c.c.orig	2018-08-31 12:55:53 UTC
++++ lib/softoken/pkcs11c.c
+@@ -5772,9 +5772,6 @@ sftk_unwrapPrivateKey(SFTKObject *key, SECItem *bpki)
+             break;
          case NSSLOWKEYDSAKey:
- 	    keyType = CKK_DSA;
--	    crv = (sftk_hasAttribute(key, CKA_NETSCAPE_DB)) ? CKR_OK :
--						CKR_KEY_TYPE_INCONSISTENT;
--	    if(crv != CKR_OK) break;
- 	    crv = sftk_AddAttributeType(key, CKA_KEY_TYPE, &keyType, 
- 						sizeof(keyType));
- 	    if(crv != CKR_OK) break;
-@@ -4638,9 +4635,6 @@
- #ifdef NSS_ENABLE_ECC
+             keyType = CKK_DSA;
+-            crv = (sftk_hasAttribute(key, CKA_NETSCAPE_DB)) ? CKR_OK : CKR_KEY_TYPE_INCONSISTENT;
+-            if (crv != CKR_OK)
+-                break;
+             crv = sftk_AddAttributeType(key, CKA_KEY_TYPE, &keyType,
+                                         sizeof(keyType));
+             if (crv != CKR_OK)
+@@ -5814,9 +5811,6 @@ sftk_unwrapPrivateKey(SFTKObject *key, SECItem *bpki)
+         /* what about fortezza??? */
          case NSSLOWKEYECKey:
- 	    keyType = CKK_EC;
--	    crv = (sftk_hasAttribute(key, CKA_NETSCAPE_DB)) ? CKR_OK :
--						CKR_KEY_TYPE_INCONSISTENT;
--	    if(crv != CKR_OK) break;
- 	    crv = sftk_AddAttributeType(key, CKA_KEY_TYPE, &keyType, 
- 						sizeof(keyType));
- 	    if(crv != CKR_OK) break;
+             keyType = CKK_EC;
+-            crv = (sftk_hasAttribute(key, CKA_NETSCAPE_DB)) ? CKR_OK : CKR_KEY_TYPE_INCONSISTENT;
+-            if (crv != CKR_OK)
+-                break;
+             crv = sftk_AddAttributeType(key, CKA_KEY_TYPE, &keyType,
+                                         sizeof(keyType));
+             if (crv != CKR_OK)
