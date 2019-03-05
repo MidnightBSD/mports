91e98093520ff3d0599631a91ac134c36b2dd7c8
Lucas Holt <luke@foolishgames.com>
2019-03-04 20:14:54 -0500

    Refactor perl extension to match others

--- Mk/extensions/perl5.mk
+++ Mk/extensions/perl5.mk
@@ -1,49 +1,44 @@
 # $MidnightBSD$
 #
 # Provide support to use perl5
-
-.if !defined(_POSTMKINCLUDED) || !defined(Perl_Pre_Include)
-
-Perl_Pre_Include=			perl.mk
-Perl_Include_MAINTAINER=	ctriv@MidnightBSD.org
-
-# This file contains the glue that is supposed to make your life easier when
-# dealing with ports of perl related software, specifially CPAN modules. It
-# is automatically included when USE_PERL5, PERL_CONFIGURE, or PERL_MODBUILD
-# is defined in the port's makefile.
 #
-##
-# USE_PERL5		- If set, this port uses perl5 in one or more of the extract,
-#				  patch, build, install or run phases.
-# USE_PERL5_BUILD
-#				- If set, this port uses perl5 in one or more of the extract,
-#				  patch, build or install phases.
-# USE_PERL5_RUN	- If set, this port uses perl5 for running.
-# PERL5			- Set to full path of perl5, either in the system or
-#				  installed from a port.
-# PERL			- Set to full path of perl5, either in the system or
-#				  installed from a port, but without the version number.
-#				  Use this if you need to replace "#!" lines in scripts.
+# PERL5		- Set to full path of perl5, either in the system or
+#		  installed from a port.
+# PERL		- Set to full path of perl5, either in the system or
+#		  installed from a port, but without the version number.
+#		  Use this if you need to replace "#!" lines in scripts.
 # PERL_VERSION	- Full version of perl5 (see below for current value).
-# PERL_VER		- Short version of perl5 (see below for current value).
+#
+# PERL_VER	- Short version of perl5 (major.minor without patchlevel)
+#
 # PERL_LEVEL	- Perl version as an integer of the form MNNNPP, where
-#				  M is major version, N is minor version, and P is
-#				  the patch level. E.g., PERL_VERSION=5.6.1 would give
-#				  a PERL_LEVEL of 500601. This can be used in comparisons
-#				  to determine if the version of perl is high enough,
-#				  whether a particular dependency is needed, etc.
-# PERL_ARCH		- Directory name of architecture dependent libraries
-#				  (value: ${ARCH}-freebsd).
-# PERL_PORT		- Name of the perl port that is installed
-#				  (value: perl5)
-# SITE_PERL		- Directory name where site specific perl packages go.
-#				  This value is added to PLIST_SUB.
-# PERL_MODBUILD	- Use Module::Build to configure, build and install port.
-##
+#		  M is major version, N is minor version, and P is
+#		  the patch level. E.g., PERL_VERSION=5.14.4 would give
+#		  a PERL_LEVEL of 501404. This can be used in comparisons
+#		  to determine if the version of perl is high enough,
+#		  whether a particular dependency is needed, etc.
+# PERL_ARCH	- Directory name of architecture dependent libraries
+#		  (value: mach).
+# PERL_PORT	- Name of the perl port that is installed
+#		  (for example: perl5.24)
+# SITE_PERL	- Directory name where site specific perl packages go.
+#		  This value is added to PLIST_SUB.
+# SITE_ARCH	- Directory name where arch site specific perl packages go.
+#		  This value is added to PLIST_SUB.
+# USE_PERL5	- If set, this port uses perl5 in one or more of the extract,
+#		  patch, build, run or test phases.
+#		  It can also have configure, modbuild and modbuildtiny when
+#		  the port needs to run Makefile.PL, Build.PL and a
+#		  Module::Build::Tiny flavor of Build.PL.
+#
+.if !defined(_INCLUDE_USES_PERL5_MK)
+_INCLUDE_USES_PERL5_MK=	yes
 
-#
-# Common Vars.
-#
+.  if !empty(perl5_ARGS)
+IGNORE=	Incorrect 'USES+=perl5:${perl5_ARGS}' perl5 takes no arguments
+.  endif
+
+USE_PERL5?=	run build
 
 PERL_BRANCH?=		${PERL_VERSION:C/\.[0-9]+$//}
 PERL_PORT?=		perl${PERL_BRANCH}
@@ -67,7 +62,7 @@ _CORE_PERL=		yes
 .else
 PERL=			${PERL_PREFIX}/bin/perl
 CPAN_CMD?=		${PERL_PREFIX}/bin/cpan
-.endif
+.  endif
 
 CONFIGURE_ENV+=	ac_cv_path_PERL=${PERL} ac_cv_path_PERL_PATH=${PERL} \
 		PERL_USE_UNSAFE_INC=1
@@ -140,21 +135,21 @@ PERL_VERSION=	${_DEFAULT_PERL_VERSION}
 
 PERL_VER?=		${PERL_VERSION}
 
-.if !defined(PERL_LEVEL) && defined(PERL_VERSION)
-perl_major=		${PERL_VERSION:C|^([1-9]+).*|\1|}
+.  if !defined(PERL_LEVEL) && defined(PERL_VERSION)
+perl_major=	${PERL_VERSION:C|^([1-9]+).*|\1|}
 _perl_minor=	00${PERL_VERSION:C|^([1-9]+)\.([0-9]+).*|\2|}
-perl_minor=		${_perl_minor:C|^.*(...)|\1|}
-.if ${perl_minor} >= 100
-perl_minor=		${PERL_VERSION:C|^([1-9]+)\.([0-9][0-9][0-9]).*|\2|}
-perl_patch=		${PERL_VERSION:C|^.*(..)|\1|}
-.else # ${perl_minor} < 100
+perl_minor=	${_perl_minor:C|^.*(...)|\1|}
+.    if ${perl_minor} >= 100
+perl_minor=	${PERL_VERSION:C|^([1-9]+)\.([0-9][0-9][0-9]).*|\2|}
+perl_patch=	${PERL_VERSION:C|^.*(..)|\1|}
+.    else # ${perl_minor} < 100
 _perl_patch=	0${PERL_VERSION:C|^([1-9]+)\.([0-9]+)\.*|0|}
-perl_patch=		${_perl_patch:C|^.*(..)|\1|}
-.endif # ${perl_minor} < 100
+perl_patch=	${_perl_patch:C|^.*(..)|\1|}
+.    endif # ${perl_minor} < 100
 PERL_LEVEL=	${perl_major}${perl_minor}${perl_patch}
-.else
+.  else
 PERL_LEVEL=0
-.endif # !defined(PERL_LEVEL) && defined(PERL_VERSION)
+.  endif # !defined(PERL_LEVEL) && defined(PERL_VERSION)
 
 
 
