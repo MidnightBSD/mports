e68c769675351e12d5625d287017285d5ca616bb
Lucas Holt <luke@foolishgames.com>
2019-03-10 19:36:28 -0400

    update curl to 7.64

--- ftp/curl/files/patch-lib-vtls-openssl.c
+++ ftp/curl/files/patch-lib-vtls-openssl.c
@@ -1,20 +1,6 @@
---- lib/vtls/openssl.c.orig	2018-03-22 14:26:38 UTC
+--- lib/vtls/openssl.c.orig	2019-02-05 14:00:12 UTC
 +++ lib/vtls/openssl.c
-@@ -117,12 +117,7 @@
- #define X509_get0_notBefore(x) X509_get_notBefore(x)
- #define X509_get0_notAfter(x) X509_get_notAfter(x)
- #define CONST_EXTS /* nope */
--#ifdef LIBRESSL_VERSION_NUMBER
--static unsigned long OpenSSL_version_num(void)
--{
--  return LIBRESSL_VERSION_NUMBER;
--}
--#else
-+#ifndef LIBRESSL_VERSION_NUMBER
- #define OpenSSL_version_num() SSLeay()
- #endif
- #endif
-@@ -3526,7 +3521,11 @@ static size_t Curl_ossl_version(char *bu
+@@ -3785,7 +3785,11 @@ static size_t Curl_ossl_version(char *bu
    unsigned long ssleay_value;
    sub[2]='\0';
    sub[1]='\0';
