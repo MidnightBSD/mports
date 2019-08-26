658f2f8fdd180b7a8f442498bb7a3f66cc22e72c
Lucas Holt <luke@foolishgames.com>
2019-08-26 11:24:41 -0400

    fix build

---
+++ devel/mk-configure/files/patch-mk_mkc.init.mk
@@ -0,0 +1,10 @@
+--- mk/mkc.init.mk.orig	2019-08-26 11:15:50.075406000 -0400
++++ mk/mkc.init.mk	2019-08-26 11:16:17.356433000 -0400
+@@ -125,6 +125,7 @@
+ ROOT_GROUP.FreeBSD   =	wheel
+ ROOT_GROUP.Darwin    =	wheel
+ ROOT_GROUP.DragonFly =	wheel
++ROOT_GROUP.MidnightBSD = wheel
+ ROOT_GROUP.MirBSD    =	wheel
+ ROOT_GROUP.HP-UX     =	bin
+ ROOT_GROUP.OSF1      =	bin
---
+++ devel/mk-configure/files/patch-mk_mkc_imp.platform.sys.mk
@@ -0,0 +1,10 @@
+--- mk/mkc_imp.platform.sys.mk.orig	2019-08-26 11:16:38.679640000 -0400
++++ mk/mkc_imp.platform.sys.mk	2019-08-26 11:17:00.689613000 -0400
+@@ -53,6 +53,7 @@
+ INSTALL.OpenBSD   =	/usr/bin/install
+ INSTALL.FreeBSD   =	/usr/bin/install
+ INSTALL.DragonFly =	/usr/bin/install
++INSTALL.MidnightBSD    =	/usr/bin/install
+ INSTALL.MirBSD    =	/usr/bin/install
+ INSTALL.Haiku     =	/bin/install
+ 
