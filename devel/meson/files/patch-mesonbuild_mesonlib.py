792b2bfbf0dafae6e3187109621aa37f77bf1a38
Lucas Holt <luke@foolishgames.com>
2019-08-29 12:31:44 -0400

    update meson

--- devel/meson/files/patch-mesonbuild_mesonlib.py
+++ devel/meson/files/patch-mesonbuild_mesonlib.py
@@ -1,12 +1,12 @@
---- mesonbuild/mesonlib.py.orig	2018-10-15 21:08:41.713343000 -0400
-+++ mesonbuild/mesonlib.py	2018-10-15 21:09:12.147491000 -0400
-@@ -292,6 +292,9 @@
- def is_freebsd():
+--- mesonbuild/mesonlib.py.orig	2019-06-16 14:54:18.000000000 -0400
++++ mesonbuild/mesonlib.py	2019-08-29 12:30:53.434739000 -0400
+@@ -463,6 +463,9 @@
+ def is_freebsd() -> bool:
      return platform.system().lower() == 'freebsd'
  
-+def is_midnightbsd():
++def is_midnightbsd() -> bool:
 +    return platform.system().lower() == 'midnightbsd'
 +
- def for_windows(is_cross, env):
-     """
-     Host machine is windows?
+ def exe_exists(arglist: List[str]) -> bool:
+     try:
+         if subprocess.run(arglist, timeout=10).returncode == 0:
