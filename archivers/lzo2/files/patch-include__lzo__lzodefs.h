6afcce0389f632415392e1cccd7d580bb61e6e13
Lucas Holt <luke@foolishgames.com>
2019-10-05 12:36:26 -0400

    lzo22.10

--- archivers/lzo2/files/patch-include__lzo__lzodefs.h
+++ archivers/lzo2/files/patch-include__lzo__lzodefs.h
@@ -1,5 +1,5 @@
---- ./include/lzo/lzodefs.h.orig	2014-06-25 15:47:13.000000000 +0200
-+++ ./include/lzo/lzodefs.h	2014-06-27 06:31:33.000000000 +0200
+--- include/lzo/lzodefs.h.orig	2017-03-01 19:54:14 UTC
++++ include/lzo/lzodefs.h
 @@ -29,6 +29,8 @@
  #ifndef __LZODEFS_H_INCLUDED
  #define __LZODEFS_H_INCLUDED 1
