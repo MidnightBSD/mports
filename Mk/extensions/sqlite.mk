90f5ee58860c4589afbb12a801785e136036d438
Lucas Holt <luke@foolishgames.com>
2019-10-06 13:00:19 -0400

    add sqlite2 to extension

--- Mk/extensions/sqlite.mk
+++ Mk/extensions/sqlite.mk
@@ -1,26 +1,14 @@
-#-*- mode: makefile; tab-width: 4; -*-
-# ex: ts=4
-#
-# $MidnightBSD$ 
-# $FreeBSD: ports/Mk/bsd.database.mk,v 1.14 2006/07/05 02:18:08 linimon Exp $
-#
+# Provide support for SQLite
+# Feature:	sqlite
+# Usage:	USES=	sqlite[:version]
 
-.if defined(_POSTMKINCLUDED) && !defined(Sqlite_Post_Include)
+.if !defined(_INCLUDE_USES_SQLITE_MK)
+_INCLUDE_USES_SQLITE_MK=	yes
 
-Sqlite_Post_Include=		sqlite.mk
-Sqlite_Include_MAINTAINER=	ports@MidnightBSD.org
-
-#
-# USE_SQLITE	- Add dependency on sqlite library. Valid values are:
-#				  3. If version is not specified directly then
-#				  sqlite3 is used (if USE_SQLITE= yes).
-# SQLITE_VER		- Detected sqlite version.
-
-.if ${USE_SQLITE:tl} == "yes"
-_SQLITE_VER=	3
-.else
-_SQLITE_VER=	 ${USE_SQLITE}
+.if !empty(sqlite_ARGS)
+SQLITE_VER=	${sqlite_ARGS}
 .endif
+SQLITE_VER?=	3
 
 
 # USE_SQLITE is specified incorrectly, so mark this as IGNORE
@@ -29,8 +17,11 @@ _SQLITE_VER=	 ${USE_SQLITE}
 LIB_DEPENDS+=	sqlite${_SQLITE_VER}:databases/sqlite${_SQLITE_VER}
 SQLITE_VER=	${_SQLITE_VER}
 .	endif
+.elif ${SQLITE_VER} == 2
+LIB_DEPENDS+=	libsqlite.so:databases/sqlite${SQLITE_VER}
+.else
 .else
 IGNORE=	cannot install: unknown sqlite version: ${_SQLITE_VER}
 .endif
 
-.endif # defined(_POSTMKINCLUDED) && !defined(Sqlite_Post_Include)
+.endif
