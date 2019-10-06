82127a265073f61f067eb6cfb59199cc45eff311
Lucas Holt <luke@foolishgames.com>
2019-10-06 13:22:13 -0400

    mark broken

--- www/qt5-webengine/files/patch-src_3rdparty_gn_build_gen.py
+++ www/qt5-webengine/files/patch-src_3rdparty_gn_build_gen.py
@@ -1,10 +1,12 @@
---- src/3rdparty/gn/build/gen.py.orig	2018-11-13 18:25:11 UTC
-+++ src/3rdparty/gn/build/gen.py
-@@ -41,10 +41,12 @@ class Platform(object):
+--- src/3rdparty/gn/build/gen.py.orig	2019-03-07 04:23:57.000000000 -0500
++++ src/3rdparty/gn/build/gen.py	2019-10-06 13:06:52.762743000 -0400
+@@ -41,10 +41,14 @@
        self._platform = 'aix'
      elif self._platform.startswith('fuchsia'):
        self._platform = 'fuchsia'
 +    elif self._platform.lower().startswith('freebsd'):
++      self._platform = 'bsd'
++    elif self._platform.lower().startswith('midnightbsd'):
 +      self._platform = 'bsd'
  
    @staticmethod
@@ -14,7 +16,7 @@
  
    def platform(self):
      return self._platform
-@@ -67,8 +69,11 @@ class Platform(object):
+@@ -67,8 +71,11 @@
    def is_aix(self):
      return self._platform == 'aix'
  
@@ -27,7 +29,7 @@
  
  
  def main(argv):
-@@ -221,6 +226,7 @@ def WriteGenericNinja(path, static_libraries, executab
+@@ -221,6 +228,7 @@
        'darwin': 'build_mac.ninja.template',
        'linux': 'build_linux.ninja.template',
        'aix': 'build_aix.ninja.template',
@@ -35,7 +37,7 @@
    }[platform.platform()])
  
    with open(template_filename) as f:
-@@ -388,6 +394,11 @@ def WriteGNNinja(path, platform, host, options, linux_
+@@ -388,6 +396,11 @@
      elif platform.is_aix():
        cflags_cc.append('-maix64')
        ldflags.extend(['-maix64', '-pthread'])
@@ -47,7 +49,7 @@
  
      if options.use_lto:
        cflags.extend(['-flto', '-fwhole-program-vtables'])
-@@ -686,7 +697,7 @@ def WriteGNNinja(path, platform, host, options, linux_
+@@ -686,7 +699,7 @@
          'base/strings/string16.cc',
      ])
  
