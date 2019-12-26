ff169d6790ac10ee82935ecb5bde934f441c615c
Lucas Holt <luke@foolishgames.com>
2019-12-26 14:52:51 -0500

    update talloc and tevent

--- devel/tevent/files/patch-buildtools_wafsamba_samba__install.py
+++ devel/tevent/files/patch-buildtools_wafsamba_samba__install.py
@@ -1,6 +1,6 @@
---- ./buildtools/wafsamba/samba_install.py.orig	2015-08-09 19:55:47.819170736 +0000
-+++ ./buildtools/wafsamba/samba_install.py	2015-08-09 19:56:18.727356203 +0000
-@@ -116,7 +116,7 @@
+--- buildtools/wafsamba/samba_install.py.orig	2019-03-21 10:12:32 UTC
++++ buildtools/wafsamba/samba_install.py
+@@ -115,7 +115,7 @@ def install_library(self):
                  inst_name    = bld.make_libname(t.target)
          elif self.vnum:
              vnum_base    = self.vnum.split('.')[0]
@@ -8,4 +8,4 @@
 +            install_name = bld.make_libname(target_name, version=vnum_base)
              install_link = bld.make_libname(target_name, version=vnum_base)
              inst_name    = bld.make_libname(t.target)
-             if not self.private_library:
+             if not self.private_library or not t.env.SONAME_ST:
