2d5f9370f12ea4d0e47c07e819e5820c7b7658e7
Lucas Holt <luke@foolishgames.com>
2020-01-01 14:32:42 -0500

    cleanup

--- Mk/extensions/ldap.mk
+++ Mk/extensions/ldap.mk
@@ -20,7 +20,8 @@ Database_Include_MAINTAINER=		ports@MidnightBSD.org
 # WANT_OPENLDAP_VER
 #				- Maintainer can set an arbitrary version of OpenLDAP by using it.
 # WANT_OPENLDAP_SASL
-#				- If set, this port depends on SASL enabled OpenLDAP client.
+#				- User-defined variable to depend upon SASL-enabled OpenLDAP
+#				  client. Must NOT be set in a port Makefile.
 # IGNORE_OPENLDAP_OPENLDAP
 #				- This variable can be defined if the ports doesn't support
 #				  one or more version of OpenLDAP.
@@ -32,7 +33,6 @@ Database_Include_MAINTAINER=		ports@MidnightBSD.org
 .if defined(USE_OPENLDAP)
 DEFAULT_OPENLDAP_VER?=	24
 # OpenLDAP client versions currently supported
-OPENLDAP23_LIB=		libldap-2.3.so.2
 OPENLDAP24_LIB=		libldap-2.4.so.2
 
 .if exists(${LOCALBASE}/bin/ldapwhoami)
@@ -41,7 +41,7 @@ _OPENLDAP_VER!=	${LOCALBASE}/bin/ldapwhoami -VV 2>&1 | ${GREP} ldapwhoami | ${SE
 
 .if defined(WANT_OPENLDAP_VER)
 .if defined(WITH_OPENLDAP_VER) && ${WITH_OPENLDAP_VER} != ${WANT_OPENLDAP_VER}
-IGNORE=		cannot install: the port wants openldap${WANT_OPENLDAP_VER}-client and you try to install openldap${WITH_OPENLDAP_VER}-client.
+IGNORE=		cannot install: the port wants openldap${WANT_OPENLDAP_VER}-client and you try to install openldap${WITH_OPENLDAP_VER}-client
 .endif
 OPENLDAP_VER=	${WANT_OPENLDAP_VER}
 .elif defined(WITH_OPENLDAP_VER)
@@ -85,7 +85,7 @@ IGNORE=		cannot install: doesn't work with OpenLDAP version: ${OPENLDAP_VER} (Do
 .		endif
 .	endfor
 .endif # IGNORE_WITH_OPENLDAP
-LIB_DEPENDS+=	${OPENLDAP${OPENLDAP_VER}_LIB}:${PORTSDIR}/net/openldap${OPENLDAP_VER}${_OPENLDAP_FLAVOUR}-client
+LIB_DEPENDS+=	${OPENLDAP${OPENLDAP_VER}_LIB}:net/openldap${OPENLDAP_VER}${_OPENLDAP_FLAVOUR}-client
 .else
 IGNORE=		cannot install: unknown OpenLDAP version: ${OPENLDAP_VER}
 .endif # Check for correct libs
