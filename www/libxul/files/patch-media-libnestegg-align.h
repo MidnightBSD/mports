896e6957552cccbca3018f1139f9959477d9dda6
Lucas Holt <luke@foolishgames.com>
2020-05-28 11:08:07 -0400

    fixup

--- www/libxul/files/patch-media-libnestegg-align.h
+++ www/libxul/files/patch-media-libnestegg-align.h
@@ -1,19 +1,16 @@
 --- media/libnestegg/src/align.h.orig	2016-05-12 13:09:57.000000000 -0400
-+++ media/libnestegg/src/align.h	2020-01-26 12:58:42.832617000 -0500
-@@ -12,6 +12,8 @@
-  *	http://www.opensource.org/licenses/bsd-license.php
++++ media/libnestegg/src/align.h	2020-05-28 11:07:17.823057000 -0400
+@@ -24,6 +24,7 @@
+ 
+ #else
+ 
++#if !defined(__MidnightBSD__)
+ /*
+  *	a type with the most strict alignment requirements
   */
- 
-+#include <stddef.h>
-+
- #ifndef _LIBP_ALIGN_H_
- #define _LIBP_ALIGN_H_
- 
-@@ -39,7 +41,9 @@
- 	void (*q)(void);
+@@ -40,6 +41,7 @@
  };
  
-+#if !defined(max_align_t)
  typedef union max_align max_align_t;
 +#endif
  
