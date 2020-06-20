f8447d06e1794288644f65c23ad8abf6b32afb95
Lucas Holt <luke@foolishgames.com>
2020-06-20 14:38:42 -0400

    add python cssselect and cssutils ports

---
+++ www/py-cssselect/files/patch-docs__conf.py
@@ -0,0 +1,10 @@
+--- docs/conf.py.orig	2013-09-15 08:25:25 UTC
++++ docs/conf.py
+@@ -244,7 +244,3 @@ texinfo_documents = [
+ 
+ # How to display URL addresses: 'footnote', 'no', or 'inline'.
+ #texinfo_show_urls = 'footnote'
+-
+-
+-# Example configuration for intersphinx: refer to the Python standard library.
+-intersphinx_mapping = {'http://docs.python.org/': None}
