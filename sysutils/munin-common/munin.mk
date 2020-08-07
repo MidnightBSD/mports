eadb3d03d4e9ace7663ac860569e60c061f64359
Lucas Holt <luke@foolishgames.com>
2020-08-07 15:35:18 -0400

    fix include

--- sysutils/munin-common/munin.mk
+++ sysutils/munin-common/munin.mk
@@ -31,7 +31,6 @@ GROUPS=		munin
 PLIST_SUB=	${MUNIN_DIRS} USER=${USERS} GROUP=${GROUPS}
 SUB_LIST=	${MUNIN_DIRS} USER=${USERS} GROUP=${GROUPS}
 
-USE_PERL5=	yes
 CPE_VENDOR=	munin-monitoring
 
 MAKE_JOBS_UNSAFE=	Try to use things before making thems.
