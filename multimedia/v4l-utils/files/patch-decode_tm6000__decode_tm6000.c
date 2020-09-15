ab9445ace2fa5e90f2cc7f2448ac0a40f523b6ff
Lucas Holt <luke@foolishgames.com>
2020-09-15 19:20:42 -0400

    update v4l

--- multimedia/v4l-utils/files/patch-decode_tm6000__decode_tm6000.c
+++ multimedia/v4l-utils/files/patch-decode_tm6000__decode_tm6000.c
@@ -1,11 +1,20 @@
---- ./decode_tm6000/decode_tm6000.c.orig	2011-02-09 23:04:27.000000000 +0100
-+++ ./decode_tm6000/decode_tm6000.c	2011-06-03 19:08:13.000000000 +0200
-@@ -41,6 +41,8 @@
- 	{ 0, 0, 0, 0, 0, 0 }
- };
+--- ./decode_tm6000/decode_tm6000.c.bak	2019-05-03 13:21:14.000000000 -0400
++++ ./decode_tm6000/decode_tm6000.c	2020-09-15 19:05:15.682417000 -0400
+@@ -16,7 +16,7 @@
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335 USA.
+  */
+-#include "../utils/libv4l2util/v4l2_driver.h"
++#include "utils/libv4l2util/v4l2_driver.h"
+ #include <stdio.h>
+ #include <string.h>
+ #include <argp.h>
+@@ -57,7 +57,7 @@
  
-+#define devname dev_name
-+
- static char outbuf[692224];
- static int debug=0, audio=0, use_mmap=1, nbufs=4;
- static float freq_mhz=193.25;
+ //const char args_doc[]="ARG1 ARG2";
+ 
+-static error_t parse_opt (int key, char *arg, struct argp_state *state)
++static int parse_opt (int key, char *arg, struct argp_state *state)
+ {
+ 	switch (key) {
+ 	case 'a':
