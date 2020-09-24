1247dd9743b2fbc21e0c3d90f368a30945fccae2
Lucas Holt <luke@foolishgames.com>
2020-09-24 13:30:30 -0400

    add bison wrapper

--- Mk/extensions/bison.mk
+++ Mk/extensions/bison.mk
@@ -4,13 +4,17 @@
 #
 # Feature:	bison
 # Usage:	USES=bison or USES=bison:ARGS
-# Valid ARGS:	build (default, implicit), run, both
+# Valid ARGS:	build (default, implicit), run, both, alias, wrappers
 #
+# 'alias'	will automatically create a BINARY_ALIAS to use base
+#		byacc invoked as bison
+# 'wrapper'	will automatically create BINARY_WRAPPER to use base
+#		byacc with a wrapper to mimic a bit more bison
 #
 .if !defined(_INCLUDE_USES_BISON_MK)
 _INCLUDE_USES_BISON_MK=	yes
 
-_BISON_DEPENDS=	bison:${PORTSDIR}/devel/bison
+_BISON_DEPENDS=	bison:devel/bison
 
 .if empty(bison_ARGS)
 bison_ARGS=	build
@@ -23,6 +27,10 @@ RUN_DEPENDS+=	${_BISON_DEPENDS}
 .elif ${bison_ARGS} == "both"
 BUILD_DEPENDS+=	${_BISON_DEPENDS}
 RUN_DEPENDS+=	${_BISON_DEPENDS}
+.elif ${bison_ARGS} == "alias"
+BINARY_ALIAS+=	bison=byacc
+.elif ${bison_ARGS} == "wrapper"
+BINARY_WRAPPERS+=	bison
 .else
 IGNORE=	USES=bison - invalid args: [${bison_ARGS}] specified
 .endif
