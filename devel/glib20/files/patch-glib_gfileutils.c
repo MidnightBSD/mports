26c4df828cc1f84898c151fdc72df2602fa397b4
Lucas Holt <luke@foolishgames.com>
2020-09-28 17:13:51 -0400

    fix some build issues with new glib

--- devel/glib20/files/patch-glib_gfileutils.c
+++ devel/glib20/files/patch-glib_gfileutils.c
@@ -1,9 +1,6 @@
-https://gitlab.gnome.org/GNOME/glib/merge_requests/832
-
-Index: glib/gfileutils.c
---- glib/gfileutils.c.orig
-+++ glib/gfileutils.c
-@@ -259,7 +259,7 @@ g_mkdir_with_parents (const gchar *pathname,
+--- glib/gfileutils.c.orig	2020-09-10 06:42:41.582684000 -0400
++++ glib/gfileutils.c	2020-09-28 17:05:26.669088000 -0400
+@@ -263,7 +263,7 @@
        
        if (!g_file_test (fn, G_FILE_TEST_EXISTS))
  	{
@@ -12,3 +9,12 @@ Index: glib/gfileutils.c
  	    {
  	      int errno_save = errno;
  	      if (errno != ENOENT || !p)
+@@ -1450,7 +1450,7 @@
+            * (https://www.freebsd.org/cgi/man.cgi?query=open&sektion=2#STANDARDS),
+            * and NetBSD uses EFTYPE
+            * (https://netbsd.gw.com/cgi-bin/man-cgi?open+2+NetBSD-current). */
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
++#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__) || defined(__MidnightBSD__)
+           if (saved_errno == EMLINK)
+ #elif defined(__NetBSD__)
+           if (saved_errno == EFTYPE)
