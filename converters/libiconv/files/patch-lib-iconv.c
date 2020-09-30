aa650960714424632ee04238a46635d8fd8061ee
Lucas Holt <luke@foolishgames.com>
2020-09-30 15:03:54 -0400

    update libiconv

--- converters/libiconv/files/patch-lib-iconv.c
+++ converters/libiconv/files/patch-lib-iconv.c
@@ -1,6 +1,6 @@
---- lib/iconv.c.orig
+--- lib/iconv.c.orig	2018-09-17 16:07:16 UTC
 +++ lib/iconv.c
-@@ -598,7 +598,7 @@
+@@ -602,7 +602,7 @@ int _libiconv_version = _LIBICONV_VERSIO
     It wants to define the symbols 'iconv_open', 'iconv', 'iconv_close'.  */
  #define strong_alias(name, aliasname) _strong_alias(name, aliasname)
  #define _strong_alias(name, aliasname) \
