26ea81f559305dc5418fc04aff45f9079b84ae89
Lucas Holt <luke@foolishgames.com>
2020-09-29 21:09:19 -0400

    turn off check when indexing.
    add libgsasl

--- Mk/bsd.mport.mk
+++ Mk/bsd.mport.mk
@@ -258,11 +258,13 @@ show-unsupported-system-error:
 _OSVERSION_MAJOR=      ${OSVERSION:C/([0-9])([0-9][0-9])[0-9]{3}/\1/}
 # Sanity checks for chroot/jail building.
 # Skip if OSVERSION specified on cmdline for testing. Only works for bmake.
+.if !defined(INDEXING)
 .if !defined(.MAKEOVERRIDES) || !${.MAKEOVERRIDES:MOSVERSION}
 .if ${_OSVERSION_MAJOR} != ${_OSRELEASE:R:R}
 .error UNAME_r (${_OSRELEASE}) and OSVERSION (${OSVERSION}) do not agree on major version number.
 .elif ${_OSVERSION_MAJOR} != ${OSREL:R:R}
 .error OSREL (${OSREL}) and OSVERSION (${OSVERSION}) do not agree on major version number.
+.endif
 .endif
 .endif
 
