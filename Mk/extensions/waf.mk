deebb51ca7512b7335ca77f2fc587c06805a014f
Lucas Holt <luke@foolishgames.com>
2020-10-01 14:59:58 -0400

    fix depends

--- Mk/extensions/waf.mk
+++ Mk/extensions/waf.mk
@@ -23,7 +23,7 @@ IGNORE=	Incorrect 'USES+= waf:${waf_ARGS}' waf takes no arguments
 .endif
 
 .if !${USES:Mpython*}
-python_ARGS=	2,build
+python_ARGS=	2.7,build
 .include "${MPORTEXTENSIONS}/python.mk"
 .endif
 
