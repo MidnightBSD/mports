71a57de037e1fc5c684bfc43b184bd1ebc4a8af1
Lucas Holt <luke@foolishgames.com>
2020-10-03 14:24:58 -0400

    Update mDNSrepsonder

--- net/mDNSResponder/files/patch-mDNSPosix-mDNSPosix.c
+++ net/mDNSResponder/files/patch-mDNSPosix-mDNSPosix.c
@@ -1,6 +1,6 @@
---- mDNSPosix/mDNSPosix.c.orig	2016-09-13 22:37:18 UTC
+--- mDNSPosix/mDNSPosix.c.orig	2019-06-27 09:29:40 UTC
 +++ mDNSPosix/mDNSPosix.c
-@@ -654,7 +654,7 @@ mDNSlocal int SetupSocket(struct sockadd
+@@ -971,7 +971,7 @@ mDNSlocal int SetupSocket(struct sockaddr *intfAddr, m
          // to bind to the socket. Our suggestion was to switch the order in which
          // SO_REUSEPORT and SO_REUSEADDR was tested so that SO_REUSEADDR stays on
          // top and SO_REUSEPORT to be used only if SO_REUSEADDR doesn't exist.
@@ -9,22 +9,23 @@
          err = setsockopt(*sktPtr, SOL_SOCKET, SO_REUSEADDR, &kOn, sizeof(kOn));
          #elif defined(SO_REUSEPORT)
          err = setsockopt(*sktPtr, SOL_SOCKET, SO_REUSEPORT, &kOn, sizeof(kOn));
-@@ -755,7 +755,13 @@ mDNSlocal int SetupSocket(struct sockadd
+@@ -1074,16 +1074,28 @@ mDNSlocal int SetupSocket(struct sockaddr *intfAddr, m
      {
          struct ipv6_mreq imr6;
          struct sockaddr_in6 bindAddr6;
 -    #if defined(IPV6_PKTINFO)
 +    #if defined(IPV6_RECVPKTINFO)
-+        if (err == 0)
-+        {
+         if (err == 0)
+         {
 +            err = setsockopt(*sktPtr, IPPROTO_IPV6, IPV6_RECVPKTINFO, &kOn, sizeof(kOn));
 +            if (err < 0) { err = errno; perror("setsockopt - IPV6_RECVPKTINFO"); }
 +        }
 +    #elif defined(IPV6_PKTINFO)
-         if (err == 0)
-         {
++        if (err == 0)
++        {
              err = setsockopt(*sktPtr, IPPROTO_IPV6, IPV6_2292_PKTINFO, &kOn, sizeof(kOn));
-@@ -764,7 +770,13 @@ mDNSlocal int SetupSocket(struct sockadd
+             if (err < 0) { err = errno; perror("setsockopt - IPV6_PKTINFO"); }
+         }
      #else
          #warning This platform has no way to get the destination interface information for IPv6 -- will only work for single-homed hosts
      #endif
