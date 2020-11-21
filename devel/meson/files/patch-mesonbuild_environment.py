ae773d6079e0ae39fc668ff69a17dd12e414ef3d
Lucas Holt <luke@foolishgames.com>
2020-11-21 02:45:06 -0500

    update meson and ninja

--- devel/meson/files/patch-mesonbuild_environment.py
+++ devel/meson/files/patch-mesonbuild_environment.py
@@ -1,20 +1,20 @@
---- mesonbuild/environment.py.orig	2020-07-08 12:43:40.944825000 -0400
-+++ mesonbuild/environment.py	2020-07-08 12:44:13.532935000 -0400
-@@ -324,7 +324,7 @@
+--- mesonbuild/environment.py.orig	2020-10-30 04:17:30.000000000 -0400
++++ mesonbuild/environment.py	2020-11-21 02:44:15.211761000 -0500
+@@ -354,7 +354,7 @@
      """
      if mesonlib.is_windows():
          trial = detect_windows_arch(compilers)
--    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd():
-+    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_midnightbsd():
+-    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_qnx() or mesonlib.is_aix():
++    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_midnightbsd() or mesonlib.is_qnx() or mesonlib.is_aix():
          trial = platform.processor().lower()
      else:
          trial = platform.machine().lower()
-@@ -376,7 +376,7 @@
+@@ -409,7 +409,7 @@
  def detect_cpu(compilers: CompilersDict):
      if mesonlib.is_windows():
          trial = detect_windows_arch(compilers)
--    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd():
-+    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_midnightbsd():
+-    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_aix():
++    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_aix() or mesonlib.is_midnightbsd():
          trial = platform.processor().lower()
      else:
          trial = platform.machine().lower()
