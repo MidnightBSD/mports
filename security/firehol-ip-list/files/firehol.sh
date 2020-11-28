a4cf09a620de841c9c0ed17161043dd837d653a1
Lucas Holt <luke@foolishgames.com>
2020-11-28 15:46:41 -0500

    add a flush

--- security/firehol-ip-list/files/firehol.sh
+++ security/firehol-ip-list/files/firehol.sh
@@ -5,6 +5,7 @@ cd /tmp && fetch "https://iplists.firehol.org/files/firehol_level1.netset"
 configfile='/tmp/firehol_level1.netset'
 [ $# -gt 0 ] && [ -r "$1" ] && configfile="$1"
 
+ipfw -q table 1 flush
 sed -e 's|0.0.0.0/8||' -e 's|10.0.0.0/8||' -e 's/[[:space:]]*#.*// ; /^[[:space:]]*$/d' "$configfile" |
     while read var1 var2 var3 var4; do
       # stuff with var1, etc.
