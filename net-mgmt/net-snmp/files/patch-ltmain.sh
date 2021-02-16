90de80c34a3251e7b535efe330e83f9894167e08
Lucas Holt <luke@foolishgames.com>
2021-02-16 17:51:13 -0500

    update net-snmp

--- net-mgmt/net-snmp/files/patch-ltmain.sh
+++ net-mgmt/net-snmp/files/patch-ltmain.sh
@@ -1,24 +1,24 @@
---- ltmain.sh.orig	2019-10-04 10:42:51.191836000 -0400
-+++ ltmain.sh	2019-10-04 10:43:34.335698000 -0400
-@@ -5597,7 +5597,7 @@
+--- ltmain.sh.orig	2020-08-14 17:41:47.000000000 -0400
++++ ltmain.sh	2021-02-16 16:23:54.072092000 -0500
+@@ -7019,7 +7019,7 @@
  	    # These systems don't actually have a C library (as such)
- 	    test "X$arg" = "X-lc" && continue
+ 	    test X-lc = "X$arg" && continue
  	    ;;
--	  *-*-openbsd* | *-*-freebsd* | *-*-dragonfly*)
-+	  *-*-openbsd* | *-*-freebsd* | *-*-dragonfly* | *-*-midnightbsd*)
+-	  *-*-openbsd* | *-*-freebsd* | *-*-dragonfly* | *-*-bitrig*)
++	  *-*-openbsd* | *-*-freebsd* | *-*-dragonfly* | *-*-bitrig* | *-*-midnightbsd*)
  	    # Do not include libc due to us having libc/libc_r.
- 	    test "X$arg" = "X-lc" && continue
+ 	    test X-lc = "X$arg" && continue
  	    ;;
-@@ -5617,7 +5617,7 @@
+@@ -7039,7 +7039,7 @@
  	  esac
- 	elif test "X$arg" = "X-lc_r"; then
+ 	elif test X-lc_r = "X$arg"; then
  	 case $host in
--	 *-*-openbsd* | *-*-freebsd* | *-*-dragonfly*)
-+	 *-*-openbsd* | *-*-freebsd* | *-*-dragonfly* | *-*-midnightbsd*)
+-	 *-*-openbsd* | *-*-freebsd* | *-*-dragonfly* | *-*-bitrig*)
++	 *-*-openbsd* | *-*-freebsd* | *-*-dragonfly* | *-*-bitrig* | *-*-midnightbsd*)
  	   # Do not include libc_r directly, use -pthread flag.
  	   continue
  	   ;;
-@@ -7616,7 +7616,7 @@
+@@ -9122,7 +9122,7 @@
  	  *-*-netbsd*)
  	    # Don't link with libc until the a.out ld.so is fixed.
  	    ;;
