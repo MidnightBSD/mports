1dd1e676b2e5b88dcd32033b238947d31537a86a
Lucas Holt <luke@foolishgames.com>
2021-02-20 00:55:14 -0500

    add math patch

---
+++ graphics/qt5-imageformats/files/patch-src-plugins-imageformats-jp2-qjp2handler.cpp
@@ -0,0 +1,11 @@
+--- src/plugins/imageformats/jp2/qjp2handler.cpp.orig	2021-02-20 00:53:12.035459000 -0500
++++ src/plugins/imageformats/jp2/qjp2handler.cpp	2021-02-20 00:53:47.343142000 -0500
+@@ -44,6 +44,8 @@
+ #include "qvariant.h"
+ #include "qcolor.h"
+ 
++#include <cmath.h>
++
+ #include <jasper/jasper.h>
+ 
+ QT_BEGIN_NAMESPACE
