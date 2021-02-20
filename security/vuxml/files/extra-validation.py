552c4313e30652c3f62db9767794ffc234a35b5a
Lucas Holt <luke@foolishgames.com>
2021-02-20 11:19:48 -0500

    update vuxml

--- security/vuxml/files/extra-validation.py
+++ security/vuxml/files/extra-validation.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
-# $FreeBSD: head/security/vuxml/files/extra-validation.py 389986 2015-06-17 17:35:58Z sunpoet $
+# $FreeBSD$
 
 import datetime
 import xml.etree.ElementTree as ET
 import sys
+import re
 
 if len(sys.argv) != 2:
     print("Usage: %s vuln.xml" % (sys.argv[0]))
     sys.exit(1)
+
+re_date = re.compile(r'^(19|20)[0-9]{2}-[0-9]{2}-[0-9]{2}$')
+re_invalid_package_name = re.compile('[@!#$%^&*()<>?/\|}{~:]')
+
+# warn if description has more than X characters
+DESCRIPTION_LENGTH = 5000
 
 tree = ET.parse(sys.argv[1])
 root = tree.getroot()
@@ -69,5 +76,29 @@ for vuln in root:
             if not (dateof(discovery.text) <= dateof(entry.text) <= dateof(modified.text)):
                 print("Error: dates are insane : {0}".format(vid))
                 ret = 1
+
+        # Make sure the dates are in YYYY-MM-DD format
+        datelist = [discovery.text, entry.text] + ([modified.text] if modified is not None else [])
+        for d in datelist:
+            if not re_date.match(d):
+                print("Warning: dates must be in YYYY-MM-DD format: {0}".format(d))
+
+        # Check description lengths
+        description = vuln.find(namespace + "description")
+        description_len = len(ET.tostring(description))
+        if description_len > DESCRIPTION_LENGTH:
+            print("Warning: description too long ({0} chars, {1} is warning threshold): {2})" \
+                  .format(description_len, DESCRIPTION_LENGTH, vid))
+
+        # Walk and validate package names
+        affects = vuln.find(namespace + "affects")
+        packages = affects.findall(namespace + "package")
+        for package in packages:
+            names = package.findall(namespace + "name")
+
+            for name in names:
+                if (re_invalid_package_name.search(name.text) is not None):
+                    print("Error: invalid package name: " + name.text + " for VID " + format(vid))
+                    ret = 1
 
 sys.exit(ret)
