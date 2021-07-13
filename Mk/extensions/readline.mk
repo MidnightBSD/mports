fa0e64e2a5de1332d29f5e48ed150197a30eb133
Lucas Holt <luke@foolishgames.com>
2021-07-13 15:33:54 -0400

    force ports readline now

--- Mk/extensions/readline.mk
+++ Mk/extensions/readline.mk
@@ -8,17 +8,15 @@
 .if !defined(_INCLUDE_USES_READLINE_MK)
 _INCLUDE_USES_READLINE_MK=	yes
 
-.if !exists(/usr/lib/libreadline.so)
-readline_ARGS=	port
+.if !empty(readline_ARGS)
+IGNORE= Incorrect 'USES+= readline:${readline_ARGS}' readline takes no arguments
 .endif
 
-.if ${readline_ARGS} == port
 # magus freaks out because system readline is 8
 BUILD_DEPENDS+=		/usr/local/lib/libreadline.so.8.1:devel/readline
 RUN_DEPENDS+=		/usr/local/lib/libreadline.so.8.1:devel/readline
 LIB_DEPENDS+=		libreadline.so.8.1:devel/readline
 CPPFLAGS+=		-I${LOCALBASE}/include
 LDFLAGS+=		-L${LOCALBASE}/lib
-.endif
 
 .endif
