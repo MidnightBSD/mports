e94ee9213c74113d29e0cee3e03f1f74dea3219e
Lucas Holt <luke@foolishgames.com>
2021-07-23 13:49:15 -0400

    switch to flat

--- security/vxquery/files/pkg_check.sh
+++ security/vxquery/files/pkg_check.sh
@@ -2,7 +2,7 @@
 VXQUERY="vxquery"
 PORTSDIR="${PORTSDIR:-/usr/mports}"
 FORMAT="${FORMAT:-text}"
-VULN_XML="${VULN_XML:-${PORTSDIR}/security/vuxml/vuln.xml}"
+VULN_XML="${VULN_XML:-${PORTSDIR}/security/vuxml/vuln-flat.xml}"
 
 set -e
 /usr/libexec/mport.list | "${VXQUERY}" -f - -t "${FORMAT}" "${VULN_XML}"
