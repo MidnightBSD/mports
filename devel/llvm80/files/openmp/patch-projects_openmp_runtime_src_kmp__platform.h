2225190ac53ae54a582a38d56249ee7737708d46
Lucas Holt <luke@foolishgames.com>
2021-09-30 13:52:48 -0400

    fix name

--- devel/llvm80/files/openmp/patch-projects_openmp_runtime_src_kmp__platform.h
+++ devel/llvm80/files/openmp/patch-projects_openmp_runtime_src_kmp__platform.h
@@ -4,7 +4,7 @@
  #define KMP_OS_LINUX 0
  #define KMP_OS_DRAGONFLY 0
  #define KMP_OS_FREEBSD 0
-+#define KMP_OS_MidnightBSD 0
++#define KMP_OS_MIDNIGHTBSD 0
  #define KMP_OS_NETBSD 0
  #define KMP_OS_OPENBSD 0
  #define KMP_OS_DARWIN 0
