34edee854f367692e8b8923d8e03ff0ae9e12cd4
Lucas Holt <luke@foolishgames.com>
2021-11-04 00:27:02 -0400

    update patchset

--- comms/acfax/files/patch-RegExp.c
+++ comms/acfax/files/patch-RegExp.c
@@ -1,7 +1,5 @@
-$FreeBSD: ports/comms/acfax/files/patch-RegExp.c,v 1.1 2002/06/08 04:53:25 petef Exp $
-
---- RegExp.c.old	Sun Feb 10 05:33:07 2002
-+++ RegExp.c	Sun Feb 10 05:33:24 2002
+--- RegExp.c.orig	1995-07-08 18:20:49 UTC
++++ RegExp.c
 @@ -23,7 +23,6 @@
   */ 
  
--- comms/acfax/files/patch-mod_demod.c
+++ comms/acfax/files/patch-mod_demod.c
@@ -1,7 +1,5 @@
-$FreeBSD: ports/comms/acfax/files/patch-mod_demod.c,v 1.1 2002/06/08 04:53:25 petef Exp $
-
---- mod_demod.c.orig	Sat Jun  8 00:38:29 2002
-+++ mod_demod.c	Sat Jun  8 00:38:41 2002
+--- mod_demod.c.orig	1998-07-08 16:32:02 UTC
++++ mod_demod.c
 @@ -29,6 +29,8 @@
  #include <unistd.h>
  #include "mod_demod.h"
