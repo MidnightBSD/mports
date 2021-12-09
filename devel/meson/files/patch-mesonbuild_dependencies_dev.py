cc442bff6518118d1ca711c2012d07c937da05d5
Lucas Holt <luke@foolishgames.com>
2021-12-09 13:59:20 -0500

    update meson.

--- devel/meson/files/patch-mesonbuild_dependencies_dev.py
+++ devel/meson/files/patch-mesonbuild_dependencies_dev.py
@@ -1,11 +1,11 @@
---- mesonbuild/dependencies/dev.py.orig	2021-03-09 00:41:22 UTC
-+++ mesonbuild/dependencies/dev.py
-@@ -471,7 +471,7 @@ class ZlibSystemDependency(ExternalDepen
+--- mesonbuild/dependencies/dev.py.orig	2021-08-18 07:22:33.000000000 -0400
++++ mesonbuild/dependencies/dev.py	2021-12-09 13:53:17.012187000 -0500
+@@ -474,7 +474,7 @@
          # I'm not sure this is entirely correct. What if we're cross compiling
          # from something to macOS?
          if ((m.is_darwin() and isinstance(self.clib_compiler, (AppleClangCCompiler, AppleClangCPPCompiler))) or
 -                m.is_freebsd() or m.is_dragonflybsd()):
 +                m.is_freebsd() or m.is_midnightbsd() or m.is_dragonflybsd()):
-             self.is_found = True
-             self.link_args = ['-lz']
- 
+             # No need to set includes,
+             # on macos xcode/clang will do that for us.
+             # on freebsd zlib.h is in /usr/include
