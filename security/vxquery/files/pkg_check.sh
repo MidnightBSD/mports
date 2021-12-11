580176bd68b8876caf18c01f60765898150c679d
Lucas Holt <luke@foolishgames.com>
2021-12-11 17:38:16 -0500

    update script

--- security/vxquery/files/pkg_check.sh
+++ security/vxquery/files/pkg_check.sh
@@ -5,4 +5,4 @@ FORMAT="${FORMAT:-text}"
 VULN_XML="${VULN_XML:-${PORTSDIR}/security/vuxml/vuln-flat.xml}"
 
 set -e
-/usr/libexec/mport.list | "${VXQUERY}" -f - -t "${FORMAT}" "${VULN_XML}"
+cd ${PORTSDIR}/security/vuxml && /usr/libexec/mport.list | "${VXQUERY}" -f - -t "${FORMAT}" "${VULN_XML}"
