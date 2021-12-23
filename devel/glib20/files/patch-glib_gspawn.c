b04472a5cb5b8b241f7c601f2e6a9a51b4070a64
Lucas Holt <luke@foolishgames.com>
2021-12-22 21:42:31 -0500

    devel/glib20:  update to 2.70.2

--- devel/glib20/files/patch-glib_gspawn.c
+++ devel/glib20/files/patch-glib_gspawn.c
@@ -1,10 +1,10 @@
---- glib/gspawn.c.orig	2020-12-17 03:47:11.474608400 -0800
-+++ glib/gspawn.c	2021-02-17 13:58:15.271434000 -0800
+--- glib/gspawn.c.orig	2021-12-03 05:02:55.854885000 -0500
++++ glib/gspawn.c	2021-12-22 21:37:08.218383000 -0500
 @@ -51,6 +51,12 @@
  #include <sys/syscall.h>  /* for syscall and SYS_getdents64 */
  #endif
  
-+#ifdef __FreeBSD__
++#if defined(__FreeBSD__) || defined(__MidnightBSD__)
 +#include <sys/types.h>
 +#include <sys/user.h>
 +#include <libutil.h>
@@ -13,11 +13,11 @@
  #include "gspawn.h"
  #include "gspawn-private.h"
  #include "gthread.h"
-@@ -1204,6 +1210,33 @@ filename_to_fd (const char *p)
+@@ -1399,6 +1405,33 @@
  }
  #endif
  
-+#ifdef __FreeBSD__
++#if defined(__FreeBSD__) || defined(__MidnightBSD__)
 +static int
 +fdwalk2(int (*func)(void *, int), void *udata, gint *ret)
 +{
@@ -47,16 +47,34 @@
  /* This function is called between fork() and exec() and hence must be
   * async-signal-safe (see signal-safety(7)). */
  static int
-@@ -1228,6 +1261,12 @@ safe_fdwalk (int (*cb)(void *data, int fd), void *data
-   
- #if 0 && defined(HAVE_SYS_RESOURCE_H)
+@@ -1425,6 +1458,12 @@
    struct rlimit rl;
-+#endif
-+
-+#ifdef __FreeBSD__
+ #endif
+ 
++#if defined(__FreeBSD__) || defined(__MidnightBSD__)
 +  if (fdwalk2(cb, data, &res) == 0)
 +      return res;
 +  /* If any sysctl/malloc call fails continue with the fall back method */
- #endif
- 
++#endif
++
  #ifdef __linux__
+   /* Avoid use of opendir/closedir since these are not async-signal-safe. */
+   int dir_fd = open ("/proc/self/fd", O_RDONLY | O_DIRECTORY);
+@@ -1489,7 +1528,7 @@
+   if (getrlimit (RLIMIT_NOFILE, &rl) == 0 && rl.rlim_max != RLIM_INFINITY)
+     open_max = rl.rlim_max;
+ #endif
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__) || defined(__MidnightBSD__)
+   /* Use sysconf() function provided by the system if it is known to be
+    * async-signal safe.
+    *
+@@ -1545,7 +1584,7 @@
+ static void
+ safe_closefrom (int lowfd)
+ {
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || \
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__MidnightBSD__) || \
+   (defined(__sun__) && defined(F_CLOSEFROM))
+   /* Use closefrom function provided by the system if it is known to be
+    * async-signal safe.
