4c16ac411e0cf85aac1e148b95736e6fa21e51b5
Lucas Holt <luke@foolishgames.com>
2021-12-29 09:59:19 -0500

    lang/rust: 1.38.0

--- lang/rust/files/patch-src_llvm-project_llvm_cmake_modules_HandleLLVMOptions.cmake
+++ lang/rust/files/patch-src_llvm-project_llvm_cmake_modules_HandleLLVMOptions.cmake
@@ -1,10 +1,11 @@
---- src/llvm-project/llvm/cmake/modules/HandleLLVMOptions.cmake.orig	2019-04-02 15:19:15.000000000 -0400
-+++ src/llvm-project/llvm/cmake/modules/HandleLLVMOptions.cmake	2021-12-13 14:13:19.750772000 -0500
-@@ -138,6 +138,7 @@
+--- src/llvm-project/llvm/cmake/modules/HandleLLVMOptions.cmake.orig	2019-09-09 14:30:53.000000000 -0400
++++ src/llvm-project/llvm/cmake/modules/HandleLLVMOptions.cmake	2021-12-28 17:49:54.259328000 -0500
+@@ -169,7 +169,7 @@
+ 
+ # Pass -Wl,-z,defs. This makes sure all symbols are defined. Otherwise a DSO
  # build might work on ELF but fail on MachO/COFF.
- if(NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin" OR WIN32 OR CYGWIN OR
-         ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD" OR
-+        ${CMAKE_SYSTEM_NAME} MATCHES "MidnightBSD" OR
- 	${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD" OR
- 	${CMAKE_SYSTEM_NAME} MATCHES "DragonFly") AND
+-if(NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin|FreeBSD|OpenBSD|DragonFly|AIX" OR
++if(NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin|FreeBSD|OpenBSD|MidnightBSD|DragonFly|AIX" OR
+         WIN32 OR CYGWIN) AND
     NOT LLVM_USE_SANITIZER)
+   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-z,defs")
