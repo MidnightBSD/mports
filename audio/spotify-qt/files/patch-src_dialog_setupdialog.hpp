c75d6c85adb6cdf37b1a348f58f5f4872d28d87d
Lucas Holt <luke@foolishgames.com>
2022-01-07 18:20:50 -0500

    audio/spotify-qt: try patch

---
+++ audio/spotify-qt/files/patch-src_dialog_setupdialog.hpp
@@ -0,0 +1,10 @@
+--- src/dialog/setupdialog.hpp.orig	2022-01-07 23:18:51 UTC
++++ src/dialog/setupdialog.hpp
+@@ -12,6 +12,7 @@
+ #include <QLineEdit>
+ #include <QMessageBox>
+ #include <QPushButton>
++#include <QTcpSocket>
+ #include <QTcpServer>
+ #include <QUrl>
+ #include <QVBoxLayout>
