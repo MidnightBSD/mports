78c0c81fce91119cba5214ca4c0d2ba64147f589
Lucas Holt <luke@foolishgames.com>
2022-02-03 18:56:27 -0500

    expand mirrors

--- Mk/components/sites.mk
+++ Mk/components/sites.mk
@@ -25,7 +25,8 @@
 MASTER_SITE_LOCAL+= \
 	ftp://ftp3.MidnightBSD.org/pub/MidnightBSD/mports/distfiles/%SUBDIR%/ \
 	ftp://ftp1.MidnightBSD.org/pub/MidnightBSD/mports/distfiles/%SUBDIR%/ \
-	ftp://mirror.sandyriver.net/midnightbsd/mports/distfiles/%SUBDIR%/
+	ftp://mirror.sandyriver.net/midnightbsd/mports/distfiles/%SUBDIR%/ \
+	https://discovery.midnightbsd.org/mports/distfiles/%SUBDIR%/ 
 
 MASTER_SITE_PORTS_JP+= \
 	ftp://ports.jp.FreeBSD.org/pub/FreeBSD-jp/ports-jp/LOCAL_PORTS/%SUBDIR%/ \
