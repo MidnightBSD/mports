45d76a4136a4452bf40deabf40a3a586db82da9d
Lucas Holt <luke@foolishgames.com>
2022-04-27 17:52:16 -0400

    audio/vorbis-tools: 1.4.2

--- audio/vorbis-tools/files/patch-ogg123_vorbis__comments.c
+++ audio/vorbis-tools/files/patch-ogg123_vorbis__comments.c
@@ -1,7 +1,6 @@
-$FreeBSD: ports/audio/vorbis-tools/files/patch-ogg123_vorbis_comments.c,v 1.1 2010/06/06 20:37:32 naddy Exp $
---- ogg123/vorbis_comments.c.orig	2010-06-05 16:48:23.000000000 +0200
-+++ ogg123/vorbis_comments.c	2010-06-05 16:55:21.000000000 +0200
-@@ -72,7 +72,7 @@ char *lookup_comment_prettyprint (char *
+--- ogg123/vorbis_comments.c.orig	2021-01-07 23:31:25 UTC
++++ ogg123/vorbis_comments.c
+@@ -74,7 +74,7 @@ char *lookup_comment_prettyprint (const char *comment,
  
    /* Use default formatting */
    j = strcspn(comment, "=");
