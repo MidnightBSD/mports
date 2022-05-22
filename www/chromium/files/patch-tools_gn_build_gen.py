baec5459ce1b2af1e113b8c6d838adef41c708fa
Lucas Holt <luke@foolishgames.com>
2022-05-22 11:52:16 -0400

    www/chromium: reroll patch

--- www/chromium/files/patch-tools_gn_build_gen.py
+++ www/chromium/files/patch-tools_gn_build_gen.py
@@ -1,20 +1,52 @@
---- tools/gn/build/gen.py.orig	2020-07-07 22:09:39 UTC
+--- tools/gn/build/gen.py.orig	2020-09-21 18:48:29 UTC
 +++ tools/gn/build/gen.py
-@@ -78,6 +78,9 @@ class Platform(object):
+@@ -45,6 +45,8 @@ class Platform(object):
+       self._platform = 'fuchsia'
+     elif self._platform.startswith('freebsd'):
+       self._platform = 'freebsd'
++    elif self._platform.startswith('midnightbsd'):
++      self._platform = 'midnightbsd'
+     elif self._platform.startswith('openbsd'):
+       self._platform = 'openbsd'
+     elif self._platform.startswith('haiku'):
+@@ -52,7 +54,7 @@ class Platform(object):
+ 
+   @staticmethod
+   def known_platforms():
+-    return ['linux', 'darwin', 'mingw', 'msvc', 'aix', 'fuchsia', 'freebsd', 'openbsd', 'haiku']
++    return ['linux', 'darwin', 'mingw', 'msvc', 'aix', 'fuchsia', 'freebsd', 'openbsd', 'haiku', 'midnightbsd']
+ 
+   def platform(self):
+     return self._platform
+@@ -78,8 +80,14 @@ class Platform(object):
    def is_haiku(self):
      return self._platform == 'haiku'
  
 +  def is_freebsd(self):
-+    return self._platform in ['freebsd','midnightbsd']
++    return self._platform in ['freebsd','midnightbsd', 'midnightbsd2']
++
++  def is_midnightbsd(self):
++    return self._platform in ['midnightbsd', 'midnightbsd2']
 +
    def is_posix(self):
-     return self._platform in ['linux', 'freebsd', 'darwin', 'aix', 'openbsd', 'haiku']
+-    return self._platform in ['linux', 'freebsd', 'darwin', 'aix', 'openbsd', 'haiku']
++    return self._platform in ['linux', 'freebsd', 'midnightbsd', 'darwin', 'aix', 'openbsd', 'haiku']
  
-@@ -372,6 +375,11 @@ def WriteGNNinja(path, platform, host, options):
+ 
+ def main(argv):
+@@ -200,6 +208,7 @@ def WriteGenericNinja(path, static_libra
+       'darwin': 'build_mac.ninja.template',
+       'linux': 'build_linux.ninja.template',
+       'freebsd': 'build_linux.ninja.template',
++      'midnightbsd': 'build_linux.ninja.template',
+       'aix': 'build_aix.ninja.template',
+       'openbsd': 'build_openbsd.ninja.template',
+       'haiku': 'build_haiku.ninja.template',
+@@ -372,6 +381,11 @@ def WriteGNNinja(path, platform, host, o
      elif platform.is_haiku():
        cflags.append('-fPIC')
        cflags.extend(['-D_BSD_SOURCE'])
-+    elif platform.is_freebsd():
++    elif platform.is_freebsd() or platform.is_midnightbsd():
 +      cflags.extend(['-Wno-deprecated-register', '-Wno-parentheses-equality'])
 +      ldflags.extend(['-pthread'])
 +      libs.extend(['-lexecinfo', '-lkvm', '-lutil'])
