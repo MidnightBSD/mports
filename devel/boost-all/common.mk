7240be98a1831dce1baf6058fb614e565e0d7083
Lucas Holt <luke@foolishgames.com>
2022-05-31 16:50:35 -0400

    update boost

--- devel/boost-all/common.mk
+++ devel/boost-all/common.mk
@@ -1,5 +1,4 @@
-
-PORTVERSION=	1.72.0
+PORTVERSION=	1.79.0
 PORTVERSUFFIX=	${PORTVERSION:C/\.[0-9]+$//}
 DISTNAME=	boost_${PORTVERSION:S/./_/g}
 
@@ -7,7 +6,7 @@ CATEGORIES=	devel
 MAINTAINER=	ports@MidnightBSD.org
 
 CONFLICTS+=	boost-python-1* boost-1*
-MASTER_SITES=	https://dl.bintray.com/boostorg/release/${PORTVERSION}/source/ \
-		SF/boost/boost/${PORTVERSION}
+MASTER_SITES=	SF/boost/boost/${PORTVERSION} \
+		https://boostorg.jfrog.io/artifactory/main/release/${PORTVERSION}/source/
 
 USES+=		tar:bzip2
