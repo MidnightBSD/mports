7fae5d7b7924fa05c985c0e146eefb4e1e69ee7b
Lucas Holt <luke@foolishgames.com>
2022-06-06 09:57:03 -0400

    devel/llvm90: update patch

--- devel/llvm90/files/openmp/patch-projects_openmp_runtime_cmake_LibompHandleFlags.cmake
+++ devel/llvm90/files/openmp/patch-projects_openmp_runtime_cmake_LibompHandleFlags.cmake
@@ -5,7 +5,7 @@
      libomp_append(libflags_local -lirc_pic LIBOMP_HAVE_IRC_PIC_LIBRARY)
    endif()
 -  if(${CMAKE_SYSTEM_NAME} MATCHES "DragonFly")
-+  if(${CMAKE_SYSTEM_NAME} MATCHES "DragonFly|FreeBSD")
++  if(${CMAKE_SYSTEM_NAME} MATCHES "DragonFly|FreeBSD|MidnightBSD")
      libomp_append(libflags_local "-Wl,--no-as-needed" LIBOMP_HAVE_AS_NEEDED_FLAG)
      libomp_append(libflags_local "-lm")
      libomp_append(libflags_local "-Wl,--as-needed" LIBOMP_HAVE_AS_NEEDED_FLAG)
