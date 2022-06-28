f6e73116e5928c50d8e4814f00062ae675c19b57
Lucas Holt <luke@foolishgames.com>
2022-06-28 10:24:45 -0400

    add minizip-ng

---
+++ Mk/extensions/minizip.mk
@@ -0,0 +1,18 @@
+# handle minizip version
+#
+# Feature:	minizip
+# Usage:	USES=minizip[:ng]
+#
+
+.if !defined(_INCLUDE_USES_MINIZIP_MK)
+_INCLUDE_USES_MINIZIP_MK=	yes
+
+.  if empty(minizip_ARGS)
+LIB_DEPENDS+=		libminizip.so:archivers/minizip
+.  elif ${minizip_ARGS} == "ng"
+LIB_DEPENDS+=		libminizip-ng.so:archivers/minizip-ng
+.  else
+IGNORE=	Incorrect 'USES+=minizip:${minizip_ARGS}' expecting 'USES+=minizip[:ng]'
+.  endif
+
+.endif
