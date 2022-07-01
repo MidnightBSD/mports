67bfafe9462a7860ee95e1c0e33461951bd74173
Lucas Holt <luke@foolishgames.com>
2022-07-01 08:52:51 -0400

    devel/meson: 0.62.2

--- devel/meson/files/patch-mesonbuild_dependencies_dev.py
+++ devel/meson/files/patch-mesonbuild_dependencies_dev.py
@@ -1,11 +1,11 @@
---- mesonbuild/dependencies/dev.py.orig	2021-08-18 07:22:33.000000000 -0400
-+++ mesonbuild/dependencies/dev.py	2021-12-09 13:53:17.012187000 -0500
-@@ -474,7 +474,7 @@
+--- mesonbuild/dependencies/dev.py.orig	2022-06-02 12:17:06.000000000 -0400
++++ mesonbuild/dependencies/dev.py	2022-07-01 08:51:32.931304000 -0400
+@@ -495,7 +495,7 @@
          # I'm not sure this is entirely correct. What if we're cross compiling
          # from something to macOS?
          if ((m.is_darwin() and isinstance(self.clib_compiler, (AppleClangCCompiler, AppleClangCPPCompiler))) or
--                m.is_freebsd() or m.is_dragonflybsd()):
-+                m.is_freebsd() or m.is_midnightbsd() or m.is_dragonflybsd()):
+-                m.is_freebsd() or m.is_dragonflybsd() or m.is_android()):
++                m.is_freebsd() or m.is_midnightbsd() or m.is_dragonflybsd() or m.is_android()):
              # No need to set includes,
              # on macos xcode/clang will do that for us.
              # on freebsd zlib.h is in /usr/include
