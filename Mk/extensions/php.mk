3fedc4830218d126b07a6a18b33a20c08969491f
Lucas Holt <luke@foolishgames.com>
2022-09-01 15:31:37 -0400

    update php config and roundcube

--- Mk/extensions/php.mk
+++ Mk/extensions/php.mk
@@ -110,7 +110,7 @@ DIST_SUBDIR=	PECL
 
 PHPBASE?=	${LOCALBASE}
 
-_ALL_PHP_VERSIONS=	74 80 81
+_ALL_PHP_VERSIONS=	74 80 81 82
 
 # Make the already installed PHP the default one.
 .  if exists(${PHPBASE}/etc/php.conf) && !defined(INDEXING)
@@ -179,7 +179,10 @@ PHP_VER=	${FLAVOR:S/^php//}
 	(${FLAVOR:Mphp[0-9][0-9]} && ${FLAVOR} != ${FLAVORS:[1]})
 # When adding a version, please keep the comment in
 # Mk/components/default-versions.mk in sync.
-.    if ${PHP_VER} == 81
+.    if ${PHP_VER} == 82
+PHP_EXT_DIR=   20210903
+PHP_EXT_INC=    hash json openssl pcre random spl
+.    elif ${PHP_VER} == 81
 PHP_EXT_DIR=   20210902
 PHP_EXT_INC=    hash json openssl pcre spl
 .    elif ${PHP_VER} == 80
@@ -377,16 +380,17 @@ add-plist-phpext:
 _USE_PHP_ALL=	bcmath bitset bz2 calendar ctype curl dba dom \
 		enchant exif ffi fileinfo filter ftp gd gettext gmp \
 		hash iconv igbinary imap intl json ldap mbstring mcrypt \
-		memcache memcached mysqli odbc opcache openssl \
-		pcntl pcre pdo pdo_dblib pdo_firebird pdo_mysql \
+		memcache memcached mysqli odbc opcache \
+		openssl pcntl pcre pdo pdo_dblib pdo_firebird pdo_mysql \
 		pdo_odbc pdo_pgsql pdo_sqlite phar pgsql posix \
-		pspell radius readline redis session shmop simplexml snmp soap\
-		sockets sodium spl sqlite3 sysvmsg sysvsem sysvshm \
+		pspell radius random readline redis session shmop simplexml snmp \
+		soap sockets sodium spl sqlite3 sysvmsg sysvsem sysvshm \
 		tidy tokenizer xml xmlreader xmlrpc xmlwriter xsl zip zlib
 # version specific components
-_USE_PHP_VER74=	${_USE_PHP_ALL} pdf
-_USE_PHP_VER80=	${_USE_PHP_ALL}
-_USE_PHP_VER81=	${_USE_PHP_ALL}
+_USE_PHP_VER74=	${_USE_PHP_ALL} pdf zephir_parser
+_USE_PHP_VER80=	${_USE_PHP_ALL} zephir_parser
+_USE_PHP_VER81=	${_USE_PHP_ALL} zephir_parser
+_USE_PHP_VER82=	${_USE_PHP_ALL}
 
 bcmath_DEPENDS=	math/php${PHP_VER}-bcmath
 bitset_DEPENDS=	math/pecl-bitset@${PHP_FLAVOR}
@@ -461,8 +465,13 @@ xmlrpc_DEPENDS=	net/php${PHP_VER}-xmlrpc
 .    endif
 xmlwriter_DEPENDS=	textproc/php${PHP_VER}-xmlwriter
 xsl_DEPENDS=	textproc/php${PHP_VER}-xsl
+zephir_parser_DEPENDS=	textproc/pecl-zephir_parser@${PHP_FLAVOR}
 zip_DEPENDS=	archivers/php${PHP_VER}-zip
 zlib_DEPENDS=	archivers/php${PHP_VER}-zlib
+
+.if ${PHP_VER} > 74
+PHP_EXT_INC+=	openssl
+.endif
 
 .    for extension in ${USE_PHP}
 ext=		${extension}
