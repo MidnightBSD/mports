3ff0b8f86ef5875397e3817a2735dfad9ffce274
Lucas Holt <luke@foolishgames.com>
2022-11-06 14:15:30 -0500

    www/links1: 1.0.4

--- www/links1/files/patch-default.c
+++ www/links1/files/patch-default.c
@@ -1,7 +1,7 @@
---- default.c.orig	Sun May 19 20:12:20 2002
-+++ default.c	Sun May 19 20:12:23 2002
-@@ -267,7 +267,7 @@
- 	get_system_name();
+--- default.c.orig	2018-11-27 22:19:36 UTC
++++ default.c
+@@ -785,7 +785,7 @@ void init_home()
+ 	get_compiler_name();
  	links_home = get_home(&first_use);
  	if (!links_home) {
 -		fprintf(stderr, "Unable to find or create links config directory. Please check, that you have $HOME variable set correctly and that you have write permission to your home directory.\n\007");
