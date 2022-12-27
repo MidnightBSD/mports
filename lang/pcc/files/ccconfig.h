200f65248700dcef7c0472bb5881c3fab1244cc2
Lucas Holt <luke@foolishgames.com>
2022-12-27 00:44:11 -0500

    update pcc

--- lang/pcc/files/ccconfig.h
+++ lang/pcc/files/ccconfig.h
@@ -1,9 +1,8 @@
-/* $FreeBSD: ports/lang/pcc/files/ccconfig.h,v 1.5 2009/09/23 09:20:17 pav Exp $ */
-/*	$Id: ccconfig.h,v 1.2 2010-12-11 18:44:07 laffer1 Exp $	*/
+/*	$Id: ccconfig.h,v 1.17 2012/12/28 12:38:15 ragge Exp $	*/
 
-/*
- * Copyright (c) 2007 David O'Brien
- * Copyright (c) 2004 Anders Magnusson (ragge@ludd.luth.se).
+/*-
+ * Copyright (c) 2007 David O'Brien <obrien@FreeBSD.org>
+ * Copyright (c) 2007 Ed Schouten <ed@fxq.nl>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -14,42 +13,45 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
-/*
- * Various settings that controls how the C compiler works.
- */
+#define CPPADD { "-D__FreeBSD__=" MKS(TARGOSVER), "-D__ELF__", \
+	"-D__unix__=1", "-D__unix=1", NULL, }
 
-/* common cpp predefines */
-#define CPPADD	{ "-D__FreeBSD__=7", "-D__ELF__", "-D__unix__=1", "-D__unix=1", "-D__STDC__=1", NULL, }
-#define DYNLINKER { "-dynamic-linker", "/libexec/ld-elf.so.1", NULL }
-#define CRT0FILE "/usr/lib/crt1.o"
-#define STARTFILES { "/usr/lib/crti.o", "/usr/lib/crtbegin.o", NULL }
-#define	ENDFILES { "/usr/lib/crtend.o", "/usr/lib/crtn.o", NULL }
+/* host-dependent */
+#define CRT0		"crt1.o"
+#define GCRT0		"gcrt1.o"
 #define STARTLABEL "_start"
 
-/*
- * XXX multitargets ?
- */
-#if defined(mach_i386) || defined(mach_x86)
-#define	CPPMDADD { "-D__i386__", "-D__i386", NULL, }
+/* host-independent */
+#define DYNLINKER { "-dynamic-linker", "/libexec/ld-elf.so.1", NULL }
+
+#if defined(mach_i386)
+#define CPPMDADD { "-D__i386__", "-D__i386", NULL, }
 #elif defined(mach_amd64)
-#define CPPMDADD { "-D__x86_64__", NULL, }
+#define	AMD64_32_EMUL	"elf_i386_fbsd"
+#define	AMD64_64_EMUL	"elf_x86_64_fbsd"
+#include "../inc/amd64.h"
+#elif defined(mach_sparc64)
+#define CPPMDADD \
+	{ "-D__sparc64__", "-D__sparc_v9__", "-D__sparc__", "-D__sparc",\
+	  "-D__LP64__=1", "-D_LP64=1", NULL, }
 #else
 #error defines for arch missing
 #endif
 
-#define	STABS
+#ifdef LANG_F77
+#define F77LIBLIST { "-lF77", "-lI77", "-lm", "-lc", NULL };
+#endif
