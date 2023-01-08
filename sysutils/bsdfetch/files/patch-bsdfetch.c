f4a3bfc1068727c2ae1a871225db295d85a2b48e
Lucas Holt <luke@foolishgames.com>
2023-01-08 15:26:11 -0500

    sysutils/bsdfetch: 1.0.10

--- sysutils/bsdfetch/files/patch-bsdfetch.c
+++ sysutils/bsdfetch/files/patch-bsdfetch.c
@@ -1,11 +1,11 @@
---- bsdfetch.c.orig	2022-12-30 08:05:37 UTC
+--- bsdfetch.c.orig	2023-01-08 13:19:42 UTC
 +++ bsdfetch.c
-@@ -248,7 +248,7 @@ static void get_packages(void) {
- 	f = popen("/usr/sbin/pkg_info", "r");
- #elif defined( __DragonFly__)
- 	f = popen("/usr/sbin/pkg info", "r");
--#elif defined(__FreeBSD__)
-+#elif defined(__FreeBSD__) && !defined(__MidnightBSD__)
- 	if(access("/usr/local/sbin/pkg", F_OK) == 0) {
- 		f = popen("/usr/local/sbin/pkg info", "r");
- 	} else {
+@@ -184,7 +184,7 @@ static void get_packages(void) {
+ #if defined (__OpenBSD__) || defined (__NetBSD__)
+ 	const char* const cmd = "/usr/sbin/pkg_info";
+ 
+-#elif defined (__DragonFly__) || defined (__FreeBSD__)
++#elif defined (__DragonFly__) || defined (__FreeBSD__) && !defined(__MidnightBSD__)
+ 	const char* const cmd = "/usr/sbin/pkg info";
+ 
+ #elif defined (__MidnightBSD__)
