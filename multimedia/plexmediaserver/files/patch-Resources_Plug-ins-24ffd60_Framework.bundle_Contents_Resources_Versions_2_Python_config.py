d28dc0287301292638b6bbaebbeea36d096bfb8f
Lucas Holt <luke@foolishgames.com>
2023-03-04 12:09:36 -0500

    multimedia/plexmediaserver: fix patch

--- multimedia/plexmediaserver/files/patch-Resources_Plug-ins-24ffd60_Framework.bundle_Contents_Resources_Versions_2_Python_config.py
+++ multimedia/plexmediaserver/files/patch-Resources_Plug-ins-24ffd60_Framework.bundle_Contents_Resources_Versions_2_Python_config.py
@@ -1,5 +1,5 @@
---- Resources/Plug-ins-1bef33a/Framework.bundle/Contents/Resources/Versions/2/Python/config.py.orig	2016-09-29 12:00:12 UTC
-+++ Resources/Plug-ins-1bef33a/Framework.bundle/Contents/Resources/Versions/2/Python/config.py
+--- Resources/Plug-ins-b38628e/Framework.bundle/Contents/Resources/Versions/2/Python/config.py.orig	2016-09-29 12:00:12 UTC
++++ Resources/Plug-ins-b38628e/Framework.bundle/Contents/Resources/Versions/2/Python/config.py
 @@ -92,6 +92,7 @@ os_map = {
    "Windows"                     : "Windows",
    "FreeBSD"                     : "FreeBSD",
