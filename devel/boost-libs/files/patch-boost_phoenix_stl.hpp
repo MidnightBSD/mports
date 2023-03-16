591a6b2e1bbb7d96daa8b9e208b86f6384d9460e
Lucas Holt <luke@foolishgames.com>
2023-03-16 10:35:51 -0400

    Revert "devel/boost-libs: add midnight config"
    This reverts commit d90fe406f6480bffa0024ae6e4fc0e528311b17e.

--- devel/boost-libs/files/patch-boost_phoenix_stl.hpp
+++ devel/boost-libs/files/patch-boost_phoenix_stl.hpp
@@ -1,6 +1,6 @@
 https://bugs.gentoo.org/887041
 https://github.com/boostorg/phoenix/issues/111
---- boost/phoenix/stl.hpp.orig	2022-12-08 01:02:46 UTC
+--- boost/phoenix/stl.hpp
 +++ boost/phoenix/stl.hpp
 @@ -11,6 +11,5 @@
  
