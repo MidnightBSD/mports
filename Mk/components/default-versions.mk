61e00a5fe3c5794d7c145d48db7d89c342682b90
Lucas Holt <luke@foolishgames.com>
2023-03-23 11:11:20 -0400

    Mk/components: python 3.9 default

--- Mk/components/default-versions.mk
+++ Mk/components/default-versions.mk
@@ -80,7 +80,7 @@ PYTHON_DEFAULT?=	3.9
 # Possible values: 2.7
 PYTHON2_DEFAULT?=	2.7
 # Possible values: 3.7, 3.8, 3.9, 3.10, 3.11
-PYTHON3_DEFAULT?=	3.8
+PYTHON3_DEFAULT?=	3.9
 # Possible values: 2.7, 3.0, 3.1, 3.2
 RUBY_DEFAULT?=		3.0
 # Possible values: rust, rust167, rust-bin, rust166-bin
