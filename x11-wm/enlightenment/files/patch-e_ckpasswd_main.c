0e60906845871d7019158d407bdad1d6f23fd0bd
Lucas Holt <luke@foolishgames.com>
2023-04-01 12:30:08 -0400

    x11-wm/enlightenment: wrong way to go

--- x11-wm/enlightenment/files/patch-e_ckpasswd_main.c
+++ x11-wm/enlightenment/files/patch-e_ckpasswd_main.c
@@ -1,14 +1,5 @@
 --- src/bin/e_ckpasswd_main.c.orig	2023-03-27 13:01:12.396015000 -0400
 +++ src/bin/e_ckpasswd_main.c	2023-03-27 13:03:30.042195000 -0400
-@@ -2,6 +2,8 @@
- 
- #include "config.h"
- 
-+#define __XSI_VISIBLE=1
-+
- #include <stdio.h>
- #include <stdlib.h>
- #include <unistd.h>
 @@ -39,7 +41,7 @@
  
  
