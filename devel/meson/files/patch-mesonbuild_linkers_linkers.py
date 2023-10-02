350098ecc9e4cc7a440741f287a0d18facc7b226
Lucas Holt <luke@foolishgames.com>
2023-10-02 14:20:51 -0400

    devel/meson: 0.63.1

--- devel/meson/files/patch-mesonbuild_linkers_linkers.py
+++ devel/meson/files/patch-mesonbuild_linkers_linkers.py
@@ -1,11 +1,11 @@
---- mesonbuild/linkers/linkers.py.orig	2022-09-17 14:11:42.083541000 -0400
-+++ mesonbuild/linkers/linkers.py	2022-09-17 14:12:13.075894000 -0400
-@@ -208,7 +208,7 @@
+--- mesonbuild/linkers/linkers.py.orig	2022-08-13 04:48:38.000000000 -0400
++++ mesonbuild/linkers/linkers.py	2023-10-02 14:15:37.744887000 -0400
+@@ -209,7 +209,7 @@
  
-     def get_std_link_args(self, is_thin: bool) -> T.List[str]:
+     def get_std_link_args(self, env: 'Environment', is_thin: bool) -> T.List[str]:
          # FIXME: osx ld rejects this: "file built for unknown-unsupported file format"
--        if is_thin and not mesonlib.is_osx():
-+        if is_thin and not mesonlib.is_osx() and not mesonlib.is_midnightbsd():
+-        if is_thin and not env.machines[self.for_machine].is_darwin():
++        if is_thin and not env.machines[self.for_machine].is_darwin() and not env.machines[self.for_machine].is_midnightbsd():
              return self.std_thin_args
          else:
              return self.std_args
