4ebadfe29c4f0669178a3b7cf865ede85cc17c1c
Lucas Holt <luke@foolishgames.com>
2023-10-10 16:18:38 -0400

    lang/php81: 8.1.22

--- lang/php81/files/patch-Zend-zend_gdb.c
+++ lang/php81/files/patch-Zend-zend_gdb.c
@@ -1,13 +1,14 @@
---- Zend/zend_gdb.c.orig	2022-04-14 23:09:24.074491000 -0400
-+++ Zend/zend_gdb.c	2022-04-14 23:10:01.659845000 -0400
-@@ -25,7 +25,9 @@
+--- Zend/zend_gdb.c.orig	2023-08-01 12:16:09.000000000 -0400
++++ Zend/zend_gdb.c	2023-10-10 16:16:02.086494000 -0400
+@@ -25,7 +25,10 @@
  #include <fcntl.h>
  #include <unistd.h>
  
--#if defined(__FreeBSD__)
+-#if defined(__FreeBSD__) && __FreeBSD_version >= 1100000
++
 +#if defined(__MidnightBSD__)
 +# include <sys/user.h>
-+#elif defined(__FreeBSD__)
++#elif defined(__FreeBSD__) && __FreeBSD_version >= 1100000
  # include <sys/user.h>
  # include <libutil.h>
  #endif
