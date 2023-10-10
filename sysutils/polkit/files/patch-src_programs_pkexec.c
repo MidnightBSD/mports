20d511ea23b90fbd4f94fffa740e88c7c67ea82b
Lucas Holt <luke@foolishgames.com>
2023-10-10 12:36:11 -0400

    sysutils/polkit: 123

--- sysutils/polkit/files/patch-src_programs_pkexec.c
+++ sysutils/polkit/files/patch-src_programs_pkexec.c
@@ -1,35 +1,33 @@
---- src/programs/pkexec.c.orig	2022-06-02 11:10:42 UTC
+--- src/programs/pkexec.c.orig	2022-07-11 08:46:06 UTC
 +++ src/programs/pkexec.c
-@@ -488,6 +488,15 @@ main (int argc, char *argv[])
-   pid_t pid_of_caller;
-   gpointer local_agent_handle;
+@@ -37,6 +37,11 @@
+ #include <sys/prctl.h>
+ #endif
  
++#ifdef __FreeBSD__
++#include <signal.h>
++#include <sys/procctl.h>
++#endif
 +
-+  /*
-+   * If 'pkexec' is called THIS wrong, someone's probably evil-doing. Don't be nice, just bail out.
-+   */
-+  if (argc<1)
-+    {
-+      exit(127);
-+    }
-+
-   ret = 127;
-   authority = NULL;
-   subject = NULL;
-@@ -636,7 +645,15 @@ main (int argc, char *argv[])
-           goto out;
-         }
-       g_free (path);
--      argv[n] = path = s;
-+path = s;
-+
-+      /* argc<2 and pkexec runs just shell, argv is guaranteed to be null-terminated.
-+       * /-less shell shouldn't happen, but let's be defensive and don't write to null-termination
-+       */
-+      if (argv[n] != NULL)
-+      {
-+        argv[n] = path;
-+      }
+ #include <glib/gi18n.h>
+ 
+ #ifdef POLKIT_AUTHFW_PAM
+@@ -729,10 +734,17 @@ main (int argc, char *argv[])
      }
-   if (access (path, F_OK) != 0)
+ 
+   /* make sure we are nuked if the parent process dies */
+-#ifdef __linux__
++#if defined(__linux__)
+   if (prctl (PR_SET_PDEATHSIG, SIGTERM) != 0)
      {
+       g_printerr ("prctl(PR_SET_PDEATHSIG, SIGTERM) failed: %s\n", g_strerror (errno));
++      goto out;
++    }
++#elif defined(__FreeBSD__)
++  int _sig = SIGTERM;
++  if (procctl (P_PID, 0, PROC_PDEATHSIG_CTL, &_sig) != 0)
++    {
++      g_printerr ("procctl(2) failed: %s\n", g_strerror (errno));
+       goto out;
+     }
+ #else
