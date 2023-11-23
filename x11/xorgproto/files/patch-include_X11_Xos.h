9a9d32e25cada259abc997b2cf4541d2eaa4dfcc
Lucas Holt <luke@foolishgames.com>
2023-11-23 11:38:48 -0500

    x11/xorgproto: 2023.2

--- x11/xorgproto/files/patch-include_X11_Xos.h
+++ x11/xorgproto/files/patch-include_X11_Xos.h
@@ -1,10 +1,10 @@
---- include/X11/Xos.h.orig	2022-04-21 00:48:08 UTC
+--- include/X11/Xos.h.orig	2023-06-16 07:32:38 UTC
 +++ include/X11/Xos.h
 @@ -60,7 +60,7 @@ in this Software without prior written authorization f
   */
  
  # include <string.h>
--# if defined(__SCO__) || defined(__UNIXWARE__) || defined(__sun) || defined(__CYGWIN__) || defined(_AIX) || defined(__APPLE__)
+-# if defined(__SCO__) || defined(__UNIXWARE__) || defined(__sun) || defined(__CYGWIN__) || defined(_AIX) || defined(__APPLE__) || defined(__FreeBSD__)
 +# if defined(__SCO__) || defined(__UNIXWARE__) || defined(__sun) || defined(__CYGWIN__) || defined(_AIX) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__MidnightBSD__)
  #  include <strings.h>
  # else
