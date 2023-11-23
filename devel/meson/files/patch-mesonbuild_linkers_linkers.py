b36db49200e27b420d8a1cf33934a78df6a5d8eb
Lucas Holt <luke@foolishgames.com>
2023-11-23 13:33:55 -0500

    devel/meson: 0.64.1

--- devel/meson/files/patch-mesonbuild_linkers_linkers.py
+++ devel/meson/files/patch-mesonbuild_linkers_linkers.py
@@ -1,11 +1,11 @@
---- mesonbuild/linkers/linkers.py.orig	2022-08-13 04:48:38.000000000 -0400
-+++ mesonbuild/linkers/linkers.py	2023-10-02 14:15:37.744887000 -0400
-@@ -209,7 +209,7 @@
- 
-     def get_std_link_args(self, env: 'Environment', is_thin: bool) -> T.List[str]:
-         # FIXME: osx ld rejects this: "file built for unknown-unsupported file format"
--        if is_thin and not env.machines[self.for_machine].is_darwin():
-+        if is_thin and not env.machines[self.for_machine].is_darwin() and not env.machines[self.for_machine].is_midnightbsd():
+--- mesonbuild/linkers/linkers.py.orig	2022-11-21 14:54:10.000000000 -0500
++++ mesonbuild/linkers/linkers.py	2023-11-23 13:32:49.786668000 -0500
+@@ -213,7 +213,7 @@
+         # OS X ld rejects with: "file built for unknown-unsupported file format"
+         # illumos/Solaris ld rejects with: "unknown file type"
+         if is_thin and not env.machines[self.for_machine].is_darwin() \
+-          and not env.machines[self.for_machine].is_sunos():
++          and not env.machines[self.for_machine].is_sunos() and not env.machines[self.for_machine].is_midnightbsd():
              return self.std_thin_args
          else:
              return self.std_args
