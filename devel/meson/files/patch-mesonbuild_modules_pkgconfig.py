4cc72612057c75444e8ce697fcdbe2c40ec23b74
Lucas Holt <luke@foolishgames.com>
2023-11-23 13:40:08 -0500

    devel/meson: 1.0.1

--- devel/meson/files/patch-mesonbuild_modules_pkgconfig.py
+++ devel/meson/files/patch-mesonbuild_modules_pkgconfig.py
@@ -1,5 +1,5 @@
---- mesonbuild/modules/pkgconfig.py.orig	2022-10-24 15:25:24.000000000 -0400
-+++ mesonbuild/modules/pkgconfig.py	2023-10-02 14:32:37.840065000 -0400
+--- mesonbuild/modules/pkgconfig.py.orig	2023-11-23 13:38:47.664832000 -0500
++++ mesonbuild/modules/pkgconfig.py	2023-11-23 13:39:11.719796000 -0500
 @@ -696,7 +696,7 @@
          pcfile = filebase + '.pc'
          pkgroot = pkgroot_name = kwargs['install_dir'] or default_install_dir
@@ -8,4 +8,4 @@
 +            if mesonlib.is_freebsd() or mesonlib.is_midnightbsd():
                  pkgroot = os.path.join(_as_str(state.environment.coredata.get_option(mesonlib.OptionKey('prefix'))), 'libdata', 'pkgconfig')
                  pkgroot_name = os.path.join('{prefix}', 'libdata', 'pkgconfig')
-             else:
+             elif mesonlib.is_haiku():
