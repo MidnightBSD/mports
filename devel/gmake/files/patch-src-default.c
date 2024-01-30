0c16333e4bdb7bf34cd996735d7c1f407ace7e92
Lucas Holt <luke@foolishgames.com>
2024-01-30 08:19:34 -0500

    devel/gmake: rollback gmake for now.
    It's causing a lot of issues when building nested ports

--- devel/gmake/files/patch-src-default.c
+++ devel/gmake/files/patch-src-default.c
@@ -1,11 +1,11 @@
---- src/default.c.orig	2023-06-03 15:18:36 UTC
+--- src/default.c.orig	2020-01-03 07:11:27 UTC
 +++ src/default.c
-@@ -542,7 +542,7 @@ static const char *default_variables[] =
-     "CXX", "gcc",
- #  endif /* __MSDOS__ */
- # else
+@@ -530,7 +530,7 @@ static const char *default_variables[] =
+     "OBJC", "gcc",
+ #else
+     "CC", "cc",
 -    "CXX", "g++",
 +    "CXX", "c++",
- # endif
+     "OBJC", "cc",
  #endif
-     /* This expands to $(CO) $(COFLAGS) $< $@ if $@ does not exist,
+ 
