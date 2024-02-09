314ba2108aa17be4ac7f271cea55959539884e7a
Lucas Holt <luke@foolishgames.com>
2024-02-09 10:28:40 -0500

    Tools/lib: update index version to include perl changes.

--- Tools/lib/Magus/Index.pm
+++ Tools/lib/Magus/Index.pm
@@ -43,8 +43,10 @@ sub sync {
   my $osversion;
   my %visited;
 
-  if ($osrel eq "3.2") {
-    $osversion = 302000;
+  if ($osrel eq "4.0") {
+    $osversion = 400000;
+  } elsif ($osrel eq "3.2") {
+    $osversion = 302001;
   } elsif ($osrel eq "3.1") {
     $osversion = 301000;
   } elsif ($osrel eq "3.0") {
