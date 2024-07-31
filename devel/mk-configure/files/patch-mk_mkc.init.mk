2c0f8ae2ddf079c0964e30f869a0af44c076a059
Lucas Holt <luke@foolishgames.com>
2024-07-31 13:25:14 -0400

    devel/mk-configure: man

--- devel/mk-configure/files/patch-mk_mkc.init.mk
+++ devel/mk-configure/files/patch-mk_mkc.init.mk
@@ -1,6 +1,6 @@
---- mk/mkc.init.mk.orig	2019-08-26 11:15:50.075406000 -0400
-+++ mk/mkc.init.mk	2019-08-26 11:16:17.356433000 -0400
-@@ -125,6 +125,7 @@
+--- mk/mkc.init.mk.orig	2022-11-29 13:09:00.000000000 -0500
++++ mk/mkc.init.mk	2024-07-31 13:24:27.718058000 -0400
+@@ -95,6 +95,7 @@
  ROOT_GROUP.FreeBSD   =	wheel
  ROOT_GROUP.Darwin    =	wheel
  ROOT_GROUP.DragonFly =	wheel
@@ -8,3 +8,12 @@
  ROOT_GROUP.MirBSD    =	wheel
  ROOT_GROUP.HP-UX     =	bin
  ROOT_GROUP.OSF1      =	bin
+@@ -275,7 +276,7 @@
+ VARDIR     ?=	${PREFIX}/var
+ SYSCONFDIR ?=	${PREFIX}/etc
+ INFODIR    ?=	${PREFIX}/info
+-MANDIR     ?=	${PREFIX}/man
++MANDIR     ?=	${PREFIX}/share/man
+ LIBDIR     ?=	${PREFIX}/lib
+ SCRIPTSDIR ?=	${BINDIR}
+ 
