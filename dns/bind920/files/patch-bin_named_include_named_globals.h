1ca5ceac1dd4925c607ebb5cd7620da625f575e0
Lucas Holt <luke@foolishgames.com>
2024-08-03 13:20:27 -0400

    update bind devel, add bind920, remove bind 9.16

--- dns/bind920/files/patch-bin_named_include_named_globals.h
+++ dns/bind920/files/patch-bin_named_include_named_globals.h
@@ -1,8 +1,8 @@
 We reference the pid file as being run/named/pid everywere else.
 
---- bin/named/include/named/globals.h.orig	2022-01-12 13:50:10 UTC
+--- bin/named/include/named/globals.h.orig	2023-12-01 14:21:34 UTC
 +++ bin/named/include/named/globals.h
-@@ -129,7 +129,7 @@ EXTERN bool named_g_forcelock		   INIT(false);
+@@ -121,7 +121,7 @@ EXTERN const char *named_g_defaultsessionkeyfile INIT(
  
  #if NAMED_RUN_PID_DIR
  EXTERN const char *named_g_defaultpidfile INIT(NAMED_LOCALSTATEDIR "/run/named/"
