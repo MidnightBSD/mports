329a652b6a3d34636fe9e70d11b0972c41b13737
Lucas Holt <luke@foolishgames.com>
2024-12-21 12:03:50 -0500

    Mk/extensions: rollback version

--- Mk/extensions/qt.mk
+++ Mk/extensions/qt.mk
@@ -21,8 +21,8 @@ _QT_MK_INCLUDED=	qt.mk
 # Qt versions currently supported by the framework.
 _QT_SUPPORTED?=		5 6
 QT5_VERSION?=		5.15.11
-QT6_VERSION?=		6.7.3
-PYSIDE6_VERSION?=	6.7.3
+QT6_VERSION?=		6.5.3
+PYSIDE6_VERSION?=	6.5.3
 
 # Support for intermediate Qt6 releases. This partially defines
 # _QT6_MASTER_SITE_SUBDIR and would probably be better in qt-dist.mk,
