5ff9f6ff1202bbdfdcfa30a762b74f9c5d4e1f92
Lucas Holt <luke@foolishgames.com>
2025-01-07 12:56:09 -0500

    www/qt6-webengine: update

--- www/qt6-webengine/files/patch-src_3rdparty_gn_build_gen.py
+++ www/qt6-webengine/files/patch-src_3rdparty_gn_build_gen.py
@@ -1,34 +1,45 @@
---- src/3rdparty/gn/build/gen.py.orig	2023-08-16 19:50:41 UTC
+--- src/3rdparty/gn/build/gen.py.orig	2024-02-10 00:23:21 UTC
 +++ src/3rdparty/gn/build/gen.py
-@@ -92,6 +92,12 @@ class Platform(object):
+@@ -47,6 +47,8 @@ class Platform(object):
+       self._platform = 'fuchsia'
+     elif self._platform.startswith('freebsd'):
+       self._platform = 'freebsd'
++    elif self._platform.startswith('midnightbsd'):
++      self._platform = 'midnightbsd'
+     elif self._platform.startswith('netbsd'):
+       self._platform = 'netbsd'
+     elif self._platform.startswith('openbsd'):
+@@ -60,7 +62,7 @@ class Platform(object):
+ 
+   @staticmethod
+   def known_platforms():
+-    return ['linux', 'darwin', 'mingw', 'msys', 'msvc', 'aix', 'fuchsia', 'freebsd', 'netbsd', 'openbsd', 'haiku', 'solaris', 'zos']
++    return ['linux', 'darwin', 'mingw', 'msys', 'msvc', 'aix', 'fuchsia', 'freebsd', 'midnightbsd', 'netbsd', 'openbsd', 'haiku', 'solaris', 'zos']
+ 
+   def platform(self):
+     return self._platform
+@@ -92,8 +94,14 @@ class Platform(object):
    def is_solaris(self):
      return self._platform == 'solaris'
  
-+  def is_openbsd(self):
-+    return self._platform == 'openbsd'
++  def is_midnightbsd(self):
++    return self._platform == 'midnightbsd'
 +
 +  def is_freebsd(self):
 +    return self._platform == 'freebsd'
 +
    def is_posix(self):
-     return self._platform in ['linux', 'freebsd', 'darwin', 'aix', 'openbsd', 'haiku', 'solaris', 'msys', 'netbsd']
+-    return self._platform in ['linux', 'freebsd', 'darwin', 'aix', 'openbsd', 'haiku', 'solaris', 'msys', 'netbsd']
++    return self._platform in ['linux', 'freebsd', 'midnightbsd', 'darwin', 'aix', 'openbsd', 'haiku', 'solaris', 'msys', 'netbsd']
  
-@@ -294,7 +300,7 @@ def WriteGenericNinja(path, static_libraries, executab
+   def is_zos(self):
+     return self._platform == 'zos'
+@@ -294,7 +302,7 @@ def WriteGenericNinja(path, static_libraries, executab
        'linux': 'build_linux.ninja.template',
        'freebsd': 'build_linux.ninja.template',
        'aix': 'build_aix.ninja.template',
 -      'openbsd': 'build_openbsd.ninja.template',
-+      'openbsd': 'build_linux.ninja.template',
++      'midnightbsd': 'build_linux.ninja.template',
        'haiku': 'build_haiku.ninja.template',
        'solaris': 'build_linux.ninja.template',
        'netbsd': 'build_linux.ninja.template',
-@@ -531,6 +537,9 @@ def WriteGNNinja(path, platform, host, options, args_l
- 
-     if platform.is_posix() and not platform.is_haiku():
-       ldflags.append('-pthread')
-+
-+    if platform.is_openbsd():
-+      libs.append('-lkvm')
- 
-     if platform.is_mingw() or platform.is_msys():
-       cflags.extend(['-DUNICODE',
