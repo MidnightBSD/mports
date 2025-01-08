7b8781c8b616cf6b07adff9ca5c076213b791ea4
Lucas Holt <luke@foolishgames.com>
2025-01-08 13:15:27 -0500

    use transactions for result delete too

--- Tools/magus/master/delete_result.pl
+++ Tools/magus/master/delete_result.pl
@@ -7,12 +7,39 @@ use lib qw(/usr/mports/Tools/lib);
 use Magus;
 
 #
-# quick little script to delete results from the db.
+# Script to delete results from the db with cascading deletes.
 #
 
-while (@ARGV) {
+my @result_ids = @ARGV;
 
-  my $result = Magus::Result->retrieve(shift) || die "Couldn't get result!\n";
+if (@result_ids) {
+    my $dbh = Magus::Result->db_Main;
 
-  $result->delete;
+    # Start a transaction
+    $dbh->begin_work;
+
+    eval {
+        foreach my $result_id (@result_ids) {
+            my $result = Magus::Result->retrieve($result_id);
+            if ($result) {
+                $result->delete;
+                print "Deleted result $result_id\n";
+            } else {
+                warn "Couldn't find result with ID $result_id\n";
+            }
+        }
+
+        # Commit the transaction
+        $dbh->commit;
+    };
+
+    if ($@) {
+        # If there was an error, roll back the transaction
+        $dbh->rollback;
+        die "Error deleting results: $@\n";
+    }
+
+    print "Finished deleting results.\n";
+} else {
+    print "No result IDs provided.\n";
 }
