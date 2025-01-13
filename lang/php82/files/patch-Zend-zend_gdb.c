d53b81ced73a5dc52f33a3d8f66938fc0f3baed8
Lucas Holt <luke@foolishgames.com>
2025-01-13 18:23:53 -0500

    update php 8.1 and 8.2

--- lang/php82/files/patch-Zend-zend_gdb.c
+++ lang/php82/files/patch-Zend-zend_gdb.c
@@ -1,13 +1,13 @@
---- Zend/zend_gdb.c.orig	2022-04-14 23:09:24.074491000 -0400
-+++ Zend/zend_gdb.c	2022-04-14 23:10:01.659845000 -0400
-@@ -25,7 +25,9 @@
+--- Zend/zend_gdb.c.orig	2024-12-17 06:39:23.000000000 -0500
++++ Zend/zend_gdb.c	2025-01-13 18:21:21.189652000 -0500
+@@ -25,9 +25,8 @@
  #include <fcntl.h>
  #include <unistd.h>
  
--#if defined(__FreeBSD__)
+-#if defined(__FreeBSD__) && __FreeBSD_version >= 1100000
 +#if defined(__MidnightBSD__)
-+# include <sys/user.h>
-+#elif defined(__FreeBSD__)
  # include <sys/user.h>
- # include <libutil.h>
+-# include <libutil.h>
  #endif
+ 
+ enum {
