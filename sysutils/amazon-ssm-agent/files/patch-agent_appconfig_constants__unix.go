ebf7d96e86be82fdade7675ac8cb5da443276bb4
Lucas Holt <luke@foolishgames.com>
2025-03-15 02:43:48 -0400

    sysutils/amazon-ssm-agent:  3.0.1390 (last 3.0 release from july 2021)

--- sysutils/amazon-ssm-agent/files/patch-agent_appconfig_constants__unix.go
+++ sysutils/amazon-ssm-agent/files/patch-agent_appconfig_constants__unix.go
@@ -1,20 +1,23 @@
---- agent/appconfig/constants_unix.go.orig	2020-09-02 15:00:33 UTC
+--- agent/appconfig/constants_unix.go.orig	2021-07-07 22:38:05 UTC
 +++ agent/appconfig/constants_unix.go
-@@ -53,7 +53,7 @@ const (
- 	DownloadRoot = "/var/log/amazon/ssm/download/"
- 
- 	// DefaultDataStorePath represents the directory for storing system data
--	DefaultDataStorePath = "/var/lib/amazon/ssm/"
-+	DefaultDataStorePath = "/var/run/amazon/ssm/"
- 
- 	// EC2ConfigDataStorePath represents the directory for storing ec2 config data
- 	EC2ConfigDataStorePath = "/var/lib/amazon/ec2config/"
-@@ -98,7 +98,7 @@ const (
+@@ -112,9 +112,9 @@ const (
  var PowerShellPluginCommandName string
  
  // DefaultProgramFolder is the default folder for SSM
 -var DefaultProgramFolder = "/etc/amazon/ssm/"
 +var DefaultProgramFolder = "/usr/local/etc/amazon/ssm/"
- var DefaultSSMAgentWorker = "/usr/bin/ssm-agent-worker"
- var DefaultDocumentWorker = "/usr/bin/ssm-document-worker"
- var DefaultSessionWorker = "/usr/bin/ssm-session-worker"
+ 
+-var defaultWorkerPath = "/usr/bin/"
++var defaultWorkerPath = "/usr/local/bin/"
+ var DefaultSSMAgentWorker = defaultWorkerPath + "ssm-agent-worker"
+ var DefaultDocumentWorker = defaultWorkerPath + "ssm-document-worker"
+ var DefaultSessionWorker = defaultWorkerPath + "ssm-session-worker"
+@@ -133,7 +133,7 @@ func init() {
+ 	/*
+ 	   Powershell command used to be poweshell in alpha versions, now it's pwsh in prod versions
+ 	*/
+-	PowerShellPluginCommandName = "/usr/bin/powershell"
++	PowerShellPluginCommandName = "/usr/local/bin/powershell"
+ 	if _, err := os.Stat(PowerShellPluginCommandName); err != nil {
+ 		PowerShellPluginCommandName = "/usr/bin/pwsh"
+ 	}
