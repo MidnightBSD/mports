faeecea4d5a111ba4876a859c5fbc1d909af08dc
Lucas Holt <luke@foolishgames.com>
2025-03-14 20:19:41 -0400

    sysutils/amazon-ssm-agent: 3.0.151

--- sysutils/amazon-ssm-agent/files/patch-agent_appconfig_constants__unix.go
+++ sysutils/amazon-ssm-agent/files/patch-agent_appconfig_constants__unix.go
@@ -1,4 +1,4 @@
---- agent/appconfig/constants_unix.go.orig	2019-05-08 20:19:03 UTC
+--- agent/appconfig/constants_unix.go.orig	2020-09-02 15:00:33 UTC
 +++ agent/appconfig/constants_unix.go
 @@ -53,7 +53,7 @@ const (
  	DownloadRoot = "/var/log/amazon/ssm/download/"
@@ -9,12 +9,12 @@
  
  	// EC2ConfigDataStorePath represents the directory for storing ec2 config data
  	EC2ConfigDataStorePath = "/var/lib/amazon/ec2config/"
-@@ -95,7 +95,7 @@ const (
+@@ -98,7 +98,7 @@ const (
  var PowerShellPluginCommandName string
  
  // DefaultProgramFolder is the default folder for SSM
 -var DefaultProgramFolder = "/etc/amazon/ssm/"
 +var DefaultProgramFolder = "/usr/local/etc/amazon/ssm/"
+ var DefaultSSMAgentWorker = "/usr/bin/ssm-agent-worker"
  var DefaultDocumentWorker = "/usr/bin/ssm-document-worker"
  var DefaultSessionWorker = "/usr/bin/ssm-session-worker"
- var DefaultSessionLogger = "/usr/bin/ssm-session-logger"
