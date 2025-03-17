04b6a28fb9d2d0d4334fad6e7343b2ea50427c85
Lucas Holt <luke@foolishgames.com>
2025-03-17 01:29:41 -0400

    sysutils/amazon-ssm-agent: 3.3.x

--- sysutils/amazon-ssm-agent/files/patch-agent_appconfig_constants__unix.go
+++ sysutils/amazon-ssm-agent/files/patch-agent_appconfig_constants__unix.go
@@ -1,6 +1,6 @@
---- agent/appconfig/constants_unix.go.orig	2021-07-07 22:38:05 UTC
-+++ agent/appconfig/constants_unix.go
-@@ -112,9 +112,9 @@ const (
+--- agent/appconfig/constants_unix.go.orig	2025-03-04 15:59:05.000000000 -0500
++++ agent/appconfig/constants_unix.go	2025-03-17 01:17:21.698424000 -0400
+@@ -116,9 +116,9 @@
  var PowerShellPluginCommandName string
  
  // DefaultProgramFolder is the default folder for SSM
@@ -9,15 +9,18 @@
  
 -var defaultWorkerPath = "/usr/bin/"
 +var defaultWorkerPath = "/usr/local/bin/"
+ var DefaultSSMAgentBinaryPath = defaultWorkerPath + "amazon-ssm-agent"
  var DefaultSSMAgentWorker = defaultWorkerPath + "ssm-agent-worker"
  var DefaultDocumentWorker = defaultWorkerPath + "ssm-document-worker"
- var DefaultSessionWorker = defaultWorkerPath + "ssm-session-worker"
-@@ -133,7 +133,7 @@ func init() {
+@@ -140,9 +140,9 @@
  	/*
- 	   Powershell command used to be poweshell in alpha versions, now it's pwsh in prod versions
+ 	   Powershell command used to be powershell in alpha versions, now it's pwsh in prod versions
  	*/
 -	PowerShellPluginCommandName = "/usr/bin/powershell"
 +	PowerShellPluginCommandName = "/usr/local/bin/powershell"
  	if _, err := os.Stat(PowerShellPluginCommandName); err != nil {
- 		PowerShellPluginCommandName = "/usr/bin/pwsh"
+-		PowerShellPluginCommandName = "/usr/bin/pwsh"
++		PowerShellPluginCommandName = "/usr/local/bin/pwsh"
  	}
+ 
+ 	// curdir is amazon-ssm-agent current directory path
