56594c8fbfb55aa4f3b0cd33fcddf23bc33c34a3
Lucas Holt <luke@foolishgames.com>
2025-06-04 08:54:10 -0400

    conflicts fun

---
+++ sysutils/u-boot-tools/files/patch-common_image.c
@@ -0,0 +1,10 @@
+--- common/image.c.orig	2025-06-03 20:40:45 UTC
++++ common/image.c
+@@ -131,6 +131,7 @@ static const table_entry_t uimage_os[] = {
+ 	{	IH_OS_DELL,	"dell",		"Dell",			},
+ 	{	IH_OS_ESIX,	"esix",		"Esix",			},
+ 	{	IH_OS_FREEBSD,	"freebsd",	"FreeBSD",		},
++	{	IH_OS_FREEBSD,	"midnightbsd",	"MidnightBSD",		},
+ 	{	IH_OS_IRIX,	"irix",		"Irix",			},
+ 	{	IH_OS_NCR,	"ncr",		"NCR",			},
+ 	{	IH_OS_OPENBSD,	"openbsd",	"OpenBSD",		},
