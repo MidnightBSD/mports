1c72bb17e11a992d178027eb6b63911a0b7d6da5
Lucas Holt <luke@foolishgames.com>
2025-06-07 19:29:05 -0400

    graphics/graphviz: 12.2

--- graphics/graphviz/files/patch-lib_expr_exgram.h
+++ graphics/graphviz/files/patch-lib_expr_exgram.h
@@ -1,11 +1,11 @@
---- lib/expr/exgram.h.orig	2019-09-12 02:11:30 UTC
+--- lib/expr/exgram.h.orig	2024-12-06 23:55:56 UTC
 +++ lib/expr/exgram.h
-@@ -23,7 +23,7 @@ extern "C" {
-  * sometimes free stuff can cost a lot
+@@ -19,7 +19,7 @@ extern "C" {
+  * with both yacc and bison
   */
  
 -#if !defined(_EXGRAM_H) && ( defined(MINTOKEN) || defined(YYTOKENTYPE) )
 +#if !defined(_EXGRAM_H)
  #define _EXGRAM_H
  
- #if !defined(_EXPARSE_H)
+ #include <cgraph/gv_ctype.h>
