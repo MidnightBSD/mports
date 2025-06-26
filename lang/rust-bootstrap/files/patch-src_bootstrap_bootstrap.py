c17093b53bb21c3f1f7594634a2be5895f5cf893
Lucas Holt <luke@foolishgames.com>
2025-06-26 15:06:00 -0400

    prelim

---
+++ lang/rust-bootstrap/files/patch-src_bootstrap_bootstrap.py
@@ -0,0 +1,10 @@
+--- src/bootstrap/bootstrap.py.orig	2021-12-29 17:31:06.208031000 -0500
++++ src/bootstrap/bootstrap.py	2021-12-29 17:31:34.579163000 -0500
+@@ -180,6 +180,7 @@
+         'Darwin': 'apple-darwin',
+         'DragonFly': 'unknown-dragonfly',
+         'FreeBSD': 'unknown-freebsd',
++        'MidnightBSD': 'unknown-freebsd',
+         'Haiku': 'unknown-haiku',
+         'NetBSD': 'unknown-netbsd',
+         'OpenBSD': 'unknown-openbsd'
