52a5199f96a1221c47d830f4c5e36a28ac1ee71b
Lucas Holt <luke@foolishgames.com>
2025-07-29 22:43:14 -0400

    attempt patch

---
+++ archivers/xpk/files/patch-shell_xPK.c
@@ -0,0 +1,10 @@
+--- shell/xPK.c.orig	2025-07-29 22:40:52.642049000 -0400
++++ shell/xPK.c	2025-07-29 22:41:11.204894000 -0400
+@@ -27,6 +27,7 @@
+ #include <sys/stat.h>
+ #include <unistd.h>
+ #include <errno.h>
++#include <stdlib.h>
+ #include <xpk/xpk.h>
+ #include <xpk/amigalibs.h>
+ 
