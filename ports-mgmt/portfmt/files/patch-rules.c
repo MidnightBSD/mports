5b5bedf95dc9732d81e4fd98cf2c68823d9cb283
Lucas Holt <luke@foolishgames.com>
2025-08-20 11:45:30 -0400

    ports-mgmt/portfmt: add SKIP_FAKE_CHECK

--- ports-mgmt/portfmt/files/patch-rules.c
+++ ports-mgmt/portfmt/files/patch-rules.c
@@ -1,5 +1,5 @@
---- rules.c.orig	2025-04-04 10:59:34 UTC
-+++ rules.c
+--- rules.c.orig	2025-04-04 06:59:34.000000000 -0400
++++ rules.c	2025-08-20 11:44:50.641850000 -0400
 @@ -1,7 +1,7 @@
  /*-
 - * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
@@ -10,16 +10,17 @@
   * All rights reserved.
   *
   * Redistribution and use in source and binary forms, with or without
-@@ -389,6 +389,8 @@ static struct VariableOrderEntry variable_order_[] = {
+@@ -389,6 +389,9 @@
  	{ BLOCK_LICENSE_OLD, "LEGAL_PACKAGE", VAR_DEFAULT, {} },
  	{ BLOCK_LICENSE_OLD, "LEGAL_TEXT", VAR_IGNORE_WRAPCOL, {} },
  
 +        { BLOCK_FAKE, "FAKE_OPTS", VAR_IGNORE_WRAPCOL, {} },
++        { BLOCK_FAKE, "SKIP_FAKE_CHECK", VAR_IGNORE_WRAPCOL, {} },
 +
  	{ BLOCK_BROKEN, "DEPRECATED", VAR_IGNORE_WRAPCOL, {} },
  	{ BLOCK_BROKEN, "EXPIRATION_DATE", VAR_SKIP_GOALCOL, {} },
  	{ BLOCK_BROKEN, "FORBIDDEN", VAR_IGNORE_WRAPCOL, {} },
-@@ -1403,7 +1405,7 @@ bool
+@@ -1403,7 +1406,7 @@
  extract_osrel_prefix(struct Mempool *pool, const char *var, char **prefix)
  {
  	for (size_t i = 0; i < freebsd_versions_len; i++) {
@@ -28,7 +29,7 @@
  		if (str_endswith(var, suffix)) {
  			*prefix = str_ndup(pool, var, strlen(var) - strlen(suffix));
  			return true;
-@@ -1640,7 +1642,7 @@ is_include_bsd_port_mk(struct AST *node)
+@@ -1640,7 +1643,7 @@
  	if (node->type == AST_INCLUDE &&
  	    node->include.type == AST_INCLUDE_BMAKE &&
  	    node->include.sys) {
