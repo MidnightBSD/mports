00dee5194f412656154bab7e3ccc3aadd07aa544
Lucas Holt <luke@foolishgames.com>
2025-08-25 14:04:49 -0400

    ports-mgmt/portfmt: update patchset

--- ports-mgmt/portfmt/files/patch-rules.c
+++ ports-mgmt/portfmt/files/patch-rules.c
@@ -1,5 +1,5 @@
 --- rules.c.orig	2025-04-04 06:59:34.000000000 -0400
-+++ rules.c	2025-08-20 11:44:50.641850000 -0400
++++ rules.c	2025-08-25 14:04:08.363887000 -0400
 @@ -1,7 +1,7 @@
  /*-
 - * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
@@ -10,6 +10,26 @@
   * All rights reserved.
   *
   * Redistribution and use in source and binary forms, with or without
+@@ -167,8 +167,8 @@
+ 	{ "do-install", 1 },
+ 	{ "post-install", 1 },
+ 	{ "post-install-script", 0 },
+-	{ "stage", 0 },
+-	{ "post-stage", 1 },
++	{ "fake", 0 },
++	{ "post-fake", 1 },
+ 	{ "test", 0 },
+ 	{ "pre-test", 1 },
+ 	{ "do-test", 1 },
+@@ -246,7 +246,7 @@
+ 	{ "readmes", 0 },
+ 	{ "reinstall", 0 },
+ 	{ "repackage", 0 },
+-	{ "restage", 0 },
++	{ "refake", 0 },
+ 	{ "rmconfig-recursive", 0 },
+ 	{ "rmconfig", 0 },
+ 	{ "run-autotools-fixup", 0 },
 @@ -389,6 +389,9 @@
  	{ BLOCK_LICENSE_OLD, "LEGAL_PACKAGE", VAR_DEFAULT, {} },
  	{ BLOCK_LICENSE_OLD, "LEGAL_TEXT", VAR_IGNORE_WRAPCOL, {} },
