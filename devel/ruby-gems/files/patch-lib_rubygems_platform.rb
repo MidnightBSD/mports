5875e97495d3269c657b9486f2311f156d4036f9
Lucas Holt <luke@foolishgames.com>
2026-01-08 16:13:42 -0500

    updates

--- devel/ruby-gems/files/patch-lib_rubygems_platform.rb
+++ devel/ruby-gems/files/patch-lib_rubygems_platform.rb
@@ -1,10 +1,10 @@
---- lib/rubygems/platform.rb.orig	2024-11-05 06:14:56.000000000 -0500
-+++ lib/rubygems/platform.rb	2024-12-07 12:50:45.538702000 -0500
-@@ -118,6 +118,7 @@
-                       when /darwin(\d+)?/ then          ["darwin",    $1]
-                       when /^macruby$/ then             ["macruby",   nil]
-                       when /freebsd(\d+)?/ then         ["freebsd",   $1]
-+                      when /midnightbsd(\d+)?/ then      ["midnightbsd",   $1]
-                       when /^java$/, /^jruby$/ then     ["java",      nil]
-                       when /^java([\d.]*)/ then         ["java",      $1]
-                       when /^dalvik(\d+)?$/ then        ["dalvik",    $1]
+--- lib/rubygems/platform.rb.orig	2025-09-09 04:54:45 UTC
++++ lib/rubygems/platform.rb
+@@ -108,6 +108,7 @@ class Gem::Platform
+                       when "macruby" then                    ["macruby", nil]
+                       when /^macruby-?(\d+(?:\.\d+)*)?/ then ["macruby", $1]
+                       when /freebsd-?(\d+)?/ then            ["freebsd", $1]
++                      when /midnightbsd-?(\d+)?/ then            ["midnightbsd", $1]
+                       when "java", "jruby" then              ["java",    nil]
+                       when /^java-?(\d+(?:\.\d+)*)?/ then    ["java",    $1]
+                       when /^dalvik-?(\d+)?$/ then           ["dalvik",  $1]
