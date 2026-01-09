4ef068531586251098c277cff4a38bcc992f8ff2
Lucas Holt <luke@foolishgames.com>
2026-01-09 15:45:32 -0500

    add varnish

--- Mk/bsd.mport.mk
+++ Mk/bsd.mport.mk
@@ -508,7 +508,7 @@ _ALL_EXT=	charsetfix desthack pathfix pkgconfig compiler kmod uidfix \
 		ldap libarchive libedit libtool llvm localbase lua \
 		metaport makeself meson mlt mono motif mysql ncurses objc ocaml openal \
 		pgsql php python java qt readline ruby samba scons sdl sqlite ssl \
-		tar tcl tk tex trigger uniquefiles wx xfce zip 7z
+		tar tcl tk tex trigger uniquefiles varnish wx xfce zip 7z
 
 .for EXT in ${_ALL_EXT:S/python//g:tu}
 .  if (${EXT:tl} == "linux" || ${EXT:tl} == "python" || ${EXT:tl} == "qt" || ${EXT:tl} == "php" || ${EXT:tl} == "kde" || ${EXT:tl} == "ruby" || ${EXT:tl} == "cabal" || ${EXT:tl} == "gstreamer") 
