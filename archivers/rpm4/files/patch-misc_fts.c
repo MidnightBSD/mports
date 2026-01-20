62d8f952a058720a62c9a0682a36e0cc92ffe0bb
Lucas Holt <luke@foolishgames.com>
2026-01-20 11:02:39 -0500

    archivers/rpm4: cleanup warning

--- archivers/rpm4/files/patch-misc_fts.c
+++ archivers/rpm4/files/patch-misc_fts.c
@@ -1,11 +1,11 @@
---- misc/fts.c.orig	2018-08-25 09:14:19 UTC
+--- misc/fts.c.orig	2022-04-07 11:13:19 UTC
 +++ misc/fts.c
-@@ -32,12 +32,14 @@ static char sccsid[] = "@(#)fts.c	8.6 (B
- #endif /* LIBC_SCCS and not lint */
+@@ -37,12 +37,14 @@ static char sccsid[] = "@(#)fts.c	8.6 (Berkeley) 8/14/
+ #endif
  
  /* Conditional to set up proper fstat64 implementation */
--#if defined(hpux) || defined(sun)
-+#if defined(hpux) || defined(__FreeBSD__) || defined(sun) || defined(__MidnightBSD__)
+-#if defined(hpux) || defined(sun) || (defined(__APPLE__) && defined(_DARWIN_FEATURE_ONLY_64_BIT_INODE))
++#if defined(hpux) || defined(sun) || defined(__FreeBSD__) || (defined(__APPLE__) && defined(_DARWIN_FEATURE_ONLY_64_BIT_INODE))
  #   define FTS_FSTAT64(_fd, _sbp)   fstat((_fd), (_sbp))
  #else
  #   define FTS_FSTAT64(_fd, _sbp)   fstat64((_fd), (_sbp))
@@ -16,22 +16,24 @@
  #if defined(_LIBC)
  #include <sys/param.h>
  #include <include/sys/stat.h>
-@@ -51,7 +53,7 @@ static char sccsid[] = "@(#)fts.c	8.6 (B
+@@ -56,7 +58,7 @@ static char sccsid[] = "@(#)fts.c	8.6 (Berkeley) 8/14/
  #else
  
  /* Conditionals for working around non-GNU environments */
 -#if defined(hpux)
-+#if defined(hpux) || defined(__FreeBSD__) || defined(__MidnightBSD__)
++#if defined(hpux) || defined(__FreeBSD__)
  #   define        _INCLUDE_POSIX_SOURCE
  #   define __errno_location() 	(&errno)
  #   define dirfd(dirp)		-1
-@@ -65,11 +67,9 @@ static char sccsid[] = "@(#)fts.c	8.6 (B
- #   define __errno_location()	(__error())
+@@ -73,13 +75,11 @@ static char sccsid[] = "@(#)fts.c	8.6 (Berkeley) 8/14/
+ #endif
  #endif
  
 -#include "system.h"
+ #include <fcntl.h>
  #include <stdlib.h>
  #include <string.h>
+ #include <dirent.h>
  #include <errno.h>
 -#include "misc/rpmfts.h"
  #   define __set_errno(val) (*__errno_location ()) = (val)
