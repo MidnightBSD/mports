43a8a85d6152c4880d7bf17015b4ab5a9fb7de15
Lucas Holt <luke@foolishgames.com>
2026-04-05 13:00:02 -0400

    change how we compute the OSREL to just keep major/minor

--- Mk/bsd.port.subdir.mk
+++ Mk/bsd.port.subdir.mk
@@ -91,7 +91,7 @@ _OSRELEASE!=		${UNAME} -r
 .  endif
 _EXPORTED_VARS+=	_OSRELEASE
 .  if !defined(OSREL)
-OSREL=	${_OSRELEASE:C/[-(].*//}
+OSREL=	${_OSRELEASE:C/^\([0-9]*\.[0-9]*\).*/\1/}
 .  endif
 _EXPORTED_VARS+=	OSREL
 
