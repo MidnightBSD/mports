c16aae68b732db697fa6c312b53f4ea5d5b2e983
Lucas Holt <luke@foolishgames.com>
2026-04-21 21:32:18 -0400

    make rust 1.88 copy port

---
+++ lang/rust188/files/patch-src_bootstrap_bootstrap.py
@@ -0,0 +1,10 @@
+--- src/bootstrap/bootstrap.py.orig	2025-11-28 15:45:49.533959000 -0500
++++ src/bootstrap/bootstrap.py	2025-11-28 15:46:20.218261000 -0500
+@@ -317,6 +317,7 @@
+         "Darwin": "apple-darwin",
+         "DragonFly": "unknown-dragonfly",
+         "FreeBSD": "unknown-freebsd",
++        "MidnightBSD": "unknown-freebsd",
+         "Haiku": "unknown-haiku",
+         "NetBSD": "unknown-netbsd",
+         "OpenBSD": "unknown-openbsd",
