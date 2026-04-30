1c7f585707a757ed00949135810c65a1ac3fdeef
Lucas Holt <luke@foolishgames.com>
2026-04-30 14:17:31 -0400

    update a few ports.  start working on llvm20

---
+++ devel/llvm20/files/patch-llvm_lib_TargetParser_Triple.cpp
@@ -0,0 +1,10 @@
+--- llvm/lib/TargetParser/Triple.cpp.orig	2024-10-29 10:09:39 UTC
++++ llvm/lib/TargetParser/Triple.cpp
+@@ -648,6 +648,7 @@ static Triple::OSType parseOS(StringRef
+     .StartsWith("darwin", Triple::Darwin)
+     .StartsWith("dragonfly", Triple::DragonFly)
+     .StartsWith("freebsd", Triple::FreeBSD)
++    .StartsWith("midnightbsd", Triple::FreeBSD)
+     .StartsWith("fuchsia", Triple::Fuchsia)
+     .StartsWith("ios", Triple::IOS)
+     .StartsWith("kfreebsd", Triple::KFreeBSD)
