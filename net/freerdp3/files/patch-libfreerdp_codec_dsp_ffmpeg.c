50f7daa8c39eb9d8a12e4ded053b8804f11acf0d
Lucas Holt <luke@foolishgames.com>
2026-09-16 21:10:28 -0400

    net/freerdp3: fix MidnightBSD platform detection (#1342)

---
+++ net/freerdp3/files/patch-libfreerdp_codec_dsp_ffmpeg.c
@@ -0,0 +1,5 @@
+--- libfreerdp/codec/dsp_ffmpeg.c.orig
++++ libfreerdp/codec/dsp_ffmpeg.c
+@@ -259 +259 @@
+-			context->context->profile = FF_PROFILE_AAC_MAIN;
++			context->context->profile = AV_PROFILE_AAC_MAIN;
